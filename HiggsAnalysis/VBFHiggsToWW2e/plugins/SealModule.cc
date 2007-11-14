fdedf6d492f92abfbe23ee7deb31551e1873f95d
govoni <>
2007-11-14 17:26:38 +0000

    to select only VBF events on the basis of MC info

---
+++ HiggsAnalysis/VBFHiggsToWW2e/plugins/SealModule.cc
@@ -0,0 +1,1 @@
+#include "PluginManager/ModuleDef.h"#include "FWCore/Framework/interface/InputSourceMacros.h"#include "FWCore/Framework/interface/MakerMacros.h"#include "Configuration/CSA06Skimming/interface/VBFMCProcessFilter.h"DEFINE_SEAL_MODULE () ;DEFINE_ANOTHER_FWK_MODULE (MCProcessFilter) ;
