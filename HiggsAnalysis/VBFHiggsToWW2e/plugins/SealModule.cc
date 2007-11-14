7853f90f80ffd313e5087944a338919b7206f232
govoni <>
2007-11-14 18:25:17 +0000

    dettagli

--- HiggsAnalysis/VBFHiggsToWW2e/plugins/SealModule.cc
+++ HiggsAnalysis/VBFHiggsToWW2e/plugins/SealModule.cc
@@ -1,3 +1,3 @@
-// $Id: VBFMCProcessFilter.h,v 1.1 2007/11/14 17:26:38 govoni Exp $
+// $Id: SealModule.cc,v 1.2 2007/11/14 17:34:43 govoni Exp $
 
-#include "PluginManager/ModuleDef.h"#include "FWCore/Framework/interface/InputSourceMacros.h"#include "FWCore/Framework/interface/MakerMacros.h"#include "Configuration/CSA06Skimming/interface/VBFMCProcessFilter.h"DEFINE_SEAL_MODULE () ;DEFINE_ANOTHER_FWK_MODULE (MCProcessFilter) ;
+#include "PluginManager/ModuleDef.h"#include "FWCore/Framework/interface/InputSourceMacros.h"#include "FWCore/Framework/interface/MakerMacros.h"#include "Configuration/CSA06Skimming/interface/VBFMCProcessFilter.h"DEFINE_SEAL_MODULE () ;DEFINE_ANOTHER_FWK_MODULE (VBFMCProcessFilter) ;
