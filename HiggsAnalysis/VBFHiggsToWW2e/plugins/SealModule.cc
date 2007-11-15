07da55bb24c1109f43c25d15e01a9b360f53edf1
tancini <>
2007-11-15 17:18:25 +0000

    SealModule.cc

--- HiggsAnalysis/VBFHiggsToWW2e/plugins/SealModule.cc
+++
@@ -1,9 +0,0 @@
-// $Id: SealModule.cc,v 1.3 2007/11/14 18:25:17 govoni Exp $
-
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/InputSourceMacros.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "HiggsAnalysis/VBFHiggsToWW2e/plugins/VBFMCProcessFilter.h"
-
-DEFINE_SEAL_MODULE () ;
-DEFINE_ANOTHER_FWK_MODULE (VBFMCProcessFilter) ;
