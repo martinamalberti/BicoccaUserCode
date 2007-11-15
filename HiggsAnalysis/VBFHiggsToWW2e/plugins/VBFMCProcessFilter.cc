83910f0dcbce81e8260b53ac0e21a20ef7a6250d
tancini <>
2007-11-15 16:16:19 +0000

    *** empty log message ***

--- HiggsAnalysis/VBFHiggsToWW2e/plugins/VBFMCProcessFilter.cc
+++ HiggsAnalysis/VBFHiggsToWW2e/plugins/VBFMCProcessFilter.cc
@@ -1,20 +1,37 @@
-// $Id: VBFMCProcessFilter.cc,v 1.3 2007/11/14 17:43:27 govoni Exp $
+// $Id: VBFMCProcessFilter.cc,v 1.4 2007/11/14 18:17:55 govoni Exp $
 
-#include "HiggsAnalysis/VBFHiggsToWW2e/interface/VBFMCProcessFilter.h"#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"#include <iostream>//! ctor
-VBFMCProcessFilter::VBFMCProcessFilter(const edm::ParameterSet& iConfig) :  label_ (iConfig.getUntrackedParameter ("moduleLabel",std::string ("source"))){}
+#include "HiggsAnalysis/VBFHiggsToWW2e/plugins/VBFMCProcessFilter.h"
+
+#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
+#include <iostream>
+
+
+//! ctor
+VBFMCProcessFilter::VBFMCProcessFilter(const edm::ParameterSet& iConfig) :
+  label_ (iConfig.getUntrackedParameter ("moduleLabel",std::string ("source")))
+{}
+
 
 // ------------------------------------------------------------------------------------
 
-//! dtor
-VBFMCProcessFilter::~VBFMCProcessFilter(){}
+
+//! dtor
+VBFMCProcessFilter::~VBFMCProcessFilter()
+{}
+
 
 // ------------------------------------------------------------------------------------
 
-//! filtering method
+
+//! filtering method
 bool 
-VBFMCProcessFilter::filter (edm::Event& iEvent, const edm::EventSetup& iSetup){
-  Handle<HepMCProduct> evtMC;  event.getByLabel(label_,evtMC);
+VBFMCProcessFilter::filter (edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  edm::Handle<edm::HepMCProduct> evtMC;
+  iEvent.getByLabel(label_,evtMC);
 
+ const HepMC::GenEvent * Evt = evtMC->GetEvent();
+    
  //PG loop over the generated particles
  for (HepMC::GenEvent::particle_const_iterator part = Evt->particles_begin(); 
        part != Evt->particles_end () ; 
@@ -23,7 +40,11 @@ VBFMCProcessFilter::filter (edm::Event& iEvent, const edm::EventSetup& iSetup){
        if (abs((*part)->pdg_id()) == 25 && 
           (*part)->production_vertex ()->particles_in_size () == 2 ) //PG this is an higgs boson
          {
-           int pdgID_1 = (*part)->production_vertex ()->particles_in_const_begin ()->pdg_id () ;
+           HepMC::GenParticle* motherA = (*((*part)->production_vertex()->particles_in_const_begin())); 
+           HepMC::GenParticle* motherB = (*((*part)->production_vertex()->particles_in_const_begin()+1));
+             
+           int pdgID_1=  
+           //int pdgID_1 = (*part)->production_vertex ()->particles_in_const_begin ())->pdg_id () ;
            int pdgID_2 = ((*part)->production_vertex ()->particles_in_const_begin ()+1)->pdg_id () ;
            if ( ( abs (pdgID_1) == 23 &&
                   abs (pdgID_2) == 23 ) ||
@@ -33,5 +54,10 @@ VBFMCProcessFilter::filter (edm::Event& iEvent, const edm::EventSetup& iSetup){
                return true ;
              }  
          }
-    } //PG loop over the generated particles  return false ;}	
+    } //PG loop over the generated particles
+
+  return false ;
+
+}
+	
 
