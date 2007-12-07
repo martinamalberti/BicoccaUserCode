86c1d29adcbc0b394451a47a6107df92855db57e
govoni <>
2007-12-07 16:17:27 +0000

    tolti tagli inutili

--- HiggsAnalysis/VBFHiggsToWW2e/interface/VBFProcessFilter.h
+++ HiggsAnalysis/VBFHiggsToWW2e/interface/VBFProcessFilter.h
@@ -1,4 +1,4 @@
-#ifndef VBFProcessFilter_h#define VBFProcessFilter_h// -*- C++ -*-//// Package:    VBFProcessFilter// Class:      VBFProcessFilter// /*  Description: filter events based on the Pythia ProcessID and the Pt_hat Implementation: inherits from generic EDFilter     *///// $Id: VBFProcessFilter.h,v 1.2 2007/12/07 14:47:02 govoni Exp $////// system include files#include <memory>// user include files#include "FWCore/Framework/interface/Frameworkfwd.h"#include "FWCore/Framework/interface/EDFilter.h"#include "FWCore/Framework/interface/Event.h"#include "FWCore/Framework/interface/MakerMacros.h"#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#ifndef VBFProcessFilter_h#define VBFProcessFilter_h// -*- C++ -*-//// Package:    VBFProcessFilter// Class:      VBFProcessFilter// /*  Description: filter events based on the Pythia ProcessID and the Pt_hat Implementation: inherits from generic EDFilter     *///// $Id: VBFProcessFilter.h,v 1.3 2007/12/07 16:12:47 govoni Exp $////// system include files#include <memory>// user include files#include "FWCore/Framework/interface/Frameworkfwd.h"#include "FWCore/Framework/interface/EDFilter.h"#include "FWCore/Framework/interface/Event.h"#include "FWCore/Framework/interface/MakerMacros.h"#include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include "DataFormats/JetReco/interface/CaloJet.h"
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
@@ -26,8 +26,6 @@ class VBFProcessFilter : public edm::EDFilter
       double m_jetPtMin ;
       
       //PG selections on the jet tags
-      double m_tagJetsEtaMin ;
-      double m_tagJetsEtaMax ;
       double m_tagJetsDeltaEtaMin ;
       double m_tagJetsDeltaEtaMax ;
       double m_tagJetsInvMassMin ;
--- HiggsAnalysis/VBFHiggsToWW2e/src/VBFProcessFilter.cc
+++ HiggsAnalysis/VBFHiggsToWW2e/src/VBFProcessFilter.cc
@@ -1,4 +1,4 @@
-// $Id: VBFProcessFilter.cc,v 1.5 2007/12/07 15:28:49 govoni Exp $
+// $Id: VBFProcessFilter.cc,v 1.6 2007/12/07 16:12:47 govoni Exp $
 
 #include "HiggsAnalysis/VBFHiggsToWW2e/interface/VBFProcessFilter.h"
 
@@ -11,8 +11,6 @@ VBFProcessFilter::VBFProcessFilter (const edm::ParameterSet& iConfig) :
   m_jetInputTag (iConfig.getParameter<edm::InputTag> ("jetInputTag")) ,
   m_jetEtaMax (iConfig.getParameter<double> ("jetEtaMax")) ,
   m_jetPtMin (iConfig.getParameter<double> ("jetPtMin")) ,
-  m_tagJetsEtaMin (iConfig.getParameter<double> ("tagJetsEtaMin")) ,
-  m_tagJetsEtaMax (iConfig.getParameter<double> ("tagJetsEtaMax")) ,
   m_tagJetsDeltaEtaMin (iConfig.getParameter<double> ("tagJetsDeltaEtaMin")) ,
   m_tagJetsDeltaEtaMax (iConfig.getParameter<double> ("tagJetsDeltaEtaMax")) ,
   m_tagJetsInvMassMin (iConfig.getParameter<double> ("tagJetsInvMassMin")) ,
@@ -50,8 +48,6 @@ VBFProcessFilter::filter (edm::Event& iEvent, const edm::EventSetup& iSetup)
   if (m_checkOpposite && tagJets.first->eta () * tagJets.second->eta () > 0) return false ;
   double deltaEta = fabs (tagJets.first->eta () - tagJets.second->eta ()) ;
   if (deltaEta < m_tagJetsDeltaEtaMin || deltaEta > m_tagJetsDeltaEtaMax) return false ;
-  if (tagJets.first->eta () < m_tagJetsEtaMin || tagJets.first->eta () > m_tagJetsEtaMax ||
-      tagJets.second->eta () < m_tagJetsEtaMin || tagJets.second->eta () > m_tagJetsEtaMax) return false ;
   double invMass = (tagJets.first->p4 () + tagJets.second->p4 ()).M () ;
   if (invMass < m_tagJetsInvMassMin || invMass > m_tagJetsInvMassMax) return false ;
 
