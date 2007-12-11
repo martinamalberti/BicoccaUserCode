0a7f152590431f8ed326b19957954362875f76b5
govoni <>
2007-12-11 17:35:21 +0000

    jet tagging senza merging nel codice

---
+++ HiggsAnalysis/VBFHiggsToWW2e/src/VBFSimpleJetTagger.cc
@@ -0,0 +1,41 @@
+#include "HiggsAnalysis/VBFHiggsToWW2e/interface/VBFSimpleJetTagger.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include <iostream>
+#include <algorithm>
+
+
+VBFSimpleJetTagger::VBFSimpleJetTagger (const edm::ParameterSet& iConfig) :
+  m_jetEtaMax (iConfig.getParameter<double> ("jetEtaMax")) ,
+  m_jetPtMin (iConfig.getParameter<double> ("jetPtMin")) 
+{
+}  
+
+
+// ----------------------------------------------------------------------------
+
+  
+VBFSimpleJetTagger::~VBFSimpleJetTagger ()
+{
+}
+
+
+// ----------------------------------------------------------------------------
+
+  
+void VBFSimpleJetTagger::select (edm::Handle<VBFSimpleJetTagger::collection> jetCollectionHandle, 
+                           const edm::Event& iEvent)
+{
+  m_selected.clear () ;
+
+  //PG get the jet tags
+  std::pair<VBFjetIt,VBFjetIt> tagJets = findTagJets (jetCollectionHandle->begin (),
+                                                      jetCollectionHandle->end (),
+                                                      m_jetPtMin, m_jetEtaMax) ;
+
+  m_selected.push_back (jet (jetCollectionHandle,tagJets.first - jetCollectionHandle->begin ())) ;
+  m_selected.push_back (jet (jetCollectionHandle,tagJets.second - jetCollectionHandle->begin ())) ;
+}
+
