219f871f227c066ded389dcc8fd900265c31328b
govoni <>
2007-12-23 16:09:07 +0000

    spostato InvRingCalib in EcalCalibAlgos

--- Calibration/EcalAlCaRecoProducers/src/SealModule.cc
+++ Calibration/EcalAlCaRecoProducers/src/SealModule.cc
@@ -9,11 +9,12 @@
 #include "Calibration/EcalAlCaRecoProducers/interface/AlCaPi0RecHitsProducer.h"
 #include "Calibration/EcalAlCaRecoProducers/interface/AlCaPi0BasicClusterRecHitsProducer.h"
 #include "Calibration/EcalAlCaRecoProducers/interface/TBInvMatrixLooper.h"
-#include "Calibration/EcalAlCaRecoProducers/interface/InvRingCalib.h"
 #include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectron.h"
 #include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectronFwd.h"
 #include "PhysicsTools/UtilAlgos/interface/SingleObjectSelector.h"
 #include "PhysicsTools/RecoAlgos/interface/PixelMatchGsfElectronSelector.h"
+
+
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(AlCaElectronsProducer);
 DEFINE_ANOTHER_FWK_MODULE(AlCaElectronsTest);
@@ -23,5 +24,4 @@ DEFINE_ANOTHER_FWK_MODULE(AlCaPi0RecHitsProducer);
 DEFINE_ANOTHER_FWK_MODULE(AlCaPi0BasicClusterRecHitsProducer);
 
 DEFINE_ANOTHER_FWK_LOOPER(TBInvMatrixLooper);
-DEFINE_ANOTHER_FWK_LOOPER(InvRingCalib);
 
