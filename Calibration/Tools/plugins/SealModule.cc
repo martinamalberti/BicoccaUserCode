7d32aa0a9f26890e32fe282fb56e2301c29a45fd
malberti <>
2008-01-17 12:34:24 +0000

    updated for EleCalibSelector

--- Calibration/Tools/plugins/SealModule.cc
+++ Calibration/Tools/plugins/SealModule.cc
@@ -1,15 +1,26 @@
-//#include "PluginManager/ModuleDef.h" 
+
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+
 #include "Calibration/Tools/interface/PhiRangeSelector.h"
 #include "Calibration/Tools/interface/IMASelector.h"
+#include "Calibration/Tools/interface/EleCalibSelector.h"
+
+#include "PhysicsTools/UtilAlgos/interface/ObjectSelector.h"
 #include "PhysicsTools/UtilAlgos/interface/SingleObjectSelector.h"
+#include "PhysicsTools/UtilAlgos/interface/SingleElementCollectionSelector.h"
+#include "PhysicsTools/UtilAlgos/interface/SingleObjectRefVectorSelector.h"
+#include "DataFormats/Common/interface/RefVector.h"
+
+
 #include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectron.h"
 #include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectronFwd.h"
 #include "PhysicsTools/RecoAlgos/interface/PixelMatchGsfElectronSelector.h"
 
-DEFINE_SEAL_MODULE();
 
+#include "FakeRate/EffAnalysis/plugins/RobertoSelector.h"
+
+DEFINE_SEAL_MODULE();
 
 namespace reco {
   namespace modules {
@@ -20,6 +31,10 @@ namespace reco {
     typedef SingleObjectSelector<reco::PixelMatchGsfElectronCollection,
                                  ::IMASelector> IMASelector; 
     DEFINE_ANOTHER_FWK_MODULE(IMASelector);
-
   }
 }
+
+typedef RobertoSelector<EleCalibSelector> ElectronCalibrationSelector;
+DEFINE_ANOTHER_FWK_MODULE( ElectronCalibrationSelector );
+
+
