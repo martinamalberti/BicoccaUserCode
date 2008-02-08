9880b4b9c8085e4b835a2c7b78ddabea3e46bc31
govoni <>
2008-02-08 09:49:10 +0000

    added VBFLeptPlots

--- HiggsAnalysis/VBFHiggsToWW2e/src/SealModule.cc
+++ HiggsAnalysis/VBFHiggsToWW2e/src/SealModule.cc
@@ -87,6 +87,9 @@ DEFINE_ANOTHER_FWK_MODULE (VBFplots) ;
 #include "HiggsAnalysis/VBFHiggsToWW2e/interface/VBFElePlots.h"
 DEFINE_ANOTHER_FWK_MODULE (VBFElePlots) ; 
 
+#include "HiggsAnalysis/VBFHiggsToWW2e/interface/VBFLeptPlots.h"
+DEFINE_ANOTHER_FWK_MODULE (VBFLeptPlots) ; 
+
 #include "HiggsAnalysis/VBFHiggsToWW2e/interface/VBFTestTagJets.h"
 DEFINE_ANOTHER_FWK_MODULE (VBFTestTagJets) ; 
 
