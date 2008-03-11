4458658bfbfe821237f026fa84e96410a28609f9
tancini <>
2008-03-11 11:45:30 +0000

    *** empty log message ***

--- HiggsAnalysis/VBFHiggsToWW2e/test/goToRow.py
+++ HiggsAnalysis/VBFHiggsToWW2e/test/goToRow.py
@@ -4,5 +4,5 @@ import sys
 
 #print open(sys.argv[1],"r").readlines()[int(sys.argv[2])]
 #print open(sys.argv[1],"r").readlines()[int(sys.argv[2])].split()[1]
-print open(sys.argv[1],"r").readlines()[int(sys.argv[2])]
+print open(sys.argv[1],"r").readlines()[int(sys.argv[2])][:-1]
 #print open(sys.argv[1],"r").readlines()[int(sys.argv[2])].split()[1][:-1]
