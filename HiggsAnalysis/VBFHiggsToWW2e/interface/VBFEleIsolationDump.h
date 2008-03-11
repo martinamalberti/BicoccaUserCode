de0555de877ed29aaa20519e43a1d6327ed27a31
govoni <>
2008-03-11 13:47:29 +0000

    to dump ntuple for ele isol studies

---
+++ HiggsAnalysis/VBFHiggsToWW2e/interface/VBFEleIsolationDump.h
@@ -0,0 +1,166 @@
+// -*- C++ -*-
+//
+// Package:    VBFEleIsolationDump
+// Class:      VBFEleIsolationDump
+// 
+/**\class VBFEleIsolationDump VBFEleIsolationDump.cc HiggsAnalysis/VBFEleIsolationDump/src/VBFEleIsolationDump.cc
+
+ Description: <one line class summary>
+ 
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Pietro Govoni
+//         Created:  Wed Nov 14 17:32:25 CET 2007
+// $Id: VBFEleIsolationDump.h,v 1.5 2008/03/10 17:50:10 govoni Exp $
+//
+//
+
+#ifndef VBFEleIsolationDump_H
+#define VBFEleIsolationDump_H
+
+// system include files
+#include <memory>
+#include <string>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "DataFormats/Common/interface/View.h"
+
+#include "AnalysisDataFormats/Egamma/interface/ElectronID.h"
+#include "AnalysisDataFormats/Egamma/interface/ElectronIDAssociation.h"
+#include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectronFwd.h"
+#include "DataFormats/EgammaCandidates/interface/ElectronFwd.h"
+#include "DataFormats/EgammaCandidates/interface/Electron.h"
+#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
+#include "CLHEP/HepMC/GenEvent.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
+
+#include "DataFormats/Common/interface/TriggerResults.h"
+
+#include "DataFormats/JetReco/interface/CaloJet.h"
+#include "DataFormats/JetReco/interface/CaloJetCollection.h"
+#include "DataFormats/JetReco/interface/GenJet.h"
+
+#include "DataFormats/METReco/interface/CaloMET.h"
+#include "DataFormats/METReco/interface/CaloMETCollection.h"
+#include "DataFormats/METReco/interface/GenMET.h"
+#include "DataFormats/METReco/interface/GenMETCollection.h"
+
+#include "DataFormats/HLTReco/interface/HLTFilterObject.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+
+#include "DataFormats/TrackCandidate/interface/TrackCandidate.h"
+#include "DataFormats/TrackCandidate/interface/TrackCandidateCollection.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackExtra.h"
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Candidate/interface/CandidateFwd.h"
+
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "RecoEgamma/EgammaIsolationAlgos/interface/ElectronTkIsolation.h"
+#include "RecoEgamma/EgammaIsolationAlgos/interface/EgammaHcalIsolation.h"
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/Utilities/interface/Exception.h"
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Candidate/interface/CandAssociation.h"
+#include "DataFormats/RecoCandidate/interface/RecoCandidate.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
+
+#include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
+#include "DataFormats/HcalDetId/interface/HcalDetId.h"
+#include "Geometry/CaloGeometry/interface/CaloCellGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+
+#include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "RecoCaloTools/MetaCollections/interface/CaloRecHitMetaCollections.h"
+
+#include "RecoBTag/MCTools/interface/JetFlavourIdentifier.h"
+#include "HiggsAnalysis/VBFHiggsToWW2e/interface/VBFEleTrackerIsolationAlgo.h"
+
+//root include
+#include <TNtuple.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TTree.h>
+#include <TFile.h>
+#include <TLorentzVector.h>
+#include <TClonesArray.h>
+#include <TVector3.h>
+
+
+class VBFEleIsolationDump : public edm::EDAnalyzer {
+
+  public:
+    
+      typedef math::XYZTLorentzVector LorentzVector ;
+      typedef std::vector<LorentzVector> LorentzVectorCollection ;
+      typedef edm::View<reco::PixelMatchGsfElectron> electronCollection ;
+      typedef reco::PixelMatchGsfElectronRef electronRef ;
+      typedef edm::RefToBase<reco::PixelMatchGsfElectron> electronBaseRef ;
+      typedef edm::View<reco::Track> trackCollection ; 
+
+      //! ctor
+      explicit VBFEleIsolationDump (const edm::ParameterSet&);
+      //! dtor
+      ~VBFEleIsolationDump ();
+
+      virtual void beginJob (const edm::EventSetup&);
+      virtual void analyze (const edm::Event&, const edm::EventSetup&);
+      virtual void endJob ();
+
+   private:
+
+      void findFirstTwo (edm::Handle<electronCollection> & EleHandle ,
+                         electronRef & firstEle ,
+                         electronRef & secondEle) ;
+ 
+   private:
+
+      edm::InputTag m_GSFInputTag ;
+      edm::InputTag m_AmbRefInputTag ;
+      edm::InputTag m_OLDIsoInputTag ;
+      edm::InputTag m_NEWIsoInputTag ;
+      edm::InputTag m_TrackInputTag ;
+  
+      //! isolation with the veto around all the GSF electrons
+      TNtuple * m_dumpIsoW ;
+      //! isolation with the veto around the isolated GSF electron only
+      TNtuple * m_dumpIsoWO ;
+
+      VBFEleTrackerIsolationAlgo m_tkIsolationAlgoWithOtherCones ;
+      VBFEleTrackerIsolationAlgo m_tkIsolationAlgoNoOtherCones ;
+
+};
+
+#endif
