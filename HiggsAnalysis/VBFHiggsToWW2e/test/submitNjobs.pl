7273508e14e6731ec51602014e511815d50b12b5
tancini <>
2008-03-11 12:10:44 +0000

    *** empty log message ***

--- HiggsAnalysis/VBFHiggsToWW2e/test/submitNjobs.pl
+++ HiggsAnalysis/VBFHiggsToWW2e/test/submitNjobs.pl
@@ -1,13 +1,15 @@
 $CFGFILE_TEMPLATE = $ARGV[0] ;
 $SAMPLELIST = $ARGV[1] ; # lista di tutti i file .root del sample in questione senza path
-$OUTPUTTAG = $ARGV[2] ; # nome comune per il .cfi per il .cfg per il .csh per il .root
-$FILEPERNODO = $ARGV[3] ; # numero file che si vogliono lanciare su un nodo
+$ORIGINALPATH = $ARGV[2] ;# path dei .root relativi al sample sul cluster
+$OUTPUTTAG = $ARGV[3] ; # nome comune per il .cfi per il .cfg per il .csh per il .root
+$FILEPERNODO = $ARGV[4] ; # numero file che si vogliono lanciare su un nodo
+#creare prima la dir jobDataset sotto test per la copia in locale dei file
 
 use Cwd;
 my $DIR = getcwd () ;
 
-$COPIAPATH = $DIR ;
-
+$COPIAPATH = $DIR."/jobDataset/" ;
+print ("Copiero' i .root in ".$COPIAPATH."\n") ;
 
 ########################################### #preparare i cfi
 
@@ -24,8 +26,9 @@ for ($MYRUN = 0; $MYRUN < $NUMFILES; $MYRUN++ )
 {
 	if (($MYRUN >= $MIN) && ($MYRUN < $MAX)) 
 	{
-		$COPIA = $OUTPUTTAG."_copia".$MIN.".csh"; # per la copia dei dati sul nodo
-		system ("rm -f ".$COPIA ) ;  
+		$COPIA = $COPIAPATH."/".$OUTPUTTAG."_copia".$MIN.".csh"; # per la copia dei dati sul nodo
+		system ("rm -f ".$COPIA ) ;
+                print ($COPIA."\n");  
 		open (COPIANAME,">>".$COPIA) or die "Cannot open ".$COPIA." to write the config file" ;
 	
 		#$CONFIG = "HiggsAnalysis/VBFHiggsToWW2e/data/datasets/listaFiles_From".$MIN.".cfi";
@@ -37,15 +40,15 @@ for ($MYRUN = 0; $MYRUN < $NUMFILES; $MYRUN++ )
 		$MAX = $MAX + $FILEPERNODO;
 		
 		print CONFIGNAME "source = PoolSource\n"; 
-        print CONFIGNAME "{\n"; 
+                print CONFIGNAME "{\n"; 
 		print CONFIGNAME "  untracked vstring fileNames =\n"; 
 		print CONFIGNAME "   {\n";
 	}
 
-	$RIGA =`./goToRow.py numberedFileList.txt $MYRUN`; # sriptino di Danilo per leggere una riga di un file
+	$RIGA =`./goToRow.py $SAMPLELIST $MYRUN`; # sriptino di Danilo per leggere una riga di un file
 	chop ($RIGA);
 
-	print COPIANAME "cp ".$RIGA." $COPIAPATH\n"; 	# per la copia sul singolo nodo
+	print COPIANAME "cp ".$ORIGINALPATH."/".$RIGA." ".$COPIAPATH."\n"; 	# per la copia sul singolo nodo
 	
     if ((($MYRUN%$FILEPERNODO) == ($FILEPERNODO-1)) || ($MYRUN == ($NUMFILES-1))) ### se e' l'ultima riga non serve la virgola e chiudo le graffe
 		{
@@ -55,7 +58,7 @@ for ($MYRUN = 0; $MYRUN < $NUMFILES; $MYRUN++ )
 		}
 	else 
 		{
-		print CONFIGNAME "   \"file:".$RIGA."\",  \n";
+		print CONFIGNAME "   \"file:".$COPIAPATH.$RIGA."\",  \n";
 		}
 }
 
@@ -64,22 +67,22 @@ for ($MYRUN = 0; $MYRUN < $NUMFILES; $MYRUN++ )
 
 for ($INDEX = 0; $INDEX < $NUMFILES; $INDEX = $INDEX + $FILEPERNODO)
 {
-	$CONFIG = $OUTPUTTAG."_myCfg_From".$INDEX.".cfg";
+    $CONFIG = $OUTPUTTAG."_myCfg_From".$INDEX.".cfg";
     $ROOTFILE = $OUTPUTTAG."_from_".$INDEX.".root" ;
-	$SAMPLEFILE = $OUTPUTTAG."_listaFiles_From".$INDEX.".cfi";
+    $SAMPLEFILE = "HiggsAnalysis/VBFHiggsToWW2e/test/".$OUTPUTTAG."_listaFiles_From".$INDEX.".cfi";
 
-	system ("cat ".$CFGFILE_TEMPLATE." | sed -e s%DATASETFILE%".$SAMPLEFILE."% > tempo1\n") ;
+    system ("cat ".$CFGFILE_TEMPLATE." | sed -e s%DATASETFILE%".$SAMPLEFILE."% > tempo1\n") ;
     system ("cat tempo1 | sed -e s%OUTPUT_ROOT%".$DIR."/".$ROOTFILE."% > tempo2\n") ;
     system ("mv tempo2 ".$CONFIG."\n") ;
     system ("rm tempo*\n") ;
 
-    $JOB = $OUTPUTTAG."_lanciaFiles_From".$MIN.".csh";
-    $COPIA = $OUTPUTTAG."_copia".$MIN.".csh";
+    $JOB = $OUTPUTTAG."_lanciaFiles_From".$INDEX.".csh";
+    $COPIA =  $COPIAPATH."/".$OUTPUTTAG."_copia".$INDEX.".csh";
 	
-	system ("rm -f ".$JOB ) ;  
-	open (JOBNAME,">>".$JOB) or die "Cannot open ".$JOB." to write the config file" ;
-	print JOBNAME  "cmsRun  ".$DIR."/".$CONFIG."\n"; 
-	print JOBNAME  "source $COPIA\n"; 
+    system ("rm -f ".$JOB ) ;  
+    open (JOBNAME,">>".$JOB) or die "Cannot open ".$JOB." to write the config file" ;
+    print JOBNAME  "source $COPIA\n";
+    print JOBNAME  "cmsRun  ".$DIR."/".$CONFIG."\n"; 
 
     print ("qsub -V -q fastcms ".$JOB."...\n");
     system ("qsub -V -q fastcms ".$JOB."\n");
