f89b212828b16e560d566a2059b1862510a88704
tancini <>
2008-03-11 10:50:50 +0000

    prende file senza numeri

--- HiggsAnalysis/VBFHiggsToWW2e/test/goToRow.py
+++ HiggsAnalysis/VBFHiggsToWW2e/test/goToRow.py
@@ -3,5 +3,6 @@
 import sys
 
 #print open(sys.argv[1],"r").readlines()[int(sys.argv[2])]
-print open(sys.argv[1],"r").readlines()[int(sys.argv[2])].split()[1]
+#print open(sys.argv[1],"r").readlines()[int(sys.argv[2])].split()[1]
+print open(sys.argv[1],"r").readlines()[int(sys.argv[2])]
 #print open(sys.argv[1],"r").readlines()[int(sys.argv[2])].split()[1][:-1]
