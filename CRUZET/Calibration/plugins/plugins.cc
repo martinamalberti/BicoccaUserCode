c8856a4f0896755c1d2060db90a496cb7b2501be
deguio <>
2008-07-22 09:12:06 +0000

    added EcalCosmicsTreeXtals

--- CRUZET/Calibration/plugins/plugins.cc
+++ CRUZET/Calibration/plugins/plugins.cc
@@ -5,6 +5,7 @@
 #include "CRUZET/Calibration/plugins/MuonAssociatorEcalIP.h"
 #include "CRUZET/Calibration/plugins/EcalCosmicsTree.h"
 #include "CRUZET/Calibration/plugins/EcalCosmicsTreeTest.h"
+#include "CRUZET/Calibration/plugins/EcalCosmicsTreeXtals.h"
 
 DEFINE_FWK_MODULE(MuonAssociator);
 DEFINE_FWK_MODULE(MuonAssociatorDelta);
@@ -12,3 +13,4 @@ DEFINE_FWK_MODULE(MuonAssociatorUpDown);
 DEFINE_FWK_MODULE(MuonAssociatorEcalIP);
 DEFINE_FWK_MODULE(EcalCosmicsTree);
 DEFINE_FWK_MODULE(EcalCosmicsTreeTest);
+DEFINE_FWK_MODULE(EcalCosmicsTreeXtals);
