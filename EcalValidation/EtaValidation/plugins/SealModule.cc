df78757283dce5a9c4f2603110803a354c0dd92e
amassiro <>
2008-08-28 12:53:16 +0000

    first commit

---
+++ EcalValidation/EtaValidation/plugins/SealModule.cc
@@ -0,0 +1,15 @@
+//#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ModuleFactory.h"
+#include "FWCore/Framework/interface/eventsetupdata_registration_macro.h"
+
+#include "EtaAnalyzer.h"
+
+DEFINE_SEAL_MODULE();
+
+#include "PhysicsTools/UtilAlgos/interface/Merger.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/EgammaReco/interface/SuperClusterFwd.h"
+typedef Merger<reco::SuperClusterCollection> EgammaSuperClusterMerger;
+DEFINE_FWK_MODULE( EgammaSuperClusterMerger );
+DEFINE_ANOTHER_FWK_MODULE(EtaAnalyzer);
