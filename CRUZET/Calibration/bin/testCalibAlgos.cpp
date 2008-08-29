00ceebaa1be428b2b256863434ae6ba77e1654e7
abenagli <>
2008-08-29 15:28:40 +0000

    added map filling

--- CRUZET/Calibration/bin/testCalibAlgos.cpp
+++ CRUZET/Calibration/bin/testCalibAlgos.cpp
@@ -28,135 +28,162 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include <boost/foreach.hpp>
 
-void fillEBMap (std::map<int,double> & EBxtlMap, //PG effective output
-                double & pSubtract,              //PG effective output
-                EBDetId EBmax,
-                std::vector<EBDetId> & crystals,
-                const EcalRecHitCollection * hitsCollection,
-                std::map<int,int> xtalRegionId, //PG FIXME become a member of a class
-                std::map<int,int> xtalPositionInRegion, //PG FIXME become a member of a class
-                int EBNumberOfRegion) ;
+
+int findRegion (EcalCosmicsTreeContent treeVars,
+                int SCindex,
+                EBregionBuilder EBRegionsTool) ;
 
 
 //PG ------------------------------------------------------------------
 
 
+
 int main (int argc, char** argv)
 {
+  std::cerr << "Welcome to testCalibAlgos program" << std::endl;
+
+
+  // read input parameters from config file
+  if(!argv[1])
+    {
+      std::cout << ">>> No input config file given. Terminating program <<<" << std::endl;
+      return -1;
+    }
 
   std::string fileName(argv[1]);
   boost::shared_ptr<edm::ProcessDesc> processDesc = edm::readConfigFile(fileName);
   boost::shared_ptr<edm::ParameterSet> parameterSet = processDesc->getProcessPSet() ;
   std::cout << parameterSet->dump () << std::endl ; //PG for testing
 
-  std::vector<VEcalCalibBlock*> calibBlocks ;
-  calibBlocks.push_back (new IMACalibBlock (10)) ;
-  calibBlocks.push_back (new L3CalibBlock (10, 1)) ;
+
+  std::string algorithm = "IMA" ;
+
+
 
   //PG FIXME parameters to be read from outside
-  EBregionBuilder EBregionsTool (-85, 85, 5, 0, 20, 5) ;
+  EBregionBuilder EBRegionsTool (-85, -1, 2, 1, 181, 2) ;
+
 
   //PG single blocks calibrators
   std::vector<VEcalCalibBlock *> EcalCalibBlocks ;
 
-  std::string algorithm = "IMA" ;
+
 
   //PG loop over the regions set
-  for (int region = 0 ; 
-       region < EBregionsTool.EBregionsNum () ; 
+  for (int region = 0 ;
+       region < EBRegionsTool.EBregionsNum () ;
        ++region)
-    {   
+    {
       if (algorithm == "IMA")
         EcalCalibBlocks.push_back (
-            new IMACalibBlock (EBregionsTool.xtalNumInRegion (region))
-          ) ; 
+            new IMACalibBlock (EBRegionsTool.xtalNumInRegion (region))
+          ) ;
       else if (algorithm == "L3")
         EcalCalibBlocks.push_back (
-            new L3CalibBlock (EBregionsTool.xtalNumInRegion (region), 
-                              1)
-          ) ; 
+            new L3CalibBlock (EBRegionsTool.xtalNumInRegion (region), 1)
+          ) ;
       else
         {
-          edm::LogError ("building") << algorithm 
+          edm::LogError ("building") << algorithm
                           << " is not a valid calibration algorithm" ;
-          exit (1) ;    
-        }    
+          exit (1) ;
+        }
     } //PG loop over the regions set
+
+
 
   //PG FIXME one has to read and save calibration coeffs
   //PG FIXME read from xml files or DB directly
-  
+
   //PG FIXME would it be possible to do it for rings or crystals
   //PG FIXME in the same code?
 
+
+
+  std::cout << ">>> testCalibAlgos::OpeningFiles::begin <<<" << std::endl;
+
   TChain * chain = new TChain ("EcalCosmicsAnalysis") ;
-  chain->Add (argv[1]) ;
 
-  EcalCosmicsTreeContent treeVars ; 
+  chain->Add("/castor/cern.ch/user/m/mattia/50908Global/EcalCosmicsTree-509081.tree.root");
+
+  std::cout << ">>> testCalibAlgos::OpeningFiles::end <<<" << std::endl;
+
+
+
+
+  std::cout << ">>> testCalibAlgos::TreeBuilding::begin <<<" << std::endl;
+
+  EcalCosmicsTreeContent treeVars ;
   setBranchAddresses (chain, treeVars) ;
-
   int nEntries = chain->GetEntries () ;
-  std::cout << "FOUND " << nEntries << " ENTRIES\n" ;        
+  std::cout << ">>> testCalibAlgos::Found " << nEntries << " entries\n" ;
+
+  std::cout << ">>> testCalibAlgos::TreeBuilding::end <<<" << std::endl;
+
+
 
   //PG loop over entries
   for (int entry = 0 ; entry < nEntries ; ++entry)
     {
       chain->GetEntry (entry) ;
-      if (entry % 1000) std::cout << "reading entry " << entry << "\n" ; 
+      if (entry%1000 == 0) std::cout << "reading entry " << entry << "\n" ;
 
       //PG association between muons and superclusters
       //PG -------------------------------------------
-      
+
       std::vector<ect::association> associations ;
       ect::fillAssocVector (associations, treeVars) ;
       ect::selectOnDR (associations, treeVars, 0.3) ;
 
       //PG loop on associations vector
       for (unsigned int i = 0 ; i < associations.size () ; ++i)
-        { 
+        {
+	  int MUindex = associations.at (i).first ;
+	  int SCindex = associations.at (i).second ;
+
+
+          //PG FIXME find the region ID
+	  int EBNumberOfRegion = findRegion(treeVars, SCindex, EBRegionsTool) ;
+	  if (EBNumberOfRegion == -1) continue ;
+
+
           //PG xtal - energy association map
           std::map<int,double> EBxtlMap ;
-          
-          int SCindex = associations.at (i).second ;
-          
-          //PG FIXME find the region ID
-          
-          double ESubtract = 0. ;
-          
-          //PG loop over crystals in SUPERcluster
-          for (int XTLindex = treeVars.xtalIndexInSuperCluster[SCindex] ; 
-               XTLindex < treeVars.xtalIndexInSuperCluster[SCindex] + 
-                          treeVars.nXtalsInSuperCluster[SCindex] ; 
-               ++XTLindex)
-            {
-              if (1) //PG FIXME if xtals do not fall in the right region
-                {
-                  EBxtlMap[treeVars.xtalHashedIndex[XTLindex]] = 
-                    treeVars.xtalEnergy[XTLindex] ;
-                }
-              else
-                {
-                  ESubtract += treeVars.xtalEnergy[XTLindex] ;
-                }  
-            } //PG loop over crystals in SUPERcluster
+	  double pSubtract ;
+	  EBRegionsTool.fillEBMap (EBxtlMap, pSubtract,
+				   treeVars, SCindex,
+				   EBNumberOfRegion) ;
+
+
+
+
+
 
           //PG get the matrix of crystals
           //PG FIXME as a vector of DetID to be understood
-          
+
           //PG fill the map of energies
           //PG FIXME with the function to be modified
-      
+
           //PG feed the calibration algorithm
 
-          
+
+
+	  EcalCalibBlocks.at (EBNumberOfRegion) -> Fill (EBxtlMap.begin() , EBxtlMap.end (),
+							 treeVars.muonTkLengthInEcalDetail[MUindex],
+							 pSubtract) ;
+
+
+
+
         } //PG loop on associations vector
 
 
     } //PG loop over entries
 
-  //PG extract the solution of the calibration  
+  //PG extract the solution of the calibration
 
-  return 0 ;
+return 0 ;
 
 }
 
@@ -164,41 +191,43 @@ int main (int argc, char** argv)
 //PG ------------------------------------------------------------------
 
 
-//!Fills the map to be sent to the CalibBlock
-//PG may be templated for EB/EE? FIXME
-void fillEBMap (std::map<int,double> & EBxtlMap, //PG effective output
-                double & pSubtract,              //PG effective output
-                EBDetId EBmax,
-                std::vector<EBDetId> & crystals,
-                const EcalRecHitCollection * hitsCollection,
-                std::map<int,int> xtalRegionId, //PG FIXME become a member of a class
-                std::map<int,int> xtalPositionInRegion, //PG FIXME become a member of a class
-                int EBNumberOfRegion)
+
+
+
+
+
+
+
+//!Find the region to which the SC belongs
+int findRegion (EcalCosmicsTreeContent treeVars,
+                int SCindex,
+                EBregionBuilder EBRegionsTool)
 {
-  double dummy = 0. ;
-  pSubtract = 0 ;
-  //PG loop over xtals  
-  for (std::vector<EBDetId>::const_iterator xtalIt = crystals.begin () ;
-       xtalIt != crystals.end () ;
-       ++xtalIt)
+  double dummyEnergy = 0. ;
+  EBDetId dummyDetId ;
+
+  //PG loop over xtals
+  for (int XTLindex = treeVars.xtalIndexInSuperCluster[SCindex] ;
+       XTLindex < treeVars.xtalIndexInSuperCluster[SCindex] +
+	 treeVars.nXtalsInSuperCluster[SCindex] ;
+       ++XTLindex)
     {
-      //PG find the energy corresponding to the DetId
-      EcalRecHitCollection::const_iterator curr_recHit = hitsCollection->find (*xtalIt) ;
-      if (curr_recHit == hitsCollection->end ()) continue;
-      dummy = curr_recHit->energy () ;
+      if (treeVars.xtalEnergy[XTLindex] > dummyEnergy)
+	{
+	  dummyEnergy = treeVars.xtalEnergy[XTLindex] ;
+	  dummyDetId = EBDetId::unhashIndex (treeVars.xtalHashedIndex[XTLindex]) ;
+	}
+    }
 
-//PG FIXME limits and calib coeffs to be set as members of a class,
-//PG FIXME either expected to be applied somewhere else
-//PG FIXME by an official module
-//      if ( dummy > m_minEnergyPerCrystal && dummy < m_maxEnergyPerCrystal)
-//           dummy *= m_recalibMap[det.rawId ()] ;     
-//      else continue;
+  int ieta = dummyDetId.ieta () ;
+  if (ieta < 0) ieta = ieta + 85;
+  else if (ieta > 0) ieta = ieta + 84;
+  int iphi = dummyDetId.iphi () ;
 
-      if (xtalRegionId[xtalIt->rawId ()] == EBNumberOfRegion)
-           EBxtlMap[xtalPositionInRegion[xtalIt->rawId ()]] = dummy ;
-      else pSubtract += dummy;
-    
-    }//PG loop over xtals
+  // std::cout << "ene = " << dummyEnergy << "   ieta/iphi = " << ieta << "," << iphi
+  // 	    << "    RegID = "<< EBRegionsTool.EBRegionId(ieta, iphi) << std::endl;
+
+  return EBRegionsTool.EBRegionId(ieta, iphi);
 }
 
 
