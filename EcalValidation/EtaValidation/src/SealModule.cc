dae45e4d1a37755f94749fdb269974ca9bdc2bc2
malberti <>
2008-09-03 15:32:58 +0000

    add plugin

---
+++ EcalValidation/EtaValidation/src/SealModule.cc
@@ -0,0 +1,11 @@
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "EcalValidation/EtaValidation/interface/MCResonancesAnalyzer.h"
+
+
+
+
+DEFINE_SEAL_MODULE();
+
+DEFINE_ANOTHER_FWK_MODULE(MCResonancesAnalyzer);
