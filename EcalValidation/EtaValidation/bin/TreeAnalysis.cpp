6073ac729ae400651a134248046d5c903701beaa
amassiro <>
2008-09-03 12:26:12 +0000

    cuts selection ready

--- EcalValidation/EtaValidation/bin/TreeAnalysis.cpp
+++ EcalValidation/EtaValidation/bin/TreeAnalysis.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+#include <iostream>
+#include <strstream>
+
 
 //---- CMSSW includes
 #include "DataFormats/DetId/interface/DetId.h"
@@ -16,6 +19,7 @@
 
 //---- root includes
 #include "TH1.h"
+#include "TH2.h"
 #include "TChain.h"
 #include "TFile.h"
 
@@ -23,21 +27,89 @@
 //! main program
 int main (int argc, char** argv)
 {
-  
+ 
+ //---- output File ---- where saving histograms ---- 
+ bool flagOutput = false;
  std::string outputRootName = "outputHistos.root";
  std::string testName = "-o";
  if (argc>2){
   if (argv[1] == testName) {
    outputRootName = argv[2];
+   flagOutput = true;
   }
  }
   
+ //---- input File ---- where finding input parameters ---- 
+  
+ //---- Input Variables ----
+ double R = 0.2;
+ double S4oS9C_Cut = 0.85; //---- S4oS9C_-Cluster cut ----
+ double ptC_Cut = 1.;      //---- pt-Cluster cut ----
+ double ptSum_Cut = 2.;    //---- pt-coppia cut ---- 
+ 
+ double RMax_Cut = 0.2;            //---- RMax_Cut cut ---- isolation ----
+ double DeltaEtaMax_Cut = 0.05;    //---- DeltaEtaMax_Cut ---- isolation ----
+ double et_Cut = 1.;               //---- et_Cut ---- isolation ----
+ double iso_Cut = 0.4;             //---- iso/pt-coppia cut ---- isolation ----
+ 
+ 
+ std::string inputName;
+ bool flagInput = false;
+ std::string testNameInput = "-i";
+ if (argc>2){
+  if (argv[1] ==  testNameInput) {
+   inputName = argv[2];
+   flagInput = true;
+   std::cerr << " Name input file variables = " << inputName << std::endl;
+  }
+ }
+ if (argc>4){
+  if (argv[3] ==  testNameInput) {
+   inputName = argv[4];
+   flagInput = true;
+   std::cerr << " Name input file variables = " << inputName << std::endl;
+  }
+ }
+ if (flagInput){
+  char buffer[1000];
+  ifstream file(inputName.c_str());
+  
+  std::string variableNameR = "R";
+  std::string variableNameS4oS9C_Cut = "S4oS9C_Cut";
+  std::string variableNamePtC_Cut = "ptC_Cut";
+  std::string variableNamePtSum_Cut = "ptSum_Cut";
+  std::string variableNameMax_Cut = "RMax_Cut";
+  std::string variableNameDeltaEtaMax_Cut = "DeltaEtaMax_Cut";
+  std::string variableNameEt_Cut = "et_Cut";
+  std::string variableNameIso_Cut = "iso_Cut";
+  
+  std::cerr << " Reading  " << inputName << " ... " << std::endl;
+  while (!file.eof()){
+   file.getline (&buffer[0],1000);
+   std::istrstream line(buffer);
+   std::string variableName;
+   line >> variableName;
+   double variableValue;
+   line >> variableValue;
+   std::cerr << "  File ->  " << variableName << "  =  " << variableValue << std::endl;
+   if (variableName == variableNameR) R = variableValue;
+   if (variableName == variableNameS4oS9C_Cut) S4oS9C_Cut = variableValue;
+   if (variableName == variableNamePtC_Cut) ptC_Cut = variableValue;
+   if (variableName == variableNamePtSum_Cut) ptSum_Cut = variableValue;
+   if (variableName == variableNameMax_Cut) RMax_Cut = variableValue;
+   if (variableName == variableNameDeltaEtaMax_Cut) DeltaEtaMax_Cut = variableValue;
+   if (variableName == variableNameEt_Cut) et_Cut = variableValue;
+   if (variableName == variableNameIso_Cut) iso_Cut = variableValue;
+  }
+ }
+ 
  std::string testHelp = "--help";
  if (argc==2){
   if (argv[1] == testHelp) {
    std::cout << "Help" << std::endl ;
    std::cout << " --help : display help" << std::endl ;
    std::cout << " -o : output root file name (eg histograms.root)" << std::endl ;
+   std::cout << " -i : input file name with cuts values (eg Cuts.txt)" << std::endl ;
    std::cout << " name of input file : list name of input files ntuples" << std::endl ;     
    exit(1);
   }
@@ -125,7 +197,7 @@ int main (int argc, char** argv)
  chain->SetBranchAddress("pzC_",&pzC_);
  chain->SetBranchAddress("etC_",&etC_);
  chain->SetBranchAddress("HitsC_",&HitsC_);
- chain->SetBranchAddress("HitsEnergyC_",&HitsEnergyC_);
+ //  chain->SetBranchAddress("HitsEnergyC_",&HitsEnergyC_); //---- non è presente per ora ----
  
  //---- ---- Photons from Eta ---- 
  chain->SetBranchAddress("numEta_",&numEta_);
@@ -143,19 +215,40 @@ int main (int argc, char** argv)
  
  
  
- if (argv[1] != testName){
-  chain->Add (argv[1]) ;
+ if (flagOutput && flagInput){
+  chain->Add (argv[5]) ;
  }
- else{
+ if ((flagOutput && !flagInput) || ((!flagOutput && flagInput))){
   chain->Add (argv[3]) ;
  }
+ if (!flagOutput && !flagInput){
+  chain->Add (argv[1]) ;
+ }
+ 
  
 
  //---- Histograms ----
 
  TH1F hInvMassEtaPh("hInvMassEtaPh","Invariant mass Eta from MC Truth",1000, 0., 1.);
- TH1F hInvMassEta("hInvMassEta","Invariant mass Eta from Clusters",1000, 0., 1.);
-   
+ TH1F hInvMassEtaCMCTruth("hInvMassEtaCMCTruth","Invariant mass Eta from Clusters. Match with MC Truth",1000, 0., 1.);
+ TH1F hInvMassEtaC("hInvMassEtaC","Invariant mass Eta from Clusters",1000, 0., 1.);
+ TH2F hInvMassEtaC_numC("hInvMassEtaC_numC","Invariant mass Eta from Clusters versus number of cluster selected",1000, 0., 1.,100, 0., 100.);
+
+  
+ //---- Input Variables Loaded ----
+ std::cerr << std::endl; 
+ std::cerr << " R = " << R << std::endl; 
+ std::cerr << " S4oS9C_Cut = " << S4oS9C_Cut << std::endl; 
+ std::cerr << " ptC_Cut = " << ptC_Cut << std::endl; 
+ std::cerr << " ptSum_Cut = " << ptSum_Cut << std::endl; 
+ std::cerr << " RMax_Cut = " << RMax_Cut << std::endl; 
+ std::cerr << " DeltaEtaMax_Cut = " << DeltaEtaMax_Cut << std::endl; 
+ std::cerr << " et_Cut = " << et_Cut << std::endl; 
+ std::cerr << " iso_Cut = " << iso_Cut << std::endl; 
+
+ double RQ = R*R;
+ 
+ 
  //---- Starting analysis ---- 
  int nEntries = chain->GetEntries () ;
 
@@ -164,26 +257,138 @@ int main (int argc, char** argv)
  for (int entry = 0 ; entry < nEntries ; ++entry)
  {
   chain->GetEntry (entry) ;
-  std::cout << "------> reading entry " << entry << " <------\n" ;
+//   std::cout << "------> reading entry " << entry << " <------\n" ;
   
   //---- loop over etas ----
+  int counterPhotons = 0;
   for (int ii=0; ii<numEta_; ii++){
+   math::XYZTLorentzVector pPh(0,0,0,0);
+   math::XYZTLorentzVector pC(0,0,0,0);
    //---- loop over photons generating an eta ----
-   int counterPhotons = 0;
-   math::XYZTLorentzVector p(0,0,0,0);
    for (int jj=0; jj<numPh_->at(ii); jj++){
-    double px = pxPh_->at(counterPhotons);
-    double py = pyPh_->at(counterPhotons);
-    double pz = pzPh_->at(counterPhotons);
-    double Energy = sqrt(px*px + py*py + pz*pz);
-    math::XYZTLorentzVector p_temp(px,py,pz,Energy);
-    p = p + p_temp;
+    double pxPh = pxPh_->at(counterPhotons);
+    double pyPh = pyPh_->at(counterPhotons);
+    double pzPh = pzPh_->at(counterPhotons);
+    double EnergyPh = sqrt(pxPh*pxPh + pyPh*pyPh + pzPh*pzPh);
+    math::XYZTLorentzVector pPh_temp(pxPh,pyPh,pzPh,EnergyPh);
+    double etaPh = etaPh_->at(counterPhotons);
+    double phiPh = phiPh_->at(counterPhotons);
+    pPh = pPh + pPh_temp;
+   //---- loop over clusters ----
+    for (int kk=0; kk<numC_; kk++){
+     double etaC1 = etaC_->at(kk);
+     double phiC1 = phiC_->at(kk); //---- per ora non funzionava il riempimento :D
+//      double phiC1 = 1;
+     double RC1Q = (etaPh - etaC1) * (etaPh - etaC1) + (phiPh - phiC1) * (phiPh - phiC1); //---- Q at the end stands for ^2 ----
+     if (RC1Q < RQ) { //---- found a cluster near a photon ----
+      double pxC = pxC_->at(kk);
+      double pyC = pyC_->at(kk);
+      double pzC = pzC_->at(kk);
+      double EnergyC = sqrt(pxC*pxC + pyC*pyC + pzC*pzC);
+      math::XYZTLorentzVector pC_temp(pxC,pyC,pzC,EnergyC);
+      pC = pC + pC_temp;
+     }
+    }//---- end loop clusters ----
     counterPhotons++;
-   }
-   double InvMass = p.mag();
-   hInvMassEtaPh.Fill(InvMass);
+   }//---- end loop photons generating an eta ----
+   //---- result in histograms ----
+   double InvMassPh = pPh.mag();
+   hInvMassEtaPh.Fill(InvMassPh);
+   double InvMassC = pC.mag();
+   if (InvMassC != 0) hInvMassEtaCMCTruth.Fill(InvMassC); //---- if zero -> no clusters found ----
   }
+  //---- end loop over etas ----
  }
+ 
+ 
+ 
+ //---- no MC Truth analysis ----
+ 
+ //---- loop entries ----
+ for (int entry = 0 ; entry < nEntries ; ++entry)
+ {
+  chain->GetEntry (entry) ;
+//   std::cout << "------> reading entry " << entry << " <------\n" ;
+  
+  std::vector<int> numC_Selected;
+  math::XYZTLorentzVector pC(0,0,0,0);
+ //---- loop over clusters ----
+  for (int kk=0; kk<numC_; kk++){
+   double etaC1 = etaC_->at(kk);
+   double phiC1 = phiC_->at(kk); //---- per ora non funzionava il riempimento :D
+//    double phiC1 = 1;
+   double pxC = pxC_->at(kk);
+   double pyC = pyC_->at(kk);
+   double pzC = pzC_->at(kk);
+   double ptC = sqrt(pxC*pxC + pyC*pyC);
+   double EnergyC = sqrt(pxC*pxC + pyC*pyC + pzC*pzC);
+   double etaC = etaC_->at(kk);
+   double phiC = phiC_->at(kk);//---- per ora non funzionava il riempimento :D
+//    double phiC = 1;
+   
+   //---- pt-Cluster cut ----
+   if (ptC > ptC_Cut){
+    //---- S4oS9C_-Cluster cut ----
+    if (S4oS9C_->at(kk) > S4oS9C_Cut){
+     math::XYZTLorentzVector pC_temp(pxC,pyC,pzC,EnergyC);
+     pC = pC + pC_temp;
+     numC_Selected.push_back(kk);
+    }
+   }
+  }//---- end loop clusters ----
+  
+  double pxSum = pC.x();
+  double pySum = pC.y();
+  double pzSum = pC.z();
+  double ptSum = sqrt(pxSum*pxSum + pySum*pySum);
+  double EnergySum = sqrt(pxSum*pxSum + pySum*pySum + pzSum*pzSum);
+  double InvMassSum = EnergySum;
+  
+  
+   //---- Isolation cut ----
+  double iso = 0;
+  for (std::vector<int>::iterator numC_Selected_iterator = numC_Selected.begin(); numC_Selected_iterator != numC_Selected.end(); numC_Selected_iterator++){
+   int num = *numC_Selected_iterator;
+   double etaC = etaC_->at(num);
+   double phiC = phiC_->at(num);  //---- per ora non funzionava il riempimento :D    
+//    double phiC = 1;     
+   for(int hh=0; hh<numC_; hh++)
+   {
+    bool flagIsolation = true;
+    for (std::vector<int>::iterator numC_Selected_iterator_in = numC_Selected.begin(); numC_Selected_iterator_in != numC_Selected.end(); numC_Selected_iterator_in++) {
+     if (hh == *numC_Selected_iterator_in) flagIsolation = false; //---- I'm watching an already selected cluster ----
+    }
+    if (!flagIsolation) 
+    {
+     double etaCIn = etaC_->at(hh);
+     double phiCIn = phiC_->at(hh);//---- per ora non funzionava il riempimento :D
+//      double phiCIn = 1;
+     double R = sqrt((etaCIn-etaC)*(etaCIn-etaC) + (phiCIn-phiC)*(phiCIn-phiC));
+     double deta = fabs(etaCIn - etaC);
+     double et = etC_->at(hh);
+     if ( (R < RMax_Cut)  && (deta < DeltaEtaMax_Cut) && (et > et_Cut)  ) iso = iso + et ;
+    }
+   }
+  }
+   //---- iso/pt-coppia cut ----
+  if (iso/ptSum > iso_Cut) {
+    //---- pt-coppia cut ----
+   if (ptSum > ptSum_Cut)
+    if (InvMassSum != 0) {
+    hInvMassEtaC.Fill(InvMassSum); //---- if zero -> no clusters found ----
+    hInvMassEtaC_numC.Fill(InvMassSum,numC_Selected.size());
+    }
+  }
+  
+ } //---- end loop entries ----
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  
  
  //---- save in file ---- 
@@ -191,8 +396,15 @@ int main (int argc, char** argv)
  saving.cd () ;
 
  hInvMassEtaPh.GetXaxis()->SetTitle("Invariant Mass (GeV)");
- hInvMassEtaPh.Write () ;
-
+ hInvMassEtaPh.Write();
+ hInvMassEtaCMCTruth.GetXaxis()->SetTitle("Invariant Mass (GeV)");
+ hInvMassEtaCMCTruth.Write();
+ hInvMassEtaC.GetXaxis()->SetTitle("Invariant Mass (GeV)");
+ hInvMassEtaC.Write();
+ hInvMassEtaC_numC.GetXaxis()->SetTitle("Invariant Mass (GeV)");
+ hInvMassEtaC_numC.GetYaxis()->SetTitle("Number of Cluster selected");
+ hInvMassEtaC_numC.Write();
+ 
  saving.Close ();
  delete chain;
  return 0 ;
