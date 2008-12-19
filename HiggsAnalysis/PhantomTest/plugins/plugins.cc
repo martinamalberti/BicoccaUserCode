339a59683130a18331e3536013d3dd2cbf6ea79d
govoni <>
2008-12-19 13:48:54 +0000

    first commit

---
+++ HiggsAnalysis/PhantomTest/plugins/plugins.cc
@@ -0,0 +1,4 @@
+#include <FWCore/Framework/interface/MakerMacros.h>
+
+#include "HiggsAnalysis/PhantomTest/plugins/VBFTrivialAnalysis.h"
+DEFINE_FWK_MODULE(VBFTrivialAnalysis);
---
+++ HiggsAnalysis/PhantomTest/test/trivialAnalysisTest_cfg.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("trivialAnalysisTest")
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:/gwtera5/users/govoni/Phantom/test/phamom_test3.root')
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+)
+process.MessageLogger = cms.Service("MessageLogger")
+
+process.trivialReader = cms.EDFilter("VBFTrivialAnalysis",
+    eleIDInputTag = cms.InputTag("electronId"),
+    GSFInputTag = cms.InputTag("pixelMatchGsfElectrons"),
+    muInputTag = cms.InputTag("muons"),
+    jetTagsInputTag = cms.InputTag("tagJets","tagJets"),
+    metInputTag = cms.InputTag("met")
+)
+
+process.reading = cms.Path(process.trivialReader)
+
