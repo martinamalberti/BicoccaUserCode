0c472345f2b685e97a0684ccd966f6d7fe190e3f
amassiro <>
2009-02-05 14:08:18 +0000

    MET corrector added

--- HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFPreselectionSequence_cff.py
+++ HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFPreselectionSequence_cff.py
@@ -1,11 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMetCorrector_cfi import *
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonSelector_cfi import *
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonSelectorRef_cfi import *
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonIsolator_cfi import *
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonIsolatorRef_cfi import *
 
 VBFPreselectionSequence = cms.Sequence(
+    correctedMet *
     selectedMuons *
     selectedMuonsRef *
     isolatedMuons *
