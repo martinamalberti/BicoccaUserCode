5baabc7da53a232457b72e7aaa6049d22006be13
amassiro <>
2009-02-05 09:55:36 +0000

    added selected muons

--- HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFPreselectionSequence_cff.py
+++ HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFPreselectionSequence_cff.py
@@ -1,8 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonSelector_cfi import *
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonIsolator_cfi import *
 
 higgsToWW2LeptonsPreselectionSequence = cms.Sequence(
-    isolatedMuons
+    selectedMuons *
+    selectedMuonsRef *
+    isolatedMuons *
+    isolatedMuonsRef 
     )
     
