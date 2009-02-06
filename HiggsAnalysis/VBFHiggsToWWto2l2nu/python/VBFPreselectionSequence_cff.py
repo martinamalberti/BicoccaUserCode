46e6974e1211c46b375490f093ab0ffd209f8988
amassiro <>
2009-02-06 09:17:24 +0000

    Electron ID added

--- HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFPreselectionSequence_cff.py
+++ HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFPreselectionSequence_cff.py
@@ -5,12 +5,18 @@ from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonSelector_cfi import *
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonSelectorRef_cfi import *
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonIsolator_cfi import *
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonIsolatorRef_cfi import *
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFElectronIDSequence_cff import *
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFElectronIDSelector_cfi import *
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFElectronIDSelectorRef_cfi import *
 
 VBFPreselectionSequence = cms.Sequence(
     correctedMet *
     selectedMuons *
     selectedMuonsRef *
     isolatedMuons *
-    isolatedMuonsRef 
+    isolatedMuonsRef *
+    VBFElectronIdSequence *
+    selectedIDElectrons *
+    selectedIDElectronsRef 
     )
     
