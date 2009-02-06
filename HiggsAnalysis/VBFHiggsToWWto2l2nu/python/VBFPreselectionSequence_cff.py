20e445077818c73cb2d815e71b85861ee94910d5
amassiro <>
2009-02-06 10:46:18 +0000

    HET Electron Isolation added

--- HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFPreselectionSequence_cff.py
+++ HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFPreselectionSequence_cff.py
@@ -1,13 +1,31 @@
 import FWCore.ParameterSet.Config as cms
 
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMetCorrector_cfi import *
+
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonSelector_cfi import *
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonSelectorRef_cfi import *
+
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonIsolator_cfi import *
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFMuonIsolatorRef_cfi import *
+
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFElectronIDSequence_cff import *
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFElectronIDSelector_cfi import *
 from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFElectronIDSelectorRef_cfi import *
+
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFEleHETIsolator_cfi import *
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFEleHETIsolatorRef_cfi import *
+
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFEleHETIsolator_cfi import *
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFEleHETIsolatorRef_cfi import *
+
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFEleHIsolator_cfi import *
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFEleHIsolatorRef_cfi import *
+
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFEleEIsolator_cfi import *
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFEleEIsolatorRef_cfi import *
+
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFEleTIsolator_cfi import *
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFEleTIsolatorRef_cfi import *
 
 VBFPreselectionSequence = cms.Sequence(
     correctedMet *
@@ -17,6 +35,16 @@ VBFPreselectionSequence = cms.Sequence(
     isolatedMuonsRef *
     VBFElectronIdSequence *
     selectedIDElectrons *
-    selectedIDElectronsRef 
+    selectedIDElectronsRef *
+    
+    isolatedHETElectrons * 
+    isolatedHETElectronsRef *
+    isolatedHElectrons * 
+    isolatedHElectronsRef *
+    isolatedEElectrons * 
+    isolatedEElectronsRef *
+    isolatedTElectrons * 
+    isolatedTElectronsRef 
+    
     )
     
