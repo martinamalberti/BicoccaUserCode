38db0f1d77b34f7773a4b52fb0b6c9cd12659776
amassiro <>
2009-02-12 09:51:35 +0000

    added min number of leptons filter

--- HiggsAnalysis/VBFHiggsToWWto2l2nu/test/VBFHWW2l2nuSkim_cfg.py
+++ HiggsAnalysis/VBFHiggsToWWto2l2nu/test/VBFHWW2l2nuSkim_cfg.py
@@ -10,7 +10,7 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = 'INFO'
 process.load("HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFHWW2l2nuOutputModule_cff")
 
-
+process.load("HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFSkimmingSequence_cff")
 
 # Source
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
@@ -20,13 +20,14 @@ process.source = cms.Source(
     debugFlag = cms.untracked.bool(True),
     debugVebosity = cms.untracked.uint32(1),
     fileNames = cms.untracked.vstring(
-
-        'rfio:/castor/cern.ch/cms/store/path/filename.root'
+        'file:/tmp/amassiro/VBFHWW2l2nuTest_8.root'
+#         'rfio:/castor/cern.ch/cms/store/path/filename.root'
         )
     )
 
 
-
-
+# Path
+process.p = cms.Path ( process.VBFSkimmingSequence)
+    
 # Output
 process.o = cms.EndPath ( process.VBFHWW2l2nuOutputModule )
