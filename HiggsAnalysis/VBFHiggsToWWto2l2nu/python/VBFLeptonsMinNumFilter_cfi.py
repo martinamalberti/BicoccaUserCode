1987f6d813829c788457f58e9fa9fbb15b9d0c4d
amassiro <>
2009-02-12 15:11:47 +0000

    removed, obsolete

--- HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFLeptonsMinNumFilter_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-leptonsMinNumber = cms.EDFilter("VBFLeptonsMinNumFilter",
-                       GSFInputTag = cms.InputTag("pixelMatchGsfElectrons"),
-                       muInputTag = cms.InputTag("muons"),  
-                       minLeptonsNum = cms.int32(1)
-                      )
