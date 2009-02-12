c28ae3fb6fdfc7a527b6cbf05ad35de846057c58
amassiro <>
2009-02-12 15:09:03 +0000

    removed, now useless

--- HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFSkimmingSequence_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFLeptonsMinNumFilter_cfi import *
-
-VBFSkimmingSequence = cms.Sequence(
-     leptonsMinNumber 
-    )
-    
