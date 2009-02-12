877f36d9f8820674d2dcbef6df58120a5975da3d
amassiro <>
2009-02-12 10:15:25 +0000

    corrected

--- HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFLeptonsMinNumFilter_cfi.py
+++ HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFLeptonsMinNumFilter_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 leptonsMinNumber = cms.EDFilter("VBFLeptonsMinNumFilter",
-                       m_GSFInputTag = cms.InputTag("pixelMatchGsfElectrons"),
-                       m_muInputTag = cms.InputTag("muons"),  
-                       m_minLeptonsNum = cms.int32(1)
+                       GSFInputTag = cms.InputTag("pixelMatchGsfElectrons"),
+                       muInputTag = cms.InputTag("muons"),  
+                       minLeptonsNum = cms.int32(1)
                       )
