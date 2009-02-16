a77c58def661ebc2c9d49d31d02921c1ff6459ba
amassiro <>
2009-02-16 14:53:54 +0000

    now it works

--- HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFCentralJetVetoFilter_cfi.py
+++ HiggsAnalysis/VBFHiggsToWWto2l2nu/python/VBFCentralJetVetoFilter_cfi.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 centralJetVeto = cms.EDFilter(
     "VBFCentralJetVetoFilter",
-     jetInputTag = cms.InputTag("otherJets"),
-     tagJetInputTag = cms.string("taggedJets"),
+     jetInputTag = cms.InputTag("jetTagger:otherJets"),
+     tagJetInputTag = cms.InputTag("jetTagger:taggedJets"),
      jetDeltaEta = cms.double(-0.5), # negative -> smaller region       eta jet + jetDeltaEta
      jetPtMin = cms.double(15.), # GeV
      maxJetsNum = cms.int32(0) # max number of jets
