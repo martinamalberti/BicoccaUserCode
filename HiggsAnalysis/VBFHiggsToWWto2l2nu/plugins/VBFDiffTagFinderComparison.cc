dc8e0fa36b105db63c3c73d0e2307fec982cde1f
amassiro <>
2009-02-17 16:13:51 +0000

    working with tagged jets. New MC tagger

--- HiggsAnalysis/VBFHiggsToWWto2l2nu/plugins/VBFDiffTagFinderComparison.cc
+++ HiggsAnalysis/VBFHiggsToWWto2l2nu/plugins/VBFDiffTagFinderComparison.cc
@@ -1,9 +1,11 @@
-// $Id: VBFDiffTagFinderComparison.cc,v 1.1 2009/02/03 13:50:42 abenagli Exp $
+// $Id: VBFDiffTagFinderComparison.cc,v 1.2 2009/02/16 16:34:53 amassiro Exp $
 #include "DataFormats/Candidate/interface/CandMatchMap.h"
 #include "HiggsAnalysis/VBFHiggsToWWto2l2nu/plugins/VBFDiffTagFinderComparison.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "PhysicsTools/UtilAlgos/interface/TFileService.h"
 
+#include "DataFormats/RecoCandidate/interface/RecoChargedCandidate.h"
+#include "DataFormats/RecoCandidate/interface/RecoChargedCandidateFwd.h"
 
 #include "HiggsAnalysis/VBFHiggsToWWto2l2nu/interface/VBFUtils.h"
 #include <Math/VectorUtil.h>
@@ -11,180 +13,210 @@
 VBFDiffTagFinderComparison::VBFDiffTagFinderComparison (const edm::ParameterSet& iConfig) :
       m_jetInputTag (iConfig.getParameter<edm::InputTag> ("jetInputTag")),
       m_MCtruthInputTag (iConfig.getParameter<edm::InputTag> ("MCtruthInputTag")),
-	  m_algo (iConfig.getParameter<int> ("algoType"))
+      m_algo (iConfig.getParameter<int> ("algoType"))
 
- {}
+{}
 
 
 // --------------------------------------------------------------------
 
 
-VBFDiffTagFinderComparison::~VBFDiffTagFinderComparison ()
-{
+      VBFDiffTagFinderComparison::~VBFDiffTagFinderComparison ()
+      {
  
    // do anything here that needs to be done at desctruction time
    // (e.g. close files, deallocate resources etc.)
-}
+      }
 
 
 // --------------------------------------------------------------------
 
 
-void
-VBFDiffTagFinderComparison::analyze (const edm::Event& iEvent, const edm::EventSetup& iSetup)
-{
-  edm::Handle<edm::HepMCProduct> evtMC;
-  bool sig = true;
-  try {
-  iEvent.getByLabel("source",evtMC);}
-  catch (...) {
-  sig = false;}
+      void
+        VBFDiffTagFinderComparison::analyze (const edm::Event& iEvent, const edm::EventSetup& iSetup)
+      {
+       edm::Handle<edm::HepMCProduct> evtMC;
+       bool sig = true;
+       try {
+        iEvent.getByLabel("source",evtMC);}
+        catch (...) {
+         sig = false;}
   
   //get the jet collection
-  edm::Handle<reco::CaloJetCollection> jetCollectionHandle;
-  iEvent.getByLabel (m_jetInputTag, jetCollectionHandle);
-  if (jetCollectionHandle->size () < 2) return ;
+         edm::Handle<reco::RecoChargedCandidateCollection> jetCollectionHandle;
+         iEvent.getByLabel (m_jetInputTag, jetCollectionHandle);
+         if (jetCollectionHandle->size () < 2) return ;
   //get the gen particles collection
-  edm::Handle<GenParticleCollection> genParticles;
-  iEvent.getByLabel(m_MCtruthInputTag, genParticles);
+        
+        
+         std::pair<const reco::RecoChargedCandidate*,const reco::RecoChargedCandidate*> tagJetCands (&(jetCollectionHandle->at(0)),&(jetCollectionHandle->at(1)));
+         
+         
+         
+         
+         edm::Handle<GenParticleCollection> genParticles;
+         iEvent.getByLabel(m_MCtruthInputTag, genParticles); 
+         
+//          if (m_algo == 0) tagJetCands = 
+//             vbfhww2l::findTagJets (jetCollectionHandle->begin (), jetCollectionHandle->end (), 15.0, 5.0) ;
+//          else if (m_algo == 1) tagJetCands = 
+//             vbfhww2l::findMaxPtJetsPair (jetCollectionHandle->begin (), jetCollectionHandle->end (), 15.0, 5.0) ;
 
-  std::pair<VBFjetIt,VBFjetIt> tagJetCands ;
-  if (m_algo == 0) tagJetCands = 
-    vbfhww2l::findTagJets (jetCollectionHandle->begin (), jetCollectionHandle->end (), 15.0, 5.0) ;
-  else if (m_algo == 1) tagJetCands = 
-    vbfhww2l::findMaxPtJetsPair (jetCollectionHandle->begin (), jetCollectionHandle->end (), 15.0, 5.0) ;
+//          if (tagJetCands.first->p4() == tagJetCands.second->p4()) return;
 
-  if (tagJetCands.first->p4() == tagJetCands.second->p4()) return;
-
-  double threshold = 0.3;
-  double m_ptMax, m_energyMax;
-  int m_purity = 99;
-  double m_deltaRF = 99.9;
-  double m_EratioF = 99.9;
-  double m_deltaRB = 99.9;
-  double m_EratioB = 99.9;
+         
+         
+         double threshold = 0.3;
+         double m_ptMax, m_energyMax;
+         int m_purity = 99;
+         double m_deltaRF = 99.9;
+         double m_EratioF = 99.9;
+         double m_deltaRB = 99.9;
+         double m_EratioB = 99.9;
   
-  double m_deltaEta = fabs (tagJetCands.first->p4().Eta() - tagJetCands.second->p4().Eta() );
-  double m_prdEta = (tagJetCands.first->p4().Eta() * tagJetCands.second->p4().Eta() );
+         double m_deltaEta = fabs (tagJetCands.first->p4().Eta() - tagJetCands.second->p4().Eta() );
+         double m_prdEta = (tagJetCands.first->p4().Eta() * tagJetCands.second->p4().Eta() );
   
-  math::XYZTLorentzVector summedTags = (tagJetCands.first->p4() + tagJetCands.second->p4());  
-  double m_massInv = summedTags.M ();
+         math::XYZTLorentzVector summedTags = (tagJetCands.first->p4() + tagJetCands.second->p4());  
+         double m_massInv = summedTags.M ();
   
-  if (tagJetCands.first->p4().Pt() > tagJetCands.second->p4().Pt()) m_ptMax = tagJetCands.first->p4().Pt();
-  else m_ptMax = tagJetCands.second->p4().Pt();
+         if (tagJetCands.first->p4().Pt() > tagJetCands.second->p4().Pt()) m_ptMax = tagJetCands.first->p4().Pt();
+         else m_ptMax = tagJetCands.second->p4().Pt();
   
-  if (tagJetCands.first->p4().E() > tagJetCands.second->p4().E()) m_energyMax = tagJetCands.first->p4().E();
-  else m_energyMax = tagJetCands.second->p4().E();
+         if (tagJetCands.first->p4().E() > tagJetCands.second->p4().E()) m_energyMax = tagJetCands.first->p4().E();
+         else m_energyMax = tagJetCands.second->p4().E();
   
-  if (sig)
-  {
-	  const HepMC::GenEvent * Evt = evtMC->GetEvent();
-	  int processID = Evt->signal_process_id() ;
-	  if (processID == 123 || processID == 124)
-	  {
-		  findGenParticles (genParticles, *m_genqTagF, *m_genqTagB);
-		  if (tagJetCands.first->p4().Eta() > tagJetCands.second->p4().Eta())
-			{
-			  ///////////////// ************ resolution ***********************************************                                                                       
-			  m_deltaRF = ROOT::Math::VectorUtil::DeltaR (tagJetCands.first->momentum(), m_genqTagF->Vect()) ;
-			  m_EratioF = tagJetCands.first->p4().E()/m_genqTagF->E() ;
+         if (sig)
+         {
+          const HepMC::GenEvent * Evt = evtMC->GetEvent();
+          int processID = Evt->signal_process_id() ;
+          if (processID == 123 || processID == 124)
+          {
+           std::cerr << "**** Trovato il processo giusto ****" << std::endl;
+           if (findGenParticles (genParticles, *m_genqTagF, *m_genqTagB)){
+            if (tagJetCands.first->p4().Eta() > tagJetCands.second->p4().Eta())
+            {
+             std::cerr << "****************** Fatto ******************" << std::endl;
+              ///////////////// ************ resolution ***********************************************                                                                       
+             m_deltaRF = ROOT::Math::VectorUtil::DeltaR (tagJetCands.first->momentum(), m_genqTagF->Vect()) ;
+             m_EratioF = tagJetCands.first->p4().E()/m_genqTagF->E() ;
 
-			  m_deltaRB = ROOT::Math::VectorUtil::DeltaR (tagJetCands.second->momentum(), m_genqTagB->Vect()) ;
-			  m_EratioB = tagJetCands.second->p4().E()/m_genqTagB->E() ;
-			  
-			  ///////////////// ************ purity with inv mass selection ***********************************************
+             m_deltaRB = ROOT::Math::VectorUtil::DeltaR (tagJetCands.second->momentum(), m_genqTagB->Vect()) ;
+             m_EratioB = tagJetCands.second->p4().E()/m_genqTagB->E() ;
+              
+              ///////////////// ************ purity with inv mass selection ***********************************************
 
-			  if (m_deltaRF < threshold && m_deltaRB > threshold) m_purity = 1 ; // 1 matching
-			  else if (m_deltaRB < threshold && m_deltaRF > threshold) m_purity = 1 ; // 1 matching       
-			  else if (m_deltaRB < threshold && m_deltaRF < threshold) m_purity = 2 ; // 2 matching
-			  else if (m_deltaRB > threshold && m_deltaRF > threshold) m_purity = 0 ; // 0 matching
-			}
-		  else if (tagJetCands.first->p4().Eta() < tagJetCands.second->p4().Eta()) 
-			{
-			  ///////////////// ************ resolution ***********************************************                                                                      
+             if (m_deltaRF < threshold && m_deltaRB > threshold) m_purity = 1 ; // 1 matching
+             else if (m_deltaRB < threshold && m_deltaRF > threshold) m_purity = 1 ; // 1 matching       
+             else if (m_deltaRB < threshold && m_deltaRF < threshold) m_purity = 2 ; // 2 matching
+             else if (m_deltaRB > threshold && m_deltaRF > threshold) m_purity = 0 ; // 0 matching
+            }
+            else if (tagJetCands.first->p4().Eta() < tagJetCands.second->p4().Eta()) 
+            {
+              ///////////////// ************ resolution ***********************************************                                                                      
 
-			  m_deltaRB = ROOT::Math::VectorUtil::DeltaR (tagJetCands.first->momentum(), m_genqTagB->Vect()) ;
-			  m_EratioB  = tagJetCands.first->p4().E()/m_genqTagB->E() ;
+             m_deltaRB = ROOT::Math::VectorUtil::DeltaR (tagJetCands.first->momentum(), m_genqTagB->Vect()) ;
+             m_EratioB  = tagJetCands.first->p4().E()/m_genqTagB->E() ;
 
-			  m_deltaRF = ROOT::Math::VectorUtil::DeltaR (tagJetCands.second->momentum(), m_genqTagF->Vect()) ;
-			  m_EratioF = tagJetCands.second->p4().E()/m_genqTagF->E() ;
-			  
-			  ///////////////// ************ purity with inv mass selection ***********************************************
+             m_deltaRF = ROOT::Math::VectorUtil::DeltaR (tagJetCands.second->momentum(), m_genqTagF->Vect()) ;
+             m_EratioF = tagJetCands.second->p4().E()/m_genqTagF->E() ;
+              
+              ///////////////// ************ purity with inv mass selection ***********************************************
 
-			  if (m_deltaRF < threshold && m_deltaRB > threshold) m_purity = 1 ; // 1 matching
-			  else if (m_deltaRB < threshold && m_deltaRF > threshold) m_purity = 1 ; // 1 matching       
-			  else if (m_deltaRB < threshold && m_deltaRF < threshold) m_purity = 2 ; // 2 matching
-			  else if (m_deltaRB > threshold && m_deltaRF > threshold) m_purity = 0 ; // 0 matching
-			}
-
-		}
-	}
+             if (m_deltaRF < threshold && m_deltaRB > threshold) m_purity = 1 ; // 1 matching
+             else if (m_deltaRB < threshold && m_deltaRF > threshold) m_purity = 1 ; // 1 matching       
+             else if (m_deltaRB < threshold && m_deltaRF < threshold) m_purity = 2 ; // 2 matching
+             else if (m_deltaRB > threshold && m_deltaRF > threshold) m_purity = 0 ; // 0 matching
+            }
+           }
+          }
+         }
   
-m_ntuple -> Fill (m_deltaEta, m_prdEta, m_massInv, m_energyMax, m_ptMax, m_purity, m_deltaRF, m_deltaRB, m_EratioF, m_EratioB, m_algo) ;
+         m_ntuple -> Fill (m_deltaEta, m_prdEta, m_massInv, m_energyMax, m_ptMax, m_purity, m_deltaRF, m_deltaRB, m_EratioF, m_EratioB, m_algo) ;
 
-}
+      }
 // --------------------------------------------------------------------
 
 
-void 
-VBFDiffTagFinderComparison::beginJob (const edm::EventSetup&)
-{
-  m_genqTagF = new TLorentzVector (0.0,0.0,0.0,0.0);
-  m_genqTagB = new TLorentzVector (0.0,0.0,0.0,0.0);
+      void 
+        VBFDiffTagFinderComparison::beginJob (const edm::EventSetup&)
+      {
+       m_genqTagF = new TLorentzVector (0.0,0.0,0.0,0.0);
+       m_genqTagB = new TLorentzVector (0.0,0.0,0.0,0.0);
 
-  edm::Service<TFileService> fs ;
+       edm::Service<TFileService> fs ;
 
-  m_ntuple = fs->make <TNtuple> ("ntuple","Some variables","deltaEta:prodEta:mInv:Emax:Ptmax:purity:dR1:dR2:rE1:rE2:algo");
+       m_ntuple = fs->make <TNtuple> ("ntuple","Some variables","deltaEta:prodEta:mInv:Emax:Ptmax:purity:dR1:dR2:rE1:rE2:algo");
 
-}
+      }
 
 
 // --------------------------------------------------------------------
 
 
-void 
-VBFDiffTagFinderComparison::endJob () 
-{
-}
+      void 
+        VBFDiffTagFinderComparison::endJob () 
+      {
+      }
 
 // --------------------------------------------------------------------
 
-void VBFDiffTagFinderComparison::findGenParticles (edm::Handle<GenParticleCollection> &genParticles,
-				     TLorentzVector &m_genqTagF,
-				     TLorentzVector &m_genqTagB)
-{
- for (GenParticleCollection::const_iterator p = genParticles->begin(); p != genParticles->end(); ++ p) 
-    {
-      int mumPDG = p->pdgId();
-      int mumSTATUS = p->status();
+      bool VBFDiffTagFinderComparison::findGenParticles (edm::Handle<GenParticleCollection> &genParticles,
+        TLorentzVector &m_genqTagF,
+        TLorentzVector &m_genqTagB)
+        {
+         bool foundSomething = false;
+         for (GenParticleCollection::const_iterator p = genParticles->begin(); p != genParticles->end(); ++ p) 
+         {
+          int mumPDG = p->pdgId();
+          int mumSTATUS = p->status();
 
-      ///////////////////////////////////////////////// tag quark /////////////////////////////////////////////////
+          ///////////////////////////////////////////////// tag quark /////////////////////////////////////////////////
       //misteriosamente i tag sono i fratelli dell'higgs
       //quindi parto dall'higgs e ne prendo le mamme e quindi riguardo i figli
-      if ((abs(mumPDG)==25) && (mumSTATUS ==3))
-	{
-       	  const Candidate * interact0 = p->mother(0);
-	  if ((interact0->daughter(1)->eta()) > (interact0->daughter(0)->eta())) 
-            {
-	    setMomentum (m_genqTagF, *(interact0->daughter(1)));
-	    setMomentum (m_genqTagB, *(interact0->daughter(0)));
-	    }
-       	  else 
-            {
-	    setMomentum (m_genqTagB, *(interact0->daughter(1)));
-	    setMomentum (m_genqTagF, *(interact0->daughter(0)));
-	    }
-	}
+          if ((abs(mumPDG)==25) && (mumSTATUS ==3))
+          {
+//            if(p->numberOfMothers() < 1){std::cout<<"AAAAA DiffTagFinder nMoth < 1 !!!! "<<std::endl; continue;}
+//            const Candidate * interact0 = p->mother(0);
+//            if(interact0->numberOfDaughters() < 2){std::cout<<"AAAAA DiffTagFinder nDaugh < 2 !!!! "<<std::endl;continue;}
+     
+//            const Candidate * interact0 = p->mother(0);
+     
+           if (((p-1)->eta()) > ((p-2)->eta())) {
+            setMomentum (m_genqTagF, *(p-1));
+            setMomentum (m_genqTagB, *(p-2));
+           }
+           else {
+            setMomentum (m_genqTagB, *(p-1));
+            setMomentum (m_genqTagF, *(p-2));
+           }
+
+           
+           
+           foundSomething = true;
+           
+/*           if ((interact0->daughter(1)->eta()) > (interact0->daughter(0)->eta())) 
+           {
+            setMomentum (m_genqTagF, *(interact0->daughter(1)));
+            setMomentum (m_genqTagB, *(interact0->daughter(0)));
+           }
+           else 
+           {
+            setMomentum (m_genqTagB, *(interact0->daughter(1)));
+            setMomentum (m_genqTagF, *(interact0->daughter(0)));
+           }*/
+          }
         
-    }
-}
+         }
+         return foundSomething;
+        }
 
 // --------------------------------------------------------------------
 
-void VBFDiffTagFinderComparison::setMomentum (TLorentzVector & myvector, const GenParticle & gen)
-{
-  myvector.SetPx (gen.px());
-  myvector.SetPy (gen.py());
-  myvector.SetPz (gen.pz());
-  myvector.SetE (gen.energy());
-}
+        void VBFDiffTagFinderComparison::setMomentum (TLorentzVector & myvector, const GenParticle & gen)
+        {
+         myvector.SetPx (gen.px());
+         myvector.SetPy (gen.py());
+         myvector.SetPz (gen.pz());
+         myvector.SetE (gen.energy());
+        }
--- HiggsAnalysis/VBFHiggsToWWto2l2nu/plugins/VBFMCJetTagger.cc
+++ HiggsAnalysis/VBFHiggsToWWto2l2nu/plugins/VBFMCJetTagger.cc
@@ -54,6 +54,45 @@ VBFMCJetTagger::produce (edm::Event& iEvent, const edm::EventSetup& iEventSetup)
   bool foundDaughther = false;
  for (GenParticleCollection::const_iterator p = genParticles->begin(); p != genParticles->end(); ++ p) 
     {
+     
+// // //      double chargeParticle = p->charge();
+// // //      int numberOfDaughters = p->numberOfDaughters();
+// // //      
+// // //      double chargeDaughters = 0;
+// // //      for (int ii=0; ii<numberOfDaughters;ii++){
+// // //       if (p->daughter(ii)->status() !=2) chargeDaughters += p->daughter(ii)->charge();
+// // //      }
+// // //      
+// // //      if (chargeParticle != chargeDaughters) {
+// // //       bool flagH = false;
+// // //       for (int ii=0; ii<numberOfDaughters;ii++){
+// // //        if (p->daughter(ii)->pdgId() == 25) flagH = true;      
+// // //       }
+// // //       if (flagH){ 
+// // //        std::cerr << "************* urca! la carica non si conserva! ***********" << std::endl;
+// // //        std::cerr << "************* process: ***********" << std::endl;
+// // //        std::cerr << "*** mother " << std::endl <<
+// // //          "      status = " << p->status() << std::endl <<
+// // //          "      pdgId = " << p->pdgId() << std::endl <<
+// // //          "      numberOfDaughters = " << p->numberOfDaughters() << std::endl <<
+// // //          std::endl;
+// // //        std::cerr << "***********************" << std::endl;       
+// // //        for (int ii=0; ii<numberOfDaughters;ii++){
+// // //         std::cerr << "*** daughter " << ii << std::endl <<
+// // //           "      status = " << p->daughter(ii)->status() << std::endl <<
+// // //           "      pdgId = " << p->daughter(ii)->pdgId() << std::endl <<
+// // //           "      numberOfDaughters = " << p->daughter(ii)->numberOfDaughters() << std::endl <<
+// // //           std::endl;
+// // //         std::cerr << "***********************" << std::endl;
+// // //        }
+// // //       }
+// // //      }
+          
+          
+          
+          
+          
+          
         int mumPDG = p->pdgId();
         int mumSTATUS = p->status();
 
@@ -63,11 +102,51 @@ VBFMCJetTagger::produce (edm::Event& iEvent, const edm::EventSetup& iEventSetup)
 	
         if ((abs(mumPDG)==25) && (mumSTATUS ==3))
             {
-	      if(p->numberOfMothers() < 1){std::cout<<"AAAAA nMoth < 1 !!!! "<<std::endl;continue;}
+             
+//              int idx = p - genParticles->begin();
+          
+//                std::cerr << "*** mothers of higgs = " << p->numberOfMothers() << std::endl <<
+//                  "      charge = " << p->mother(0)->charge() << std::endl <<
+//                  "      status = " << p->mother(0)->status() << std::endl <<
+//                  "      pdgId = " << p->mother(0)->pdgId() << std::endl <<
+//                  "      idx = " << idx << std::endl <<
+//                  "      numberOfMothers = " << p->mother(0)->numberOfMothers() << std::endl <<
+//                  "      numberOfDaughters = " << p->mother(0)->numberOfDaughters() << std::endl <<
+//                std::endl;
+             
+//           std::cerr << "*** mothers of mothers of higgs = " << p->mother(0)->numberOfMothers() << std::endl;
+                         
+// 	      if(p->numberOfMothers() < 1){std::cout<<"AAAAA nMoth < 1 !!!! "<<std::endl;continue;}
 	      const Candidate * interact0 = p->mother(0);
-	      if(interact0->numberOfDaughters() < 2){std::cout<<"AAAAA nDaugh < 2 !!!! "<<std::endl;continue;}
+          
+//           std::cerr << "*** daughter of mother of higgs = " << interact0->numberOfDaughters() << std::endl;
+          
+// 	      if(interact0->numberOfDaughters() < 2){std::cout<<"AAAAA nDaugh < 2 !!!! "<<std::endl;continue;}
+//           std::cerr << " trovato!" << std::endl;
+          
 	      foundDaughther = true;
-	      if ((interact0->daughter(1)->eta()) > (interact0->daughter(0)->eta())) {
+                             
+          if (((p-1)->eta()) > ((p-2)->eta())) {
+           setMomentum (MCjetTagF, *((p-1)));
+           setMomentum (MCjetTagB, *((p-2)));
+           chargeF=(p-1)->charge();
+           chargeB=(p-2)->charge();
+           idF=(p-1)->pdgId();
+           idB=(p-2)->pdgId();
+
+          }
+          else {
+           setMomentum (MCjetTagB, *((p-1)));
+           setMomentum (MCjetTagF, *((p-2)));
+           chargeB=(p-1)->charge();
+           chargeF=(p-2)->charge();
+           idB=(p-1)->pdgId();
+           idF=(p-2)->pdgId();
+          }
+
+          
+          
+/*          if ((interact0->daughter(1)->eta()) > (interact0->daughter(0)->eta())) {
 		setMomentum (MCjetTagF, *(interact0->daughter(1)));
 		setMomentum (MCjetTagB, *(interact0->daughter(0)));
 		chargeF=interact0->daughter(1)->charge();
@@ -83,7 +162,7 @@ VBFMCJetTagger::produce (edm::Event& iEvent, const edm::EventSetup& iEventSetup)
 		chargeF=interact0->daughter(0)->charge();
 		idB=interact0->daughter(1)->pdgId();
 		idF=interact0->daughter(0)->pdgId();
-	      }
+	      }*/
 	    }
     }
 
