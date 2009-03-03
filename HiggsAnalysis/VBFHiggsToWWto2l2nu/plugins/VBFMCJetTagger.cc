ef1c06bbe3e5f3e75708cb7d4677c358084438e9
amassiro <>
2009-03-03 15:59:12 +0000

    cleared

--- HiggsAnalysis/VBFHiggsToWWto2l2nu/plugins/VBFMCJetTagger.cc
+++ HiggsAnalysis/VBFHiggsToWWto2l2nu/plugins/VBFMCJetTagger.cc
@@ -55,44 +55,6 @@ VBFMCJetTagger::produce (edm::Event& iEvent, const edm::EventSetup& iEventSetup)
  for (GenParticleCollection::const_iterator p = genParticles->begin(); p != genParticles->end(); ++ p) 
     {
      
-// // //      double chargeParticle = p->charge();
-// // //      int numberOfDaughters = p->numberOfDaughters();
-// // //      
-// // //      double chargeDaughters = 0;
-// // //      for (int ii=0; ii<numberOfDaughters;ii++){
-// // //       if (p->daughter(ii)->status() !=2) chargeDaughters += p->daughter(ii)->charge();
-// // //      }
-// // //      
-// // //      if (chargeParticle != chargeDaughters) {
-// // //       bool flagH = false;
-// // //       for (int ii=0; ii<numberOfDaughters;ii++){
-// // //        if (p->daughter(ii)->pdgId() == 25) flagH = true;      
-// // //       }
-// // //       if (flagH){ 
-// // //        std::cerr << "************* urca! la carica non si conserva! ***********" << std::endl;
-// // //        std::cerr << "************* process: ***********" << std::endl;
-// // //        std::cerr << "*** mother " << std::endl <<
-// // //          "      status = " << p->status() << std::endl <<
-// // //          "      pdgId = " << p->pdgId() << std::endl <<
-// // //          "      numberOfDaughters = " << p->numberOfDaughters() << std::endl <<
-// // //          std::endl;
-// // //        std::cerr << "***********************" << std::endl;       
-// // //        for (int ii=0; ii<numberOfDaughters;ii++){
-// // //         std::cerr << "*** daughter " << ii << std::endl <<
-// // //           "      status = " << p->daughter(ii)->status() << std::endl <<
-// // //           "      pdgId = " << p->daughter(ii)->pdgId() << std::endl <<
-// // //           "      numberOfDaughters = " << p->daughter(ii)->numberOfDaughters() << std::endl <<
-// // //           std::endl;
-// // //         std::cerr << "***********************" << std::endl;
-// // //        }
-// // //       }
-// // //      }
-          
-          
-          
-          
-          
-          
         int mumPDG = p->pdgId();
         int mumSTATUS = p->status();
 
@@ -101,31 +63,9 @@ VBFMCJetTagger::produce (edm::Event& iEvent, const edm::EventSetup& iEventSetup)
         //quindi parto dall'higgs e ne prendo le mamme e quindi riguardo i figli
 	
         if ((abs(mumPDG)==25) && (mumSTATUS ==3))
-            {
+         {
              
-//              int idx = p - genParticles->begin();
-          
-//                std::cerr << "*** mothers of higgs = " << p->numberOfMothers() << std::endl <<
-//                  "      charge = " << p->mother(0)->charge() << std::endl <<
-//                  "      status = " << p->mother(0)->status() << std::endl <<
-//                  "      pdgId = " << p->mother(0)->pdgId() << std::endl <<
-//                  "      idx = " << idx << std::endl <<
-//                  "      numberOfMothers = " << p->mother(0)->numberOfMothers() << std::endl <<
-//                  "      numberOfDaughters = " << p->mother(0)->numberOfDaughters() << std::endl <<
-//                std::endl;
-             
-//           std::cerr << "*** mothers of mothers of higgs = " << p->mother(0)->numberOfMothers() << std::endl;
-                         
-// 	      if(p->numberOfMothers() < 1){std::cout<<"AAAAA nMoth < 1 !!!! "<<std::endl;continue;}
-	      const Candidate * interact0 = p->mother(0);
-          
-//           std::cerr << "*** daughter of mother of higgs = " << interact0->numberOfDaughters() << std::endl;
-          
-// 	      if(interact0->numberOfDaughters() < 2){std::cout<<"AAAAA nDaugh < 2 !!!! "<<std::endl;continue;}
-//           std::cerr << " trovato!" << std::endl;
-          
-	      foundDaughther = true;
-                             
+          foundDaughther = true;
           if (((p-1)->eta()) > ((p-2)->eta())) {
            setMomentum (MCjetTagF, *((p-1)));
            setMomentum (MCjetTagB, *((p-2)));
@@ -143,27 +83,7 @@ VBFMCJetTagger::produce (edm::Event& iEvent, const edm::EventSetup& iEventSetup)
            idB=(p-1)->pdgId();
            idF=(p-2)->pdgId();
           }
-
-          
-          
-/*          if ((interact0->daughter(1)->eta()) > (interact0->daughter(0)->eta())) {
-		setMomentum (MCjetTagF, *(interact0->daughter(1)));
-		setMomentum (MCjetTagB, *(interact0->daughter(0)));
-		chargeF=interact0->daughter(1)->charge();
-		chargeB=interact0->daughter(0)->charge();
-		idF=interact0->daughter(1)->pdgId();
-		idB=interact0->daughter(0)->pdgId();
-
-	      }
-	      else {
-		setMomentum (MCjetTagB, *(interact0->daughter(1)));
-		setMomentum (MCjetTagF, *(interact0->daughter(0)));
-		chargeB=interact0->daughter(1)->charge();
-		chargeF=interact0->daughter(0)->charge();
-		idB=interact0->daughter(1)->pdgId();
-		idF=interact0->daughter(0)->pdgId();
-	      }*/
-	    }
+         }
     }
 
  
