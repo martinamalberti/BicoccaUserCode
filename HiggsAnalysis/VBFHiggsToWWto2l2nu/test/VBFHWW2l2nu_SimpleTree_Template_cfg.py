b9988bea57213274fe3f2558cf8c7ca430b7c319
amassiro <>
2009-03-04 10:19:07 +0000

    first import

---
+++ HiggsAnalysis/VBFHiggsToWWto2l2nu/test/VBFHWW2l2nu_SimpleTree_Template_cfg.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env cmsRun
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("VBFHWW2l2nuSimpleTree")
+
+process.load("Configuration.StandardSequences.Geometry_cff") # for CaloGeometryRecord
+
+process.load('Configuration/StandardSequences/Services_cff')
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
+
+##########
+# Source #
+##########
+
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+
+from HiggsAnalysis.VBFHiggsToWWto2l2nu.Data.SimpleTreeInput_NAME_cfi import *
+
+process.source = cms.Source(
+    "PoolSource",
+    VBFHWW2l2nuSource,
+    skipBadFiles = cms.untracked.bool(True),
+    debugFlag = cms.untracked.bool(True),
+    debugVebosity = cms.untracked.uint32(1)
+   )
+
+    
+###################
+## Jet cleaning  ##
+
+process.load("HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFJetCleaner_cfi")
+process.load("HiggsAnalysis.VBFHiggsToWWto2l2nu.VBFJetCleanerRef_cfi")
+
+## end Jet cleaning  ##
+#######################
+
+   
+##########################
+## SimpleTree sequence ##
+
+process.load("HiggsAnalysis.VBFHiggsToWWto2l2nu.SimpleNtple_cfi")
+
+process.ntpleSequence = cms.Sequence(
+ process.cleanedJets *
+ process.cleanedJetsRef *
+ process.ntpla
+)
+
+## end SimpleTree sequence ##
+#############################
+
+
+
+
+########
+# Path #
+########
+
+process.pathNtuple = cms.Path(process.ntpleSequence)
+
+##########
+# Output #
+##########
+
+process.TFileService = cms.Service("TFileService", 
+                                 fileName = cms.string("VBF_SimpleTree_NAME.root"),
+                                 closeFileFast = cms.untracked.bool(True),
+                                )
+
+###########
+# shedule #
+###########
+
+
+process.schedule = cms.Schedule(process.pathNtuple) 
