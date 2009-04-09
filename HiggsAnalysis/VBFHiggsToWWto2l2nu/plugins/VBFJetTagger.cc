6819880f4c539967675e81503528fec885a30dd7
amassiro <>
2009-04-09 08:42:13 +0000

    update

--- HiggsAnalysis/VBFHiggsToWWto2l2nu/plugins/VBFJetTagger.cc
+++ HiggsAnalysis/VBFHiggsToWWto2l2nu/plugins/VBFJetTagger.cc
@@ -19,12 +19,14 @@ VBFJetTagger::VBFJetTagger (const edm::ParameterSet& iConfig) :
   m_otherJetsName (iConfig.getParameter<std::string> ("otherJetsName")) ,
   m_jetEtaMax (iConfig.getParameter<double> ("jetEtaMax")) ,
   m_jetPtMin (iConfig.getParameter<double> ("jetPtMin")) ,
+  m_jetDEtaMin (iConfig.getUntrackedParameter<double> ("jetDEtaMin")) ,
+  m_jetMjjMin (iConfig.getUntrackedParameter<double> ("jetMjjMin")) ,
   m_gatherConeSize (iConfig.getParameter<double> ("gatherConeSize")) ,
   m_algoType (iConfig.getParameter<int> ("algoType")) 
 {
 //  produces<LorentzVectorCollection> (m_tagJetsName) ;
   produces<reco::RecoChargedCandidateCollection> (m_tagJetsName) ;
-  produces<reco::CaloJetCollection> (m_otherJetsName) ;
+  produces<collection> (m_otherJetsName) ;
   //PG this has maybe to be changed into a RefVector in the future
 }  
 
@@ -43,14 +45,14 @@ VBFJetTagger::~VBFJetTagger ()
 void 
 VBFJetTagger::produce (edm::Event& iEvent, const edm::EventSetup& iEventSetup)
 {
-  edm::Handle<reco::CaloJetCollection> jetCollectionHandle ;
+  edm::Handle<collection> jetCollectionHandle ;
   iEvent.getByLabel (m_jetInputTag, jetCollectionHandle) ;
 
   if (jetCollectionHandle->size () == 0)
     {
       //VT create void collection
       std::auto_ptr<reco::RecoChargedCandidateCollection> tagJets (new reco::RecoChargedCandidateCollection) ;
-      std::auto_ptr<reco::CaloJetCollection> otherJets (new reco::CaloJetCollection) ;
+      std::auto_ptr<collection> otherJets (new collection) ;
 
       //VT insert the collection into the event
       iEvent.put (tagJets, m_tagJetsName) ;
@@ -63,7 +65,7 @@ VBFJetTagger::produce (edm::Event& iEvent, const edm::EventSetup& iEventSetup)
     {
       //VT create void collection                                                                                                                                     
       std::auto_ptr<reco::RecoChargedCandidateCollection> tagJets (new reco::RecoChargedCandidateCollection) ;
-      std::auto_ptr<reco::CaloJetCollection> otherJets (new reco::CaloJetCollection) ;
+      std::auto_ptr<collection> otherJets (new collection) ;
 
       //VT insert the collection into the event
       otherJets -> push_back(*(jetCollectionHandle->begin()));
@@ -76,13 +78,15 @@ VBFJetTagger::produce (edm::Event& iEvent, const edm::EventSetup& iEventSetup)
   //PG get the jet tags
   std::pair<vbfhww2l::VBFjetIt,vbfhww2l::VBFjetIt> tagJetCands ;
 
-  if (m_algoType == 0) tagJetCands = vbfhww2l::findTagJets (jetCollectionHandle->begin (), jetCollectionHandle->end (), m_jetPtMin, m_jetEtaMax) ;
+  if (m_algoType == 0) tagJetCands = vbfhww2l::findTagJets (jetCollectionHandle->begin (), jetCollectionHandle->end (), m_jetPtMin, m_jetEtaMax, m_jetDEtaMin, m_jetMjjMin) ;
 
   else if (m_algoType == 1) 
     {
-      //reco::CaloJetCollection TheJets = *jetCollectionHandle;  
-      //tagJetCands = findMaxPtJetsPair (TheJets, m_jetPtMin, m_jetEtaMax) ; 
-      tagJetCands = vbfhww2l::findMaxPtJetsPair (jetCollectionHandle->begin (), jetCollectionHandle->end (), m_jetPtMin, m_jetEtaMax) ;
+     tagJetCands = vbfhww2l::findMaxPtJetsPair (jetCollectionHandle->begin (), jetCollectionHandle->end (), m_jetPtMin, m_jetEtaMax, m_jetDEtaMin, m_jetMjjMin) ;
+    }
+    else if (m_algoType == 2) 
+    {
+     tagJetCands = vbfhww2l::findMaxNNJetsPair_PtOrdered (jetCollectionHandle->begin (), jetCollectionHandle->end (), m_jetPtMin, m_jetEtaMax, m_jetDEtaMin, m_jetMjjMin) ;
     }
 
   //PG build the new jets
@@ -93,8 +97,8 @@ VBFJetTagger::produce (edm::Event& iEvent, const edm::EventSetup& iEventSetup)
   math::XYZVector secondDir = tagJetCands.second->momentum () ;
   //PG look for other jets in cones around the found ones and add them to the "leading"
   
-  std::auto_ptr<reco::CaloJetCollection> otherJets 
-      (new reco::CaloJetCollection) ;
+  std::auto_ptr<collection> otherJets 
+      (new collection) ;
 
   //PG loop over the jets collection
   for (vbfhww2l::VBFjetIt jetIt = jetCollectionHandle->begin () ; 
