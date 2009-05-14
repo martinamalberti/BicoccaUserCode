655664678f683122918575e0f507c45af69cf891
amassiro <>
2009-05-14 10:23:03 +0000

    new

--- HiggsAnalysis/VBFHiggsToWWto2l2nu/python/Data/SimpleTreeInput_H160_WW_2l_cfi.py
+++ HiggsAnalysis/VBFHiggsToWWto2l2nu/python/Data/SimpleTreeInput_H160_WW_2l_cfi.py
@@ -1,32 +1,41 @@
 import FWCore.ParameterSet.Config as cms
 
+
+# VBFHWW2l2nuSource = cms.PSet(
+# fileNames = cms.untracked.vstring(
+
+readFiles = cms.untracked.vstring()
+
 VBFHWW2l2nuSource = cms.PSet(
-    fileNames = cms.untracked.vstring('rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_1.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_10.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_11.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_12.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_13.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_14.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_15.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_16.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_17.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_18.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_19.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_2.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_20.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_21.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_22.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_23.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_24.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_25.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_26.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_27.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_3.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_4.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_5.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_6.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_7.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_8.root',
-'rfio:/castor/cern.ch/user/a/amassiro/VBF/Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_9.root'  
- )
-    )
+  fileNames = readFiles
+)
+
+readFiles.extend( [
+
+
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_1.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_10.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_11.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_12.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_13.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_14.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_15.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_16.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_17.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_18.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_19.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_2.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_20.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_21.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_22.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_23.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_24.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_26.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_27.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_3.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_4.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_5.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_6.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_7.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_8.root',
+'rfio:/castor/cern.ch/user/a/amassiro/VBF/090420_Everything_Skimmed_4Cluster/H160_WW_2l/VBFHWW2l2nuTest_9.root'  ] )
