452881a5db2261fbb033782982c786f8b8128284
abenagli <>
2009-09-25 12:38:14 +0000

    *** empty log message ***

--- HiggsAnalysis/VBFHiggsToVV/python/LepMinNumFilter_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-minLepSelector = cms.EDFilter("LepMinNumFilter",
-  electronsTag = cms.InputTag ("pixelMatchGsfElectrons") ,
-  muonsTag = cms.InputTag ("muons") ,
-  ptThres = cms.double (9.) , # GeV
-  nLep = cms.int32 (2)
-)
