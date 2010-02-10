b648ce85ff1c2fd0556e1e45598861b4ba47a813
maborgia <>
2010-02-10 13:17:04 +0000

    *** empty log message ***

---
+++ HiggsAnalysis/littleH/bin/Macros/Draw_InvMass_samesign.C
@@ -0,0 +1,152 @@
+void Draw_InvMass () {
+
+  gROOT->ProcessLine(".x Style.C") ;
+
+  const int n_signal = 2 ;
+  const int n_bkg_SM = 5 ;
+  const int n_colors = 5 ;
+  const int n_steps = 5 ;
+
+  std::vector<TString> v_signal_names;
+  std::vector<TString> v_bkg_SM_names;
+
+  std::vector<TString> v_leg_signal_names;
+  std::vector<TString> v_leg_bkg_SM_names;
+
+  std::TString s_histo_input ;
+  std::TString s_plot_name ;
+
+  v_signal_names.push_back("Signal_8GeV_samesign");
+  v_signal_names.push_back("Signal_10GeV_samesign") ; 
+  
+  v_leg_signal_names.push_back("a_8GeV") ; 
+  v_leg_signal_names.push_back("a_10GeV") ;
+  
+  v_bkg_SM_names.push_back("MuMuBkg_samesign") ;
+  v_bkg_SM_names.push_back("Upsilon1S_samesign") ;
+  v_bkg_SM_names.push_back("Upsilon2S_samesign") ;
+  v_bkg_SM_names.push_back("Upsilon3S_samesign") ;
+  v_bkg_SM_names.push_back("MuMuBkg");
+  
+  v_leg_bkg_SM_names.push_back("Comb #mu#mu samesign") ;
+  v_leg_bkg_SM_names.push_back("Comb #mu#mu + #Upsilon_{1S}") ; 
+  v_leg_bkg_SM_names.push_back("Comb #mu#mu + #Upsilon_{1S} + #Upsilon_{2S}") ;
+  v_leg_bkg_SM_names.push_back("Comb #mu#mu + #Upsilon_{1S} + #Upsilon_{2S} + #Upsilon_{3S}") ; 
+  v_leg_bkg_SM_names.push_back("Comb #mu#mu") ;
+  
+  double IntLum = 500. ; // In Inverse pb
+  TString Lum = "500";
+  TString tanB = "10";
+  
+  double signal_Xsec [n_signal] = { 1893, 4537.5};// <= values for tanBeta = 10,  values for tanBeta = 3 => {94.64 151.25}  ; 
+  double bkg_SM_Xsec [n_bkg_SM] = { 85000000., 14000., 5600., 1600., 85000000. } ;
+  
+  double signal_filter_eff [n_signal] = { 1., 1. } ;
+  double bkg_SM_filter_eff [n_bkg_SM] = { 1., 1., 1., 1., 1. } ;
+
+  double signal_genMCevts [n_signal] = { 9600., 9600. } ; 
+  double bkg_SM_genMCevts [n_bkg_SM] = {  800000., 10000., 10000., 10000., 800000. } ;
+
+  double signal_weight [n_signal] ;
+  double bkg_SM_weight [n_bkg_SM] ;
+
+  int signal_colors [n_colors] = { 1, 1} ; 
+  int bkg_SM_colors [n_colors] = { 2, 4, 5, 6, 7 } ; 
+
+  int signal_markers [n_signal] = { 25, 25 } ; /*25,*/
+  int bkg_SM_markers [n_bkg_SM] = { 20, 21, 22, 23, 24 } ;
+
+  TH1F * h_signal_2mumass [n_signal] ;
+  TH1F * h_bkg_SM_2mumass [n_signal][n_bkg_SM] ;
+
+  TCanvas * can [n_signal] ;
+  TLegend *leg [n_signal] ;
+  
+  THStack *a_2muMass [n_signal] ;
+
+  //Cycle on Signals:  
+  for ( int i = 0; i < n_signal; i++ )
+  {
+    TFile *signal_fin = new TFile( "../" + v_signal_names[i] + ".root", "READ");
+    a_2muMass[i] = new THStack("invMass" + v_signal_names[i],"Invariant Mass");
+    
+    can[i] = new TCanvas ;
+//     can[i] ->SetLogy () ;
+/*    can[i]->SetGridx () ;
+    can[i]->SetGridy () ;*/
+    can[i]->cd(0) ;
+
+    leg [i] = new TLegend(0.21,0.64,0.56,0.92,NULL,"brNDC");
+    leg [i] ->SetFillColor(kWhite);
+    leg [i] ->SetTextSize(0.025);
+    leg [i] ->SetBorderSize(0);
+
+  //SM BKG
+    for ( int iSM = 0; iSM < n_bkg_SM; iSM++ )	
+      {
+	TFile *bkg_fin = new TFile ( "../" + v_bkg_SM_names[iSM] + ".root", "READ" ) ;
+	bkg_SM_weight [iSM] = IntLum * bkg_SM_Xsec [iSM] * bkg_SM_filter_eff [iSM] / bkg_SM_genMCevts [iSM] ;
+	
+	std::cout << bkg_SM_weight [iSM] << " " + v_bkg_SM_names [iSM] + " " << bkg_SM_weight [iSM] * bkg_SM_genMCevts [iSM] << "\n \n"  << std::endl;
+	
+	s_histo_input = "hInvMass" ;
+	h_bkg_SM_2mumass [i][iSM] = (TH1F *) bkg_fin-> Get (s_histo_input) ;
+	
+	//if (i==0) 
+	  ( *h_bkg_SM_2mumass [i][iSM] ) = bkg_SM_weight [iSM] * ( *h_bkg_SM_2mumass [i][iSM] ) ; //rescaling with the event weight 
+	
+	h_bkg_SM_2mumass [i][iSM] ->SetMarkerStyle(bkg_SM_markers[iSM]) ;
+	h_bkg_SM_2mumass [i][iSM]->SetMarkerColor ( bkg_SM_colors[iSM] ) ;
+	h_bkg_SM_2mumass [i][iSM]->SetMarkerSize(1.6) ;
+	h_bkg_SM_2mumass [i][iSM]->SetLineColor ( bkg_SM_colors[iSM] ) ;
+	h_bkg_SM_2mumass [i][iSM]->SetLineWidth(2.5) ;
+	h_bkg_SM_2mumass [i][iSM] -> SetFillColor (bkg_SM_colors [iSM]) ;
+	h_bkg_SM_2mumass [i][iSM] -> SetFillStyle (3018) ;
+	
+	leg[i]->AddEntry( h_bkg_SM_2mumass [i][iSM] ,v_leg_bkg_SM_names [iSM] ,"f");
+	
+	a_2muMass[i]->Add(h_bkg_SM_2mumass [i][iSM]);
+
+      }      
+    //SIGNALS
+    signal_weight [i] = IntLum * signal_Xsec [i] * signal_filter_eff [i] / signal_genMCevts [i] ;
+    
+    std::cout << signal_weight [i] << " " + v_signal_names [i] + " " << signal_weight [i] * signal_genMCevts [i] << "\n \n"  << std::endl;
+    
+    s_histo_input = "hInvMass";
+    h_signal_2mumass [i] = (TH1F *) signal_fin-> Get (s_histo_input) ;
+  
+    ( *h_signal_2mumass [i] ) = signal_weight [i] * ( *h_signal_2mumass [i] ) ; //rescaling with the event weight 
+
+    h_signal_2mumass [i] ->SetMarkerStyle(signal_markers[i]) ;
+    h_signal_2mumass [i]->SetMarkerColor ( signal_colors[i] ) ;
+    h_signal_2mumass [i]->SetMarkerSize(1.6) ;
+    h_signal_2mumass [i]->SetLineColor ( signal_colors[i] ) ;
+    h_signal_2mumass [i]->SetLineWidth(2.5) ;
+    h_signal_2mumass [i]->SetFillColor (signal_colors [i]) ;
+
+    leg[i]->AddEntry( h_signal_2mumass [i] ,v_leg_signal_names [i] ,"f");
+
+    a_2muMass[i]->Add(h_signal_2mumass [i]);
+       
+    a_2muMass[i] ->Draw() ;
+
+    a_2muMass[i] ->GetYaxis()->SetTitle("d#sigma/dm (pb/100 MeV/c^{2})") ;
+    a_2muMass[i] ->GetXaxis()->SetTitle("m_{#mu#mu} (GeV/c^{2})") ;
+    a_2muMass[i] ->GetXaxis()->SetRangeUser(0,500) ;
+
+    leg [i] ->Draw() ;
+    std::cout << Lum << endl;
+    s_plot_name = "~/www/little" + v_leg_signal_names [i] +"_" + Lum + "pb_" + "tanB"+ tanB + "samesign.gif" ;
+
+    can [i] -> Print ( s_plot_name, "gif" ) ;
+    s_plot_name = "~/www/little" + v_leg_signal_names [i] + "_" + Lum + "pb_" + "tanB"+ tanB +"samesign.eps" ;
+    can [i] -> Print ( s_plot_name, "eps" ) ;
+
+  }
+
+
+  
+
+
+}
---
+++ HiggsAnalysis/littleH/bin/Macros/Kolmogorov.C
@@ -0,0 +1,96 @@
+void Kolmogorov () {
+
+  gROOT->ProcessLine(".x Style.C") ;
+
+  const int n_bkg_SM = 2 ;
+  const int n_colors = 2 ;
+
+  std::vector<TString> v_bkg_SM_names;
+
+  std::vector<TString> v_leg_bkg_SM_names;
+
+  std::TString s_histo_input ;
+  std::TString s_plot_name ;
+
+  v_bkg_SM_names.push_back("MuMuBkg_samesign") ;
+  v_bkg_SM_names.push_back("MuMuBkg");
+  
+  v_leg_bkg_SM_names.push_back("Comb #mu#mu samesign") ;
+  v_leg_bkg_SM_names.push_back("Comb #mu#mu") ;
+  
+  double IntLum = 500. ; // In Inverse pb
+  TString Lum = "500";
+  TString tanB = "10";
+  
+  int bkg_SM_colors [n_colors] = { 2, 4 } ; 
+
+  int bkg_SM_markers [n_bkg_SM] = { 20, 21} ;
+
+  TH1F * h_bkg_SM_2mumass [n_bkg_SM] ;
+
+  TCanvas *can = new TCanvas("can", "can",4,48,800,658);;
+  TLegend *leg = new TLegend(0.2123116,0.6412698,0.5565327,0.9174603,NULL,"brNDC");;
+  
+  THStack *a_2muMass = new THStack(); ;
+
+  TH1F *samesign = new TH1F();
+  TH1F *oppositesign = new TH1F();
+
+  //SM BKG
+  for ( int iSM = 0; iSM < n_bkg_SM; iSM++ )	
+    {
+      TFile *bkg_fin = new TFile ( "../" + v_bkg_SM_names[iSM] + ".root", "READ" ) ;
+            
+      s_histo_input = "hInvMass" ;
+      h_bkg_SM_2mumass [iSM] = (TH1F *) bkg_fin-> Get (s_histo_input) ;
+      h_bkg_SM_2mumass[iSM]->Rebin(2);
+
+      ( *h_bkg_SM_2mumass [iSM] ) =  ( *h_bkg_SM_2mumass[iSM] )*(1/h_bkg_SM_2mumass[iSM]->GetEntries()) ; //rescaling with the event weight 
+
+      h_bkg_SM_2mumass [iSM] ->SetMarkerStyle(bkg_SM_markers[iSM]) ;
+      h_bkg_SM_2mumass [iSM]->SetMarkerColor ( bkg_SM_colors[iSM] ) ;
+      h_bkg_SM_2mumass [iSM]->SetMarkerSize(1.6) ;
+      h_bkg_SM_2mumass [iSM]->SetLineColor ( bkg_SM_colors[iSM] ) ;
+      h_bkg_SM_2mumass [iSM]->SetLineWidth(2.5) ;
+      h_bkg_SM_2mumass [iSM] -> SetFillColor (bkg_SM_colors [iSM]) ;
+      h_bkg_SM_2mumass [iSM] -> SetFillStyle (3018) ;
+
+      if(iSM==0){
+	samesign = *h_bkg_SM_2mumass [iSM];
+	samesign->Sumw2();          
+      }
+      if(iSM==1){
+	oppositesign = *h_bkg_SM_2mumass [iSM]; 
+	oppositesign->Sumw2();
+      }
+      leg->AddEntry( h_bkg_SM_2mumass [iSM] ,v_leg_bkg_SM_names [iSM] ,"f");
+      
+      a_2muMass->Add(h_bkg_SM_2mumass [iSM]);
+
+      //     if(iSM==1)
+      //	std::cout << h_bkg_SM_2mumass [iSM]->KolmogorovTest(h_bkg_SM_2mumass [iSM-1]) << std::endl;
+    }      
+    a_2muMass ->Draw("nostack") ;
+
+    a_2muMass ->GetYaxis()->SetTitle("d#sigma/dm (pb/100 MeV/c^{2})") ;
+    a_2muMass ->GetXaxis()->SetTitle("m_{#mu#mu} (GeV/c^{2})") ;
+    a_2muMass ->GetXaxis()->SetRangeUser(0,500) ;
+    leg->Draw() ;
+    
+    samesign->Sumw2();     
+    oppositesign->Sumw2();  
+    std::cout << samesign->KolmogorovTest(oppositesign, "UO") << std::endl;
+//     std::cout << Lum << endl;
+//     s_plot_name = "~/www/little" + "_" + Lum + "pb_" + "tanB"+ tanB + "samesign.gif" ;
+
+//     can-> Print ( s_plot_name, "gif" ) ;
+//     s_plot_name = "~/www/little" + "_" + Lum + "pb_" + "tanB"+ tanB +"samesign.eps" ;
+//     can-> Print ( s_plot_name, "eps" ) ;
+
+}
+
+
+  
+
+
+
