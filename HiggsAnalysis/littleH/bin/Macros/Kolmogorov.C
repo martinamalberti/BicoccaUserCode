f5b2fff16b7ee4b66efb8e4f64f5a1315ff46b76
maborgia <>
2010-02-23 17:49:56 +0000

    *** empty log message ***

--- HiggsAnalysis/littleH/bin/Macros/Kolmogorov.C
+++ HiggsAnalysis/littleH/bin/Macros/Kolmogorov.C
@@ -12,11 +12,11 @@ void Kolmogorov () {
   std::TString s_histo_input ;
   std::TString s_plot_name ;
 
-  v_bkg_SM_names.push_back("MuMuBkg_samesign") ;
-  v_bkg_SM_names.push_back("MuMuBkg");
+  v_bkg_SM_names.push_back("MuMuBkg_newCuts_LOOSE") ;
+  v_bkg_SM_names.push_back("MuMuBkg_newCuts");
   
-  v_leg_bkg_SM_names.push_back("Comb #mu#mu samesign") ;
-  v_leg_bkg_SM_names.push_back("Comb #mu#mu") ;
+  v_leg_bkg_SM_names.push_back("Comb #mu#mu HLT_loose") ;
+  v_leg_bkg_SM_names.push_back("Comb #mu#mu HLT") ;
   
   double IntLum = 500. ; // In Inverse pb
   TString Lum = "500";
@@ -43,10 +43,17 @@ void Kolmogorov () {
             
     s_histo_input = "hInvMass" ;
     h_bkg_SM_2mumass [iSM] = (TH1F *) bkg_fin-> Get (s_histo_input) ;
+ 
+   if(iSM==0)
+      h_bkg_SM_2mumass [iSM]->Scale(0.135);
+
     h_bkg_SM_2mumass[iSM]->Rebin(2);
     h_bkg_SM_2mumass [iSM]->Sumw2(); 
-    ( *h_bkg_SM_2mumass [iSM] ) =  ( *h_bkg_SM_2mumass[iSM] )*(1/h_bkg_SM_2mumass[iSM]->GetEntries()) ; //rescaling with the event weight 
 
+      //  ( *h_bkg_SM_2mumass [iSM] ) =  ( *h_bkg_SM_2mumass[iSM] )*0.267;
+    //     ( *h_bkg_SM_2mumass [iSM] ) =  ( *h_bkg_SM_2mumass[iSM] )*(1/h_bkg_SM_2mumass[iSM]->GetEntries()) ; //rescaling with the event weight 
+
+    cout << "entries " << h_bkg_SM_2mumass[iSM]->GetEntries() << endl;
     h_bkg_SM_2mumass [iSM] ->SetMarkerStyle(bkg_SM_markers[iSM]) ;
     h_bkg_SM_2mumass [iSM]->SetMarkerColor ( bkg_SM_colors[iSM] ) ;
     h_bkg_SM_2mumass [iSM]->SetMarkerSize(1.6) ;
@@ -79,13 +86,12 @@ void Kolmogorov () {
     
 //     samesign->Sumw2();     
 //     oppositesign->Sumw2();  
-//     if(iSM==1)
-//       std::cout << h_bkg_SM_2mumass [iSM]->KolmogorovTest(h_bkg_SM_2mumass [iSM-1], "UO") << std::endl;
-//     std::cout << Lum << endl;
-//     s_plot_name = "~/www/little" + "_" + Lum + "pb_" + "tanB"+ tanB + "samesign.gif" ;
-
+  if(iSM==1)
+    std::cout << h_bkg_SM_2mumass [iSM]->KolmogorovTest(h_bkg_SM_2mumass [iSM-1], "UO") << std::endl;
+//   std::cout << Lum << endl;
+//   s_plot_name = "~/www/little" + "_" + Lum + "pb_" + "tanB"+ tanB + "HLT.gif" ;
 //     can-> Print ( s_plot_name, "gif" ) ;
-//     s_plot_name = "~/www/little" + "_" + Lum + "pb_" + "tanB"+ tanB +"samesign.eps" ;
-//     can-> Print ( s_plot_name, "eps" ) ;
+//   s_plot_name = "~/www/little" + "_" + Lum + "pb_" + "tanB"+ tanB +"samesign.eps" ;
+//   can-> Print ( s_plot_name, "eps" ) ;
 
 }
