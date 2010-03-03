5f9a4226627264e9baa1de00c4defa76ca26e697
abenagli <>
2010-03-03 11:55:47 +0000

    updates

--- PhysicsTools/NtuplePackage/scripts/setup.sh
+++ PhysicsTools/NtuplePackage/scripts/setup.sh
@@ -5,4 +5,6 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/lib
 export PATH=${PATH}:${THISDIR}/bin
 export PATH=${PATH}:${THISDIR}/scripts
 
+export NTUPLEPKGINCLUDE=${THISDIR}/interface
 export NTUPLEPKGLIB=${THISDIR}/lib
+
