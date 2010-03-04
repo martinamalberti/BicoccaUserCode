005108205bf1e742a7c413452281b4252061f086
amassiro <>
2010-03-04 16:55:47 +0000

    description added

--- PhysicsTools/NtuplePackage/test/testH2Factory.cpp
+++ PhysicsTools/NtuplePackage/test/testH2Factory.cpp
@@ -6,8 +6,11 @@
 int main ()
 {
   h2Factory factory ("test2.root", true) ;
+  ///---- histogram with 6 bins, in the range [-3,3] (x), 4 bins, in the range [-2,2] (y),  total number of steps to fill = 2
   factory.add_h2 ("test", "jet tags eta prod", 6, -3., 3., 4, -2., 2., 2) ;
+  ///---- fill the step 0 of the histogram "test" with the pair (1.,1.) = (x,y)
   factory.Fill ("test", 0, 1., 1.) ;
+  ///---- fill the step 1 of the histogram "test" with the pair (2.,1.) = (x,y)
   factory.Fill ("test", 1, 2., 1.) ;
   int colors[2] = {9,98} ;
   factory.applyToAll (setH2Colors (std::vector<int> (colors, colors + 2))) ;
--- PhysicsTools/NtuplePackage/test/testHFactory.cpp
+++ PhysicsTools/NtuplePackage/test/testHFactory.cpp
@@ -6,8 +6,12 @@
 int main ()
 {
   hFactory factory ("test.root", true) ;
+  ///---- histogram with 200 bins, in the range [-10,10], total number of steps to fill = 3
   factory.add_h1 ("test", "jet tags eta prod", 200, -10, 10, 3) ;
+  ///---- fill the step 0 of the histogram "test" with the value 1.
   factory.Fill ("test", 0, 1.) ;
-  int colors[3] = {92,98,0} ;
+  ///---- fill the step 1 of the histogram "test" with the value 2.5
+  factory.Fill ("test", 1, 2.5) ;
+  int colors[3] = {92,98,97} ;
   factory.applyToAll (setH1Colors (std::vector<int> (colors, colors + 3))) ;
 }
