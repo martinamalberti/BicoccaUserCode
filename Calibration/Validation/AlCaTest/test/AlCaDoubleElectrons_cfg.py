dcdeaf56003a0a4626dc2440300b65d152cc97fa
amassiro <>
2010-03-23 11:09:12 +0000

    automaitc scripts

--- Calibration/Validation/AlCaTest/test/AlCaDoubleElectrons_cfg.py
+++ Calibration/Validation/AlCaTest/test/AlCaDoubleElectrons_cfg.py
@@ -20,8 +20,9 @@ process.source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNa
 
 readFiles.extend( [
 #	'/store/relval/CMSSW_3_1_0_pre8/RelValZEE/ALCARECO/STARTUP_31X_StreamALCARECOEcalCalElectron_v1/0006/C2CF2E13-DB4D-DE11-A5B1-001D09F2437B.root'
-     '/store/relval/CMSSW_3_5_4/RelValZEE/ALCARECO/START3X_V24_EcalCalElectron-v1/0004/2AB6B40D-2D2C-DF11-814B-002618943948.root'	
-        ] )
+        NAMESAMPLE
+         
+          ] )
 
 
 secFiles.extend( (
@@ -32,7 +33,7 @@ process.alCaElectronsTest = cms.EDAnalyzer("AlCaElectronsTest",
   electronLabel=cms.InputTag("gsfElectrons"),
   alcaBarrelHitCollection=cms.InputTag("alCaIsolatedElectrons:alcaBarrelHits"),
   alcaEndcapHitCollection=cms.InputTag("alCaIsolatedElectrons:alcaEndcapHits"),
-  HistOutFile = cms.untracked.string("validazione_Zee_310_pre8.root"),
+  HistOutFile = cms.untracked.string("NAMEOUTPUT"),
   EcalIsoTag = cms.InputTag("egammaEcalIsolation"),
   ElePtTkIsoTag = cms.InputTag("egammaElectronSqPtTkIsolation"),
   EleTkIsoTag = cms.InputTag("egammaElectronTkIsolation"),
