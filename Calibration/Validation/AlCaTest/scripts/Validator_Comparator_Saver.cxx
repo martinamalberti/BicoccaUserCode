92e31c7b4f661df58d7497fb4c69d69f68999ea1
amassiro <>
2010-03-26 07:50:27 +0000

    error on plot

--- Calibration/Validation/AlCaTest/scripts/Validator_Comparator_Saver.cxx
+++ Calibration/Validation/AlCaTest/scripts/Validator_Comparator_Saver.cxx
@@ -1,19 +1,25 @@
 {
 ///==== Validator ====
-  
-  TH1F etaDist ("etaDist","etaDist",200,-3,3);
-  TH1F energy ("energy","energy",300,0,800);
-  TH1F ESCoP ("ESCoP","ESCoP",200,0,20);
-  TH1F HitsOe ("HitsOe","HitsOe",200,0,1.02);
-  m_tree->Draw("eta>>etaDist");
-  m_tree->Draw("energy>>energy");
-  m_tree->Draw("ESCoP>>ESCoP");
-  m_tree->Draw("recHits/energy>>HitsOe");
-  TFile fileNew ("_NEWFILE_","RECREATE");
-  etaDist.Write();
-  energy.Write();
-  ESCoP.Write();
-  HitsOe.Write();
+
+ TH1F etaDist ("etaDist","etaDist",200,-3,3);
+ TH1F energy ("energy","energy",300,0,800);
+ TH1F ESCoP ("ESCoP","ESCoP",200,0,20);
+ TH1F HitsOe ("HitsOe","HitsOe",200,0,1.02);
+
+ etaDist.Sumw2();
+ energy.Sumw2();
+ ESCoP.Sumw2();
+ HitsOe.Sumw2();
+
+ m_tree->Draw("eta>>etaDist");
+ m_tree->Draw("energy>>energy");
+ m_tree->Draw("ESCoP>>ESCoP");
+ m_tree->Draw("recHits/energy>>HitsOe");
+ TFile fileNew ("_NEWFILE_","RECREATE");
+ etaDist.Write();
+ energy.Write();
+ ESCoP.Write();
+ HitsOe.Write();
 
 ///==== Comparator ====
 
@@ -25,6 +31,15 @@
  TH1F ESCoPOld = (TH1F) fileOld.Get("ESCoP");
  TH1F HitsOeOld = (TH1F) fileOld.Get("HitsOe");
 
+ etaDist.Scale(1. / etaDist.GetEntries());
+ energy.Scale(1. / energy.GetEntries());
+ ESCoP.Scale(1. / ESCoP.GetEntries());
+ HitsOe.Scale(1. / HitsOe.GetEntries());
+
+ etaDistOld.Scale(1. / etaDistOld.GetEntries());
+ energyOld.Scale(1. / energyOld.GetEntries());
+ ESCoPOld.Scale(1. / ESCoPOld.GetEntries());
+ HitsOeOld.Scale(1. / HitsOeOld.GetEntries());
 
  etaDistOld.SetTitle("_OLDRELEASE_");
  energyOld.SetTitle("_OLDRELEASE_");
@@ -56,36 +71,36 @@
 
  TCanvas ccetaDist("ccetaDist","ccetaDist");
  etaDist.GetXaxis().SetTitle("#eta");
- etaDist.DrawNormalized();
- etaDistOld.DrawNormalized("same");
+ etaDist.Draw("E");
+ etaDistOld.Draw("same");
  ccetaDist.SetLogy();
  legMiddle->Draw();
 //  ccetaDist.BuildLegend();
-ccetaDist.SetGrid();
+ ccetaDist.SetGrid();
  ccetaDist.SaveAs("~/public/html/AlCaRecoValidation/RelVal__NEWRELEASE_/SAMPLE_etaDist.gif");
 
  TCanvas ccenergy("ccenergy","ccenergy");
  energy.GetXaxis()->SetTitle("Energy [GeV]");
- energy.DrawNormalized();
- energyOld.DrawNormalized("same");
+ energy.Draw("E");
+ energyOld.Draw("same");
  ccenergy.SetLogy();
  ccenergy.BuildLegend();
-ccenergy.SetGrid();
+ ccenergy.SetGrid();
  ccenergy.SaveAs("~/public/html/AlCaRecoValidation/RelVal__NEWRELEASE_/SAMPLE_energy_dist.gif");
 
  TCanvas ccESCoP("ccESCoP","ccESCoP");
  ESCoP.GetXaxis()->SetTitle("E_{SC}/p");
- ESCoP.DrawNormalized();
- ESCoPOld.DrawNormalized("same");
+ ESCoP.Draw("E");
+ ESCoPOld.Draw("same");
  ccESCoP.SetLogy();
  ccESCoP.BuildLegend();
-ccESCoP.SetGrid();
+ ccESCoP.SetGrid();
  ccESCoP.SaveAs("~/public/html/AlCaRecoValidation/RelVal__NEWRELEASE_/SAMPLE_ESCoP_dist.gif");
 
  TCanvas ccHitsOe("ccHitsOe","ccHitsOe");
  HitsOe.GetXaxis()->SetTitle("#frac{#sum Hits}{energy}");
- HitsOe.DrawNormalized();
- HitsOeOld.DrawNormalized("same");
+ HitsOe.Draw("E");
+ HitsOeOld.Draw("same");
  ccHitsOe.SetLogy();
 //  ccHitsOe.BuildLegend();
  leg->Draw();
