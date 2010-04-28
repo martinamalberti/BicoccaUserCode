a7f87b1e5d0d9125e03220eb03a16e08011b7607
amassiro <>
2010-04-28 07:38:08 +0000

    first import

---
+++ HiggsAnalysis/VBFHiggsToVV/test/test_VBFPreselection_Data_cfg.py
@@ -0,0 +1,180 @@
+import FWCore.ParameterSet.Config as cms
+
+
+process = cms.Process("TEST")
+
+# --- ============== --- --- --- --- --- --- --- --- --- --- --- --- 
+# --- GENERAL THINGS --- --- --- --- --- --- --- --- --- --- --- --- 
+# --- ============== --- --- --- --- --- --- --- --- --- --- --- --- 
+
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True))
+
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load('Configuration.StandardSequences.Services_cff')
+process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.globaltag = "MC_31X_V5::All"
+process.GlobalTag.globaltag = "START3X_V26::All"
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load('Configuration/StandardSequences/Services_cff')
+process.load("FWCore.MessageService.MessageLogger_cfi")
+#process.MessageLogger = cms.Service(
+#    "MessageLogger",
+#    #destinations = cms.untracked.vstring('detailedInfo'),
+#    #detailedInfo = cms.untracked.PSet(
+#    #FwkReport = cms.untracked.PSet(
+#    #    reportEvery = cms.untracked.int32(5000),
+#    #    limit = cms.untracked.int32(10000000)
+#    #    )
+#    #),
+#    default = cms.untracked.PSet(
+#        FwkReport = cms.untracked.PSet(
+#            reportEvery = cms.untracked.int32(1000),
+#            limit = cms.untracked.int32(10000000)
+#            )
+#        )
+#    )
+
+
+process.TFileService = cms.Service(
+    "TFileService",
+    fileName = cms.string("./VBFPreselection.root"),
+    closeFileFast = cms.untracked.bool(True)
+    )
+
+
+
+
+
+
+# --- ====== --- --- --- --- --- --- --- --- --- --- --- 
+# --- INPUT  --- --- --- --- --- --- --- --- --- --- --- 
+# --- ====== --- --- --- --- --- --- --- --- --- --- --- 
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
+
+process.source = cms.Source(
+    "PoolSource",
+    #debugFlag = cms.untracked.bool(True),
+    #debugVebosity = cms.untracked.uint32(1),
+    duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
+    fileNames = cms.untracked.vstring(
+        #'file:/data/NTUPLES/VBF/qqHWW_lnujj/H200/CMSSWfile_10.root',
+        #'file:/gwtera5/users/data/NTUPLES/VBF/CMSSWfile_3_1_X.root'
+         'file:/tmp/amassiro/0C5ECFC1-C445-DF11-8133-E0CB4E19F99B.root'
+        )
+    )
+
+
+
+
+
+
+# --- ====== --- --- --- --- --- --- --- --- --- --- --- 
+# --- PRESELECTION  --- --- --- --- --- --- --- --- --- --- --- 
+# --- ====== --- --- --- --- --- --- --- --- --- --- ---
+
+process.load("HiggsAnalysis.VBFHiggsToVV.VBFPreselection_all_cff")
+process.MCDecayModeFilterAll.eventType = cms.untracked.int32(1)
+
+
+# --- ====== --- --- --- --- --- --- --- --- --- --- ---
+# sisCone5CaloJets
+# --- ====== --- --- --- --- --- --- --- --- --- --- ---
+
+#process.load("HiggsAnalysis.VBFHiggsToVV.VBFPreselection_sisCone5CaloJets_cff")
+#process.VBFNtupleSisCone5CaloJets.eventType = cms.untracked.int32(1)
+#process.VBFNtupleSisCone5CaloJets.saveMCHiggs = cms.untracked.bool(False)
+#process.VBFNtupleSisCone5CaloJets.saveMCHiggsDecay = cms.untracked.bool(False)
+
+#process.load("HiggsAnalysis.VBFHiggsToVV.VBFPreselection_L2L3SisCone5CaloJets_cff")
+#process.VBFNtupleL2L3SisCone5CaloJets.eventType = cms.untracked.int32(1)
+#process.VBFNtupleL2L3SisCone5CaloJets.saveMCHiggs = cms.untracked.bool(False)
+#process.VBFNtupleL2L3SisCone5CaloJets.saveMCHiggsDecay = cms.untracked.bool(False)
+
+
+# --- ====== --- --- --- --- --- --- --- --- --- --- ---
+# antikt5CaloJets
+# --- ====== --- --- --- --- --- --- --- --- --- --- ---
+
+process.load("HiggsAnalysis.VBFHiggsToVV.VBFPreselection_antikt5CaloJets_cff")
+process.VBFNtupleAntikt5CaloJets.eventType = cms.untracked.int32(1)
+process.VBFNtupleAntikt5CaloJets.saveMCHiggs = cms.untracked.bool(False)
+process.VBFNtupleAntikt5CaloJets.saveMCHiggsDecay = cms.untracked.bool(False)
+
+process.load("HiggsAnalysis.VBFHiggsToVV.VBFPreselection_L2L3Antikt5CaloJets_cff")
+process.VBFNtupleL2L3Antikt5CaloJets.eventType = cms.untracked.int32(1)
+process.VBFNtupleL2L3Antikt5CaloJets.saveMCHiggs = cms.untracked.bool(False)
+process.VBFNtupleL2L3Antikt5CaloJets.saveMCHiggsDecay = cms.untracked.bool(False)
+
+
+# --- ====== --- --- --- --- --- --- --- --- --- --- ---
+# antikt5PFJets
+# --- ====== --- --- --- --- --- --- --- --- --- --- ---
+
+process.load("HiggsAnalysis.VBFHiggsToVV.VBFPreselection_antikt5PFJets_cff")
+process.VBFNtupleAntikt5PFJets.eventType = cms.untracked.int32(1)
+process.VBFNtupleAntikt5PFJets.saveMCHiggs = cms.untracked.bool(False)
+process.VBFNtupleAntikt5PFJets.saveMCHiggsDecay = cms.untracked.bool(False)
+
+process.load("HiggsAnalysis.VBFHiggsToVV.VBFPreselection_L2L3Antikt5PFJets_cff")
+process.VBFNtupleL2L3Antikt5PFJets.eventType = cms.untracked.int32(1)
+process.VBFNtupleL2L3Antikt5PFJets.saveMCHiggs = cms.untracked.bool(False)
+process.VBFNtupleL2L3Antikt5PFJets.saveMCHiggsDecay = cms.untracked.bool(False)
+
+
+
+
+
+
+# --- ====== --- --- --- --- --- --- --- --- --- --- ---
+# --- OUTPUT --- --- --- --- --- --- --- --- --- --- ---
+# --- ====== --- --- --- --- --- --- --- --- --- --- ---
+
+process.load("Configuration.EventContent.EventContent_cff")
+
+process.out = cms.OutputModule(
+    "PoolOutputModule",
+    process.AODSIMEventContent,
+    verbose = cms.untracked.bool(True),
+    fileName = cms.untracked.string('VBFPreselection_backgroundTEST.root'),
+    )
+
+process.out.outputCommands.extend(cms.untracked.vstring('keep *_*_*_TEST'))
+
+
+
+
+
+
+# --- ====== --- --- --- --- --- --- --- --- --- --- --- 
+# --- PATHS  --- --- --- --- --- --- --- --- --- --- --- 
+# --- ====== --- --- --- --- --- --- --- --- --- --- --- 
+                               
+#process.p1 = cms.Path(
+#    process.VBFPreselectionSequenceSisCone5CaloJets
+#    )
+
+#process.p2 = cms.Path(
+#    process.VBFPreselectionSequenceL2L3SisCone5CaloJets
+#    )
+
+process.p3 = cms.Path(
+    process.VBFPreselectionSequenceAntikt5CaloJets
+    )
+
+process.p4 = cms.Path(
+    process.VBFPreselectionSequenceL2L3Antikt5CaloJets
+    )
+
+process.p5 = cms.Path(
+    process.VBFPreselectionSequenceAntikt5PFJets
+    )
+
+process.p6 = cms.Path(
+    process.VBFPreselectionSequenceL2L3Antikt5PFJets
+    )
+
+#process.o = cms.EndPath( process.out )
