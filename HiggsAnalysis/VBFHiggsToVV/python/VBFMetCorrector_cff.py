f492ba7c6678f8c2b3daf589ec22e28c40155145
amassiro <>
2010-04-29 13:23:04 +0000

    corrected

--- HiggsAnalysis/VBFHiggsToVV/python/VBFMetCorrector_cff.py
+++ HiggsAnalysis/VBFHiggsToVV/python/VBFMetCorrector_cff.py
@@ -13,15 +13,15 @@ from JetMETCorrections.Type1MET.MetType1Corrections_cff import *
 
 
 
-#VBFType1SisCone5CaloMet = metJESCorIC5CaloJet.clone(
-    #inputUncorJetsLabel = "sisCone5CaloJets",
-    #corrector = "L2L3JetCorrectorSC5Calo",
-    #inputUncorMetLabel = "corMetGlobalMuons"
-    #)
+VBFType1SisCone5CaloMet = metJESCorIC5CaloJet.clone(
+    inputUncorJetsLabel = "sisCone5CaloJets",
+    corrector = "L2L3JetCorrectorSC5Calo",
+    inputUncorMetLabel = "corMetGlobalMuons"
+    )
 
-#VBFType1SisCone5CaloMetSequence = cms.Sequence(
-    #VBFType1SisCone5CaloMet
-    #)
+VBFType1SisCone5CaloMetSequence = cms.Sequence(
+    VBFType1SisCone5CaloMet
+    )
 
 
 
@@ -30,7 +30,7 @@ from JetMETCorrections.Type1MET.MetType1Corrections_cff import *
 
 VBFType1Antikt5CaloMet = metJESCorIC5CaloJet.clone(
     inputUncorJetsLabel = "ak5CaloJets",
-    corrector = "ak5CaloL2L3",
+    corrector = "L2L3JetCorrectorAK5Calo",
     inputUncorMetLabel = "corMetGlobalMuons"
     )
 
