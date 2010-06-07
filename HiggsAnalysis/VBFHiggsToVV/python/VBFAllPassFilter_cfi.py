62229f7a5b9b633b1d1b93bb64d55f4bfaa98393
abenagli <>
2010-06-07 15:56:36 +0000

    first import

---
+++ HiggsAnalysis/VBFHiggsToVV/python/VBFAllPassFilter_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+
+
+VBFAllPassFilter = cms.EDFilter(
+    "VBFAllPassFilter"
+    )
+
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
