da6beb16a3a02cbf3aec6e5861f00668534dcc15
amassiro <>
2010-08-31 14:16:03 +0000

    new Ntuple

--- WAnalysis/WenuAnalysis/plugins/plugins.cc
+++ WAnalysis/WenuAnalysis/plugins/plugins.cc
@@ -1,8 +1,11 @@
 #include <FWCore/Framework/interface/MakerMacros.h>
+
 #include "WAnalysis/WenuAnalysis/plugins/WenuTree.h"
-
-
 DEFINE_FWK_MODULE(WenuTree);
+
+
+#include "WAnalysis/WenuAnalysis/plugins/SimpleNtple.h"
+DEFINE_FWK_MODULE(SimpleNtple);
 
 
 
