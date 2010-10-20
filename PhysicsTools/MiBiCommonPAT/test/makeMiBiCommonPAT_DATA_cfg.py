ac85f2f83e2bc69e7aa45078ca84b4973920e5e1
dimatteo <>
2010-10-20 14:35:23 +0000

    first import

---
+++ PhysicsTools/MiBiCommonPAT/test/makeMiBiCommonPAT_DATA_cfg.py
@@ -0,0 +1,24 @@
+import FWCore.ParameterSet.Config as cms
+
+from PhysicsTools.MiBiCommonPAT.makeMiBiCommonPAT_cff import *
+
+
+
+process = cms.Process("MiBiCommonPAT")
+
+
+
+makeMiBiCommonPAT(process, GlobalTag="GR10_P_V10::All", MC=False, Filter=True, SavePAT=True)
+
+process.source.fileNames = cms.untracked.vstring(
+  'file:/tmp/dimatteo/D8700674-35CB-DF11-8C60-0024E85A3ED8.root'
+  #'file:/gwpool/users/amassiro/VBF/Releases/CMSSW_3_8_3/src/PhysicsTools/MiBiCommonPAT/669A4128-43D0-DF11-AE93-001A92810ACE.root'  
+
+)
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False))
+
+process.out.fileName = cms.untracked.string("file:/tmp/dimatteo/MiBiCommonPAT.root")
+
+
