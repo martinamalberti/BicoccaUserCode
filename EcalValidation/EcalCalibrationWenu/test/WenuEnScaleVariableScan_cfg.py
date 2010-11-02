1741d7ab21da5d44fed7976b93ca5262270b8e0d
amassiro <>
2010-11-02 17:20:04 +0000

    first import

---
+++ EcalValidation/EcalCalibrationWenu/test/WenuEnScaleVariableScan_cfg.py
@@ -0,0 +1,50 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TEST")
+
+process.inputTree = cms.PSet(
+  treeNameDATA = cms.string("ntupleEcalCalibration/myTree"),
+  inputFileDATA = cms.string("test/EcalCalibration.root"),
+  treeNameMC = cms.string("myTree"),
+  inputFileMC = cms.string("test/Wenu_WSUMMED_4analysis.root")
+)
+
+process.outputTree = cms.PSet(
+  outputFile = cms.string("out.root")
+)
+
+
+process.options = cms.PSet(
+  EEorEB = cms.int32(5),
+    
+  numToyMC = cms.int32(10),
+  MinScan = cms.double(-0.1),
+  MaxScan = cms.double(0.05),
+  iNoSteps = cms.int32(200),
+
+  MinScanRange = cms.double(-0.04),
+  MaxScanRange = cms.double(0.01),
+
+  cut = cms.string("1"),
+  
+  minET = cms.double(20.),
+
+  minBINS      = cms.double(0.8),
+  maxBINS      = cms.double(2.2),
+  numBINS      = cms.int32(30)
+)
+
+
+
+
+process.ScanOptions = cms.PSet(
+   ScanVariables = cms.vstring("abs(eleFBrem)"),
+   ScanVariablesId = cms.vstring("abseleFBrem"),
+   minValueabseleFBrem = cms.vdouble(0.0,0.5),
+   maxValueabseleFBrem = cms.vdouble(0.5,1.0),
+   variablesName = cms.vstring("ESC/p"),
+   variablesNameId = cms.vstring("ESCp")
+)
+
+
+
