4bd7123fbcdf20f1f22267055f07b54b9f10c068
dimatteo <>
2011-01-05 10:29:38 +0000

    updates due to PosCalc issues with 3_10_X

--- Validation/EcalValidation/interface/EcalValidation.h
+++ Validation/EcalValidation/interface/EcalValidation.h
@@ -91,13 +91,6 @@ class EcalValidation : public edm::EDAnalyzer {
 
 	 // for Pi0
 	 PositionCalc posCalculator_ ;
-	 bool ParameterLogWeighted_;
-	 double ParameterX0_;
-	 double ParameterT0_barl_;
-	 double ParameterT0_endc_;
-	 double ParameterT0_endcPresh_;
-	 double ParameterW0_;
-	 
 	 
 	 double clusSeedThr_;
 	 double clusSeedThr_EE_;
