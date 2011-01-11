29e8b1094fff95969f6109b13249f5fc6451ed59
abenagli <>
2011-01-11 11:40:52 +0000

    added Dec22ReReco file

--- PhysicsTools/MiBiCommonPAT/test/makeMiBiCommonNT_DATA_cfg.py
+++ PhysicsTools/MiBiCommonPAT/test/makeMiBiCommonNT_DATA_cfg.py
@@ -8,7 +8,10 @@ process = cms.Process("MiBiCommonNT")
 makeMiBiCommonNT(process, GlobalTag="GR_R_38X_V15::All", HLT='', MC=False, MCType='Other')
   
 process.source.fileNames = cms.untracked.vstring(
-  'file:/grid_mnt/data__DATA/data.polcms/cms/abenagli/DATA_38X_RECO.root'
+  # Dec22ReReco
+  '/store/data/Run2010B/Electron/RECO/Dec22ReReco_v1/0000/049351B8-E20D-E011-A421-00304867C1BC.root'
+  
+  #'file:/grid_mnt/data__DATA/data.polcms/cms/abenagli/DATA_38X_RECO.root'
   #'file:/tmp/dimatteo/D8700674-35CB-DF11-8C60-0024E85A3ED8.root'
   #'/store/data/Run2010B/Electron/RECO/PromptReco-v2/000/147/451/FA4BFB16-7AD3-DF11-AD7C-0019B9F72BFF.root',
   #'/store/data/Run2010B/Electron/RECO/PromptReco-v2/000/147/451/F023A43C-83D3-DF11-9EC7-001D09F24047.root',
