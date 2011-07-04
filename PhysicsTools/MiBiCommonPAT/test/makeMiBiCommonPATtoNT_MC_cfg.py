cdd9a0c5c1e26a29e0db1ae54d3a8c57b3e31af9
amassiro <>
2011-07-04 10:18:48 +0000

    update sample

--- PhysicsTools/MiBiCommonPAT/test/makeMiBiCommonPATtoNT_MC_cfg.py
+++ PhysicsTools/MiBiCommonPAT/test/makeMiBiCommonPATtoNT_MC_cfg.py
@@ -9,7 +9,8 @@ process = cms.Process("MiBiCommonNT")
 makeMiBiCommonNT(process, GlobalTag="START41_V0::All", HLT='', MC=True, MCType='Other')
   
 process.source.fileNames = cms.untracked.vstring(
-   'file:/data2/amassiro/CMSSWRoot/Latinos_ggH160toWW_to2L2Nu_PAT/ggToH160toWWto2L2Nu_7_1_QhP.root'
+#   'file:/data2/amassiro/CMSSWRoot/Latinos_ggH160toWW_to2L2Nu_PAT/ggToH160toWWto2L2Nu_7_1_QhP.root'
+   'file:/data2/amassiro/CMSSWRoot/Latinos_DY_PAT/DYtoMuMu_31_1_RfT.root'
 )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
