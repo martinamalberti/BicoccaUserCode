dfc0633312c2ed269eca323340d5bc0323bda9d1
amassiro <>
2011-08-11 13:12:25 +0000

    update

--- PhysicsTools/MiBiCommonPAT/test/makeMiBiCommonPATtoNT_DATA_cfg.py
+++ PhysicsTools/MiBiCommonPAT/test/makeMiBiCommonPATtoNT_DATA_cfg.py
@@ -7,11 +7,12 @@ process = cms.Process("MiBiCommonNT")
 # the MiBiNT: Choose the MC type!!!
 # Possible choices: TTBar, Higgs, HiggsWW, HiggsGammaGamma, ZW, Other
 #  makeMiBiCommonNT(process, GlobalTag="GR_R_42_V12::All", HLT='', MC=False, MCType='Other')
-#  makeMiBiCommonNT(process, GlobalTag="GR_R_42_V14::All", HLT='', MC=False, MCType='Other')
-makeMiBiCommonNT(process, GlobalTag="GR_R_41_V0::All", HLT='', MC=False, MCType='Other')  
+makeMiBiCommonNT(process, GlobalTag="GR_R_42_V19::All", HLT='', MC=False, MCType='Other')
+#  makeMiBiCommonNT(process, GlobalTag="GR_R_41_V0::All", HLT='', MC=False, MCType='Other')  
 
 process.source.fileNames = cms.untracked.vstring(
-   'file:/data2/amassiro/CMSSWRoot/Latinos_DATA_PAT/DoubleMuon2011A_27_1_GPa.root'
+#   'file:/data2/amassiro/CMSSWRoot/Latinos_DATA_PAT/DoubleMuon2011A_27_1_GPa.root'
+   'file:/data2/amassiro/CMSSWRoot/Latinos_DATA_PAT/DoubleElectron2011AReRecoMay10_80_1_40B.root'
 )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
