337f72c93b4274c193ef30ec30dac4e8ce95b692
abenagli <>
2011-10-21 22:44:39 +0000

    updates

--- Calibration/EcalCalibNtuple/cfg/selections_WZAnalysis_cfg.py
+++ Calibration/EcalCalibNtuple/cfg/selections_WZAnalysis_cfg.py
@@ -9,18 +9,13 @@ process.Input = cms.PSet(
     inputFileList = cms.string("cfg/list.txt"),
     
     #jsonFileName = cms.string("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions10/7TeV/Reprocessing/Cert_136033-149442_7TeV_Dec22ReReco_Collisions10_JSON_v4.txt")
-    #jsonFileName = cms.string("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-163869_7TeV_PromptReco_Collisions11_JSON.txt")
-    jsonFileName = cms.string("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Reprocessing/Cert_160404-163869_7TeV_May10ReReco_Collisions11_JSON.txt")
     #jsonFileName = cms.string("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/DCSOnly/json_DCSONLY.txt")
+    jsonFileName = cms.string("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-178078_7TeV_PromptReco_Collisions11_JSON.txt")
     )
 
 process.Output = cms.PSet(
-    outputRootFilePath = cms.string("/data1/abenagli/NTUPLES/CALIBRATION/Run2011A/WZAnalysis/"),
-
-    #outputRootFileName = cms.string("WZAnalysis_EGElectron_Run2010AB-Apr21ReReco-v1")
-    #outputRootFileName = cms.string("WZAnalysis_SingleElectron_Run2011A-May10ReReco-v1_160329-163869")
-    outputRootFileName = cms.string("WZAnalysis_SingleElectron_Run2011A-WElectron-May10ReReco-v1_2011Laser_newAPDPNRatiosRef")
-    #outputRootFileName = cms.string("WZAnalysis_SingleElectron_Run2011A-PromptReco-v4_165071-165980")
+    outputRootFilePath = cms.string("/data2/calibrator/NTUPLES/Run2011A/WZAnalysis/"),
+    outputRootFileName = cms.string("WZAnalysis_SingleElectron_Run2011A-05Jul2011ReReco-ECAL-v1_new")
     )
 
 process.Options = cms.PSet(
