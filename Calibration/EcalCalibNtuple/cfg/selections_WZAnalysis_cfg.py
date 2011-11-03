4381c6d13f0454fd167337e1211470755aa88275
rgerosa <>
2011-11-03 16:11:34 +0000

    Update

--- Calibration/EcalCalibNtuple/cfg/selections_WZAnalysis_cfg.py
+++ Calibration/EcalCalibNtuple/cfg/selections_WZAnalysis_cfg.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 
 
-process = cms.Process("WZAnalysis")
+process = cms.Process("new_WZAnalysis")
 
 process.Input = cms.PSet(
 
@@ -11,7 +11,7 @@ process.Input = cms.PSet(
     #jsonFileName = cms.string("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions10/7TeV/Reprocessing/Cert_136033-149442_7TeV_Dec22ReReco_Collisions10_JSON_v4.txt")
     #jsonFileName = cms.string("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/DCSOnly/json_DCSONLY.txt")
     jsonFileName = cms.string("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-178078_7TeV_PromptReco_Collisions11_JSON.txt"),
-    inputFlag_isCalib=true
+    inputFlag_isCalib=cms.bool(True)
     )
 
 process.Output = cms.PSet(
@@ -21,8 +21,8 @@ process.Output = cms.PSet(
 
 process.Options = cms.PSet(
     entryMIN = cms.int32(0),
-    entryMAX = cms.int32(-1),
-    entryMODULO = cms.int32(100000),
+    entryMAX = cms.int32(100000),
+    entryMODULO = cms.int32(1000),
     dataFlag = cms.int32(1),
     jsonFlag = cms.int32(1),
     crossSection = cms.double(1.),
