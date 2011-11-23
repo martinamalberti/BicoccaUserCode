fa21239874566e8758ef75e45bb2a17c3bb1f523
abenagli <>
2011-11-23 09:35:11 +0000

    first import

---
+++ PhysicsTools/MiBiCommonPAT/python/SimpleNtuple_noPAT_cfi.py
@@ -0,0 +1,97 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.GeometryDB_cff import *
+
+SimpleNtuple_noPAT = cms.EDAnalyzer(
+    "SimpleNtuple_noPAT",
+    
+    #-------------------
+    #### Input Tags ####
+
+    MCPileupTag = cms.InputTag ("addPileupInfo"),
+    
+    TriggerEventTag = cms.InputTag("hltTriggerSummaryAOD"),
+    TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
+    
+    PVTag = cms.InputTag("goodOfflinePrimaryVertices"),
+
+    #### tracks ####
+    TracksTag = cms.InputTag("generalTracks"),
+
+    #### muons ####
+    TauTag = cms.InputTag("patTaus"),
+    
+    #### muons ####
+    MuTag = cms.InputTag("patMuons"),
+     
+    #### electrons ####
+    EleTag                = cms.InputTag("patElectrons"),
+    EleID_names           = cms.vstring(
+        #'simpleEleId95relIso',
+        #'simpleEleId90relIso',
+        #'simpleEleId85relIso',
+        #'simpleEleId80relIso',
+        #'simpleEleId70relIso',
+        #'simpleEleId60relIso',
+        #'simpleEleId95cIso',
+        #'simpleEleId90cIso',
+        #'simpleEleId85cIso',
+        #'simpleEleId80cIso',
+        #'simpleEleId70cIso',
+        #'simpleEleId60cIso'
+        ),
+    
+    #### photons ####
+    PhotonTag    = cms.InputTag("patPhotons"),
+    EBRechitTag  = cms.InputTag("reducedEcalRecHitsEB"),
+    EERechitTag  = cms.InputTag("reducedEcalRecHitsEE"),
+    
+    
+    #### jets ####
+    JetTag     = cms.InputTag("patJets"),
+    BTag_names = cms.vstring('trackCountingHighEffBJetTags','trackCountingHighPurBJetTags','simpleSecondaryVertexHighPurBJetTags','simpleSecondaryVertexHighEffBJetTags'),
+    
+    MetTag   = cms.InputTag("patMETs"),
+    TCMetTag = cms.InputTag("patMETsAK5TC"),
+    PFMetTag = cms.InputTag("patMETsAK5PF"),
+    
+    #### jets ####
+    MCtruthTag = cms.InputTag("genParticles"),
+    
+    MatchingConeTreshold = cms.double(0.05),
+    ElectronPtCut        = cms.double(8.0),     
+    MuonPtCut        = cms.double(8.0),     
+    
+    #--------------
+    #### flags ####
+
+    dataFlag       = cms.untracked.bool (True),
+    saveHLT        = cms.untracked.bool (True),     
+    saveBS         = cms.untracked.bool (True),
+    savePV         = cms.untracked.bool (True),
+    saveRho        = cms.untracked.bool (True),
+    saveEleLessPV  = cms.untracked.bool (False), # default no revertex
+    saveMuonLessPV = cms.untracked.bool (False), # default no revertex
+    saveTrack      = cms.untracked.bool (False), # default no tracks
+    saveTau        = cms.untracked.bool (False), # default no tau saved
+    saveMu         = cms.untracked.bool (True),
+    saveEle        = cms.untracked.bool (True),
+    saveMet        = cms.untracked.bool (True),
+    saveJet        = cms.untracked.bool (True),
+    savePhoton     = cms.untracked.bool (False),
+    saveHCALNoise  = cms.untracked.bool (False),
+    
+    saveMCPU              = cms.untracked.bool (False),
+    saveMCPtHat           = cms.untracked.bool (False),
+    saveMCTTBar           = cms.untracked.bool (False),    
+    saveMCHiggs           = cms.untracked.bool (False),
+    saveMCHiggsWW         = cms.untracked.bool (False),
+    saveMCHiggsGammaGamma = cms.untracked.bool (False),
+    saveMCZW              = cms.untracked.bool (False),
+    saveGenJet            = cms.untracked.bool (False),
+    saveProcessId         = cms.untracked.bool (False),
+    savePhotonsMother     = cms.untracked.bool (False),
+       
+    verbosity = cms.untracked.bool(False),
+    eventType = cms.untracked.int32(0) 
+)
---
+++ PhysicsTools/MiBiCommonPAT/python/makeMiBiCommonNT_noPAT_cff.py
@@ -0,0 +1,161 @@
+import FWCore.ParameterSet.Config as cms
+
+from PhysicsTools.PatAlgos.selectionLayer1.leptonCountFilter_cfi import *
+from PhysicsTools.PatAlgos.selectionLayer1.photonCountFilter_cfi import *
+from PhysicsTools.PatAlgos.selectionLayer1.electronCountFilter_cfi import *
+from PhysicsTools.PatAlgos.selectionLayer1.jetCountFilter_cfi import *
+
+def makeMiBiCommonNT_noPAT(process, GlobalTag, HLT='HLT', MC=False, MCType='Other'):
+
+    # Setup the process
+    process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+    process.load("FWCore.MessageService.MessageLogger_cfi")
+    process.MessageLogger.cerr.FwkReport.reportEvery = 100
+    process.load('Configuration.StandardSequences.GeometryDB_cff')
+    process.load("Configuration.StandardSequences.MagneticField_cff")
+    process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+    process.GlobalTag.globaltag = GlobalTag
+    
+    
+    # Source
+    process.source = cms.Source(
+        "PoolSource",
+        fileNames = cms.untracked.vstring()
+    )
+    
+    # Out
+    process.out = cms.OutputModule(
+        "PoolOutputModule",
+        outputCommands = cms.untracked.vstring(),
+        fileName = cms.untracked.string('file:./MiBiCommonPAT.root'),
+    )
+    #process.e = cms.EndPath(process.out)
+    
+    
+    
+    process.load("PhysicsTools.NtupleUtils.PUDumper_cfi")
+    process.PUDumper.mcFlag = cms.untracked.bool(MC)
+    
+    process.load("PhysicsTools.NtupleUtils.AllPassFilter_cfi")
+    
+    
+    
+    #--------------------------
+    # Counter1: All read events
+    process.AllEvents = process.AllPassFilter.clone()
+    
+
+    
+    # the MiBiPAT path
+    process.MiBiCommonPAT = cms.Sequence(
+        process.PUDumper * 
+        process.AllEvents # -> Counter
+        )
+    
+    
+    
+    #----------------------
+    # Lepton/Photon Filters
+    process.load('PhysicsTools.PatAlgos.selectionLayer1.leptonCountFilter_cfi')
+    
+    process.LeptonsFilter = countPatLeptons.clone(
+      electronSource = cms.InputTag("selectedPatElectrons"),
+      muonSource     = cms.InputTag("selectedPatMuons"),
+      minNumber      = cms.uint32(1)
+     )
+
+    process.LeptonsFilterPFlow = countPatLeptons.clone(
+      electronSource = cms.InputTag("selectedPatElectronsPFlow"),
+      muonSource     = cms.InputTag("selectedPatMuonsPFlow"),
+      minNumber      = cms.uint32(1)
+     )
+    
+    
+    
+    #------------
+    # Jet Filters
+    process.load('PhysicsTools.PatAlgos.selectionLayer1.jetCountFilter_cfi')    
+    
+    process.JetFilterAK5PF = countPatJets.clone(
+      src = cms.InputTag("selectedPatJetsAK5PF"),
+      minNumber      = cms.uint32(2)
+    )
+    
+    process.JetFilterPFlow = countPatJets.clone(
+      #src = cms.InputTag("selectedPatJetsPFlow"),
+      src = cms.InputTag("goodPatJetsPFlow"),      
+      minNumber      = cms.uint32(2)
+    )
+    
+    
+    
+    #-----------
+    # Sequences & Other counters
+    process.LeptonsFilterEvents = process.AllPassFilter.clone()
+    process.JetFilterAK5PFEvents = process.AllPassFilter.clone()
+    process.LeptonsFilterPFlowEvents = process.AllPassFilter.clone()
+    process.JetFilterPFlowEvents = process.AllPassFilter.clone()
+    
+    process.OneLeptonTwoJetsAK5PFSeq = cms.Sequence(
+        process.LeptonsFilter*
+        process.LeptonsFilterEvents*
+        process.JetFilterAK5PF*
+        process.JetFilterAK5PFEvents
+        )
+    
+    process.OneLeptonTwoJetsPFlowSeq = cms.Sequence(
+        process.LeptonsFilterPFlow*
+        process.LeptonsFilterPFlowEvents*
+        process.JetFilterPFlow*
+        process.JetFilterPFlowEvents
+        )
+    
+    
+    
+    
+    
+    
+    # the MiBiNTUPLE
+    process.load("PhysicsTools.MiBiCommonPAT.SimpleNtuple_noPAT_cfi")
+    process.MiBiCommonNT = process.SimpleNtuple_noPAT.clone()
+    process.MiBiCommonNT.saveMCPU              = cms.untracked.bool (MC)
+    process.MiBiCommonNT.saveProcessId         = cms.untracked.bool (MC)
+    process.MiBiCommonNT.saveGenJet            = cms.untracked.bool (MC)
+    process.MiBiCommonNT.saveMCPtHat           = cms.untracked.bool (MC)
+    
+    if MCType == 'ZW':
+        process.MiBiCommonNT.saveMCZW = cms.untracked.bool (True)
+    if MCType == 'TTBar':
+        process.MiBiCommonNT.saveMCTTBar = cms.untracked.bool (True)
+    if MCType == 'Higgs':
+        process.MiBiCommonNT.saveMCHiggs = cms.untracked.bool (True)
+    if MCType == 'HiggsWW':
+        process.MiBiCommonNT.saveMCHiggs   = cms.untracked.bool (True)
+        process.MiBiCommonNT.saveMCHiggsWW = cms.untracked.bool (True)
+    
+    
+    process.MiBiCommonNTOneLeptonTwoJetsAK5PF = process.MiBiCommonNT.clone()
+    process.MiBiCommonNTOneLeptonTwoJetsAK5PF.MuTag         = cms.InputTag("selectedPatMuons")
+    process.MiBiCommonNTOneLeptonTwoJetsAK5PF.EleTag        = cms.InputTag("selectedPatElectrons")
+    process.MiBiCommonNTOneLeptonTwoJetsAK5PF.JetTag        = cms.InputTag("selectedPatJetsAK5PF")
+    process.MiBiCommonNTOneLeptonTwoJetsAK5PF.MetTag        = cms.InputTag("patMETsAK5PF")
+    
+    process.MiBiCommonNTOneLeptonTwoJetsPFlow = process.MiBiCommonNT.clone()
+    process.MiBiCommonNTOneLeptonTwoJetsPFlow.MuTag         = cms.InputTag("selectedPatMuonsPFlow")
+    process.MiBiCommonNTOneLeptonTwoJetsPFlow.EleTag        = cms.InputTag("selectedPatElectronsPFlow")
+    #process.MiBiCommonNTOneLeptonTwoJetsPFlow.JetTag        = cms.InputTag("selectedPatJetsPFlow")
+    process.MiBiCommonNTOneLeptonTwoJetsPFlow.JetTag        = cms.InputTag("goodPatJetsPFlow")
+    process.MiBiCommonNTOneLeptonTwoJetsPFlow.MetTag        = cms.InputTag("patMETsPFlow")
+    process.MiBiCommonNTOneLeptonTwoJetsPFlow.saveBS        = cms.untracked.bool(False)
+    process.MiBiCommonNTOneLeptonTwoJetsPFlow.saveRho       = cms.untracked.bool(False)
+    
+    
+    
+    
+    
+    
+    # paths
+    #process.MiBiPathAK5PF = cms.Path(process.MiBiCommonPAT*process.OneLeptonTwoJetsAK5PFSeq*process.MiBiCommonNTOneLeptonTwoJetsAK5PF)
+    process.MiBiPathPFlow = cms.Path(process.MiBiCommonPAT*process.OneLeptonTwoJetsPFlowSeq*process.MiBiCommonNTOneLeptonTwoJetsPFlow)    
+    
+    #process.out.outputCommands = cms.untracked.vstring('keep *')
