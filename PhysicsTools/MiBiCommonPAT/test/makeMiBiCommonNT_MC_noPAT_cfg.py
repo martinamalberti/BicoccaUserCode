46fbe14e2c0fb1cc3f4f2cc96e3dc380a451a011
abenagli <>
2011-12-20 12:21:24 +0000

    updates

--- PhysicsTools/MiBiCommonPAT/test/makeMiBiCommonNT_MC_noPAT_cfg.py
+++ PhysicsTools/MiBiCommonPAT/test/makeMiBiCommonNT_MC_noPAT_cfg.py
@@ -9,16 +9,12 @@ process = cms.Process("MiBiCommonNT")
 makeMiBiCommonNT_noPAT(process, GlobalTag="START42_V14B::All", HLT='', MC=True, MCType='Other')
 
 process.source.fileNames = cms.untracked.vstring(
-     'file:/data1/govoni/DY_PAT.root'
-#     'file:/media/DATA/WJetFall11_Example.root'
-#    'file:/data1/govoni/fastSimTest/test.root'
-#    'file:/gwteraz/users/benaglia/pat_42x_fall11_SQWaT_PAT_42X_WJetsToLNu_TuneZ2_7TeV-madgraph-tauola_v1.root'
+    #'file:/gwteraz/users/benaglia/pat_42x_fall11_SQWaT_PAT_42X_WJetsToLNu_TuneZ2_7TeV-madgraph-tauola_v1.root'
+    #'file:/gwteraz/users/benaglia/pat_42x_fall11_SQWaT_PAT_42X_WW_TuneZ2_7TeV_pythia6_tauola_v1.root'
     )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True))
-
-#process.SimpleNtuple_noPAT.saveSC = cms.untracked.bool (True)
 
 process.TFileService = cms.Service(
     "TFileService", 
