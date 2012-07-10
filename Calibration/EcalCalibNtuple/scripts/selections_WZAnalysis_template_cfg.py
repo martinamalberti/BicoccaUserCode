eae8497e0a8faedc1ce2673a62e0755f5a697e06
rgerosa <>
2012-07-10 12:27:06 +0000

    Template File for new WZAnalysis

--- Calibration/EcalCalibNtuple/scripts/selections_WZAnalysis_template_cfg.py
+++ Calibration/EcalCalibNtuple/scripts/selections_WZAnalysis_template_cfg.py
@@ -7,13 +7,13 @@ process = cms.Process("WZAnalysis")
 process.Input = cms.PSet(
 
     inputFileList = cms.string("LISTFILES"),
-    jsonFileName = cms.string("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/DCSOnly/json_DCSONLY.txt"),
+    jsonFileName = cms.string("/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions11/7TeV/Prompt/Cert_160404-180252_7TeV_PromptReco_Collisions11_JSON.txt"),
     inputFlag_isCalib=cms.bool(ISCAlib)
     )
 
 process.Output = cms.PSet(
-    outputRootFilePath = cms.string("OUTPUTSAVEPATH/OUTPUTSAVEFOLDER"),
-    outputRootFileName = cms.string("OUTPUTFILENAME_RUN")
+    outputRootFilePath = cms.string("/"),
+    outputRootFileName = cms.string("OUTPUTFILENAME_jobIt")
     )
 
 process.Options = cms.PSet(
@@ -21,6 +21,7 @@ process.Options = cms.PSet(
     entryMAX = cms.int32(-1),
     entryMODULO = cms.int32(100000),
     dataFlag = cms.int32(0),
+    dataRun = cms.string("RUN"),
     jsonFlag = cms.int32(0),
     crossSection = cms.double(1.),
     verbosity = cms.int32(0),
