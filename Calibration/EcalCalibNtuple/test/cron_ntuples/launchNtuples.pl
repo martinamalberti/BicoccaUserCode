f04fe2c5672cb32478c5f643d73e9cebc55fabaa
abenagli <>
2012-08-21 19:34:06 +0000

    updates

--- Calibration/EcalCalibNtuple/test/cron_ntuples/launchNtuples.pl
+++ Calibration/EcalCalibNtuple/test/cron_ntuples/launchNtuples.pl
@@ -39,34 +39,74 @@ print "PERIODName  = ".$PERIODName.  "\n";
 
 
 #--------------------
-# create cronjob file
+# create crab job dir
 
-system("rm lancia.sh");
-open LANCIA, ">", "lancia.sh";
-system("chmode a+x lancia.sh");
+@time = localtime(time);
+
+$year    = 1900+$time[5];
+$month   = 1+$time[4];
+$day     = $time[3];
+$hours   = $time[2];
+$minutes = $time[3];
+
+$date = $year."-".$month."-".$day."_".$hours.":".$minutes;
+print("date: ".$date."\n");
+
+system("mkdir ".$BASEDir."/test/cron_ntuples/".$FOLDERName."/".$date);
+system("cat crab_simpleNtupleEoverP_data_template.cfg   | sed -e s%BASEDIR%".$BASEDir.
+                                                    "%g | sed -e s%DATASETNAME%".$DATASETName.
+                                                    "%g | sed -e s%FOLDERNAME%".$FOLDERName.
+                                                    "%g | sed -e s%PERIODNAME%".$PERIODName.
+                                                    "%g | sed -e s%DATE%".$date.
+                                                    "%g > ".$BASEDir."/test/cron_ntuples/".$FOLDERName."/".$date."/crab_simpleNtupleEoverP_data.cfg");
+
+
+
+#-----------------
+# create json file
+# e.g.: {"190645": [[10, 110]], ... }
 
 open(RUNLIST,"runList.txt");
+
+open JSON, ">", "tempJson.txt";
+print JSON "{";
+
+$isFirstRun = 1;
+
 while(<RUNLIST>)
 {
   chomp;
   $run = $_;
   
-  print("run: ".$run."\n");
-  system("mkdir ".$BASEDir."/test/cron_ntuples/".$FOLDERName."/run".$run);
-  
-  system("cat crab_simpleNtupleEoverP_data_template.cfg   | sed -e s%BASEDIR%".$BASEDir.
-                                                      "%g | sed -e s%DATASETNAME%".$DATASETName.
-                                                      "%g | sed -e s%FOLDERNAME%".$FOLDERName.
-                                                      "%g | sed -e s%PERIODNAME%".$PERIODName.
-                                                      "%g | sed -e s%JSONFILE%".$JSONFile.
-                                                      "%g | sed -e s%RUN%".$run.
-                                                      "%g > ".$BASEDir."/test/cron_ntuples/".$FOLDERName."/run".$run."/crab_simpleNtupleEoverP_data.cfg");
-  
-  print LANCIA "echo \"\"\n";
-  print LANCIA "echo \"\"\n";
-  print LANCIA "echo \"\"\n";
-  print LANCIA "echo \">>>>>> RUN ".$run." <<<<<<\"\n";
-  print LANCIA "cd ".$BASEDir."/test/cron_ntuples/".$FOLDERName."/run".$run."\n";
-  print LANCIA "crab -create -submit -cfg ".$BASEDir."/test/cron_ntuples/".$FOLDERName."/run".$run."/crab_simpleNtupleEoverP_data.cfg\n";
-  print LANCIA "\n";
+  if( $isFirstRun == 1 )
+  {
+    print JSON "\"".$run."\": [[1,999999]]";
+    $isFirstRun = 0;
+  }
+  else
+  {
+    print JSON ", \"".$run."\": [[1,999999]]";
+  }
 }
+
+print JSON "}\n";
+
+$CMSSW_RELEASE_BASE = $ENV{'CMSSW_RELEASE_BASE'};
+system("python ".$CMSSW_RELEASE_BASE."/src/FWCore/PythonUtilities/scripts/compareJSON.py --and tempJson.txt /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/DCSOnly/json_DCSONLY.txt json.txt");
+
+
+
+#-------------------
+# create script file
+
+system("rm lancia.sh");
+open LANCIA, ">", "lancia.sh";
+system("chmode a+x lancia.sh");
+
+print LANCIA "echo \"\"\n";
+print LANCIA "echo \"\"\n";
+print LANCIA "echo \"\"\n";
+print LANCIA "echo \"****** DATE: ".$date." ******\"\n";
+print LANCIA "cd ".$BASEDir."/test/cron_ntuples/".$FOLDERName."/".$date."\n";
+print LANCIA "crab -create -submit -cfg ".$BASEDir."/test/cron_ntuples/".$FOLDERName."/".$date."/crab_simpleNtupleEoverP_data.cfg\n";
+print LANCIA "\n";
