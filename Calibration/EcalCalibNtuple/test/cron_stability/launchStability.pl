54114e70e590819e1351102dbe2ed6d8925cc927
ecalmon <>
2012-09-18 08:31:28 +0000

    updates

--- Calibration/EcalCalibNtuple/test/cron_stability/launchStability.pl
+++ Calibration/EcalCalibNtuple/test/cron_stability/launchStability.pl
@@ -9,6 +9,32 @@ use POSIX;
 
 
 
+#-------------
+# get the date
+
+@time = localtime(time);
+
+$year    = 1900+$time[5];
+$month   = 1+$time[4];
+$day     = $time[3];
+$hours   = $time[2];
+$minutes = $time[1];
+if( $minutes < 9 )
+{
+  $minutes = "0".$minutes;
+}
+
+$date = $month."/".$day."/".$year." ".$hours.":".$minutes;
+
+$command = "rm ./lastStabilityPlot.dat";
+system($command);
+
+open LASTSTABILITY, ">", "./lastStabilityPlot.dat";
+print LASTSTABILITY "Latest plot creation time: ".$date."\n";
+close LASTSTABILITY;
+
+
+
 #------------------
 # get the user name
 
@@ -21,15 +47,15 @@ print("user: ".$user."\n");
 #--------------------------------
 # get the files used for template
 
-$command = "cp -r /afs/cern.ch/user/a/abenagli/eos/cms/store/group/alca_ecalcalib/ecalMIBI/NTUPLES_EOverP/ReReco/13Jul2012_v2/SingleElectron_Run2012A-WElectron-13Jul2012-v1_USER/ /tmp/".$user;
+$command = "cp -r /afs/cern.ch/user/e/ecalmon//eos/cms/store/group/alca_ecalcalib/ecalMIBI/NTUPLES_EOverP/ReReco/13Jul2012_v2/SingleElectron_Run2012A-WElectron-13Jul2012-v1_USER/ /tmp/".$user;
 system($command);
-$command = "cp -r /afs/cern.ch/user/a/abenagli/eos/cms/store/group/alca_ecalcalib/ecalMIBI/NTUPLES_EOverP/ReReco/13Jul2012_v2/SingleElectron_Run2012B-WElectron-13Jul2012-v1_USER/ /tmp/".$user;
+$command = "cp -r /afs/cern.ch/user/e/ecalmon//eos/cms/store/group/alca_ecalcalib/ecalMIBI/NTUPLES_EOverP/ReReco/13Jul2012_v2/SingleElectron_Run2012B-WElectron-13Jul2012-v1_USER/ /tmp/".$user;
 system($command);
 
 open INPUTMC, ">", "inputMC.txt";
 
-print INPUTMC "/tmp/abenagli/SingleElectron_Run2012A-WElectron-13Jul2012-v1_USER/*.root\n";
-print INPUTMC "/tmp/abenagli/SingleElectron_Run2012B-WElectron-13Jul2012-v1_USER/*.root\n";
+print INPUTMC "/tmp/ecalmon/SingleElectron_Run2012A-WElectron-13Jul2012-v1_USER/*.root\n";
+print INPUTMC "/tmp/ecalmon/SingleElectron_Run2012B-WElectron-13Jul2012-v1_USER/*.root\n";
 
 
 
@@ -38,8 +64,11 @@ print INPUTMC "/tmp/abenagli/SingleElectron_Run2012B-WElectron-13Jul2012-v1_USER
 
 open INPUTDATA, ">", "inputDATA.txt";
 
-$command = "ls -lh /afs/cern.ch/user/a/abenagli/eos/cms/store/group/alca_ecalcalib/ecalMIBI/NTUPLES_EOverP/PromptReco/".
-           " | grep Run2012".
+print INPUTDATA "/tmp/ecalmon/SingleElectron_Run2012A-WElectron-13Jul2012-v1_USER/*.root\n";
+print INPUTDATA "/tmp/ecalmon/SingleElectron_Run2012B-WElectron-13Jul2012-v1_USER/*.root\n";
+
+$command = "ls -lh /afs/cern.ch/user/e/ecalmon//eos/cms/store/group/alca_ecalcalib/ecalMIBI/NTUPLES_EOverP/PromptReco/".
+           " | egrep 'Run2012C|Run2012D|Run2012E|Run2012F'".
            " | awk \'{print \$9}\'".
            " > list_periods.txt";
 system($command);
@@ -51,13 +80,13 @@ while(<LISTPERIODS>)
   $period = $_;
   print(">>> ".$period."\n");
   
-  $command = "ls -lh /afs/cern.ch/user/a/abenagli/eos/cms/store/group/alca_ecalcalib/ecalMIBI/NTUPLES_EOverP/PromptReco/".$period.
+  $command = "ls -lh /afs/cern.ch/user/e/ecalmon//eos/cms/store/group/alca_ecalcalib/ecalMIBI/NTUPLES_EOverP/PromptReco/".$period.
              " | grep Single".
              " | awk \'{print \$9}\'".
              " > list_".$period."_datasets.txt";
   system($command);
   
-  $command = "cp -r /afs/cern.ch/user/a/abenagli/eos/cms/store/group/alca_ecalcalib/ecalMIBI/NTUPLES_EOverP/PromptReco/".$period."/cron*Single* /tmp/".$user;
+  $command = "cp -r /afs/cern.ch/user/e/ecalmon//eos/cms/store/group/alca_ecalcalib/ecalMIBI/NTUPLES_EOverP/PromptReco/".$period."/cron*Single* /tmp/".$user;
   system($command);
   
   
@@ -69,7 +98,7 @@ while(<LISTPERIODS>)
     $dataset = $_;
     print(">>>>>> ".$dataset."\n");
     
-    $command = "ls -lh /afs/cern.ch/user/a/abenagli/eos/cms/store/group/alca_ecalcalib/ecalMIBI/NTUPLES_EOverP/PromptReco/".$period."/".$dataset.
+    $command = "ls -lh /afs/cern.ch/user/e/ecalmon//eos/cms/store/group/alca_ecalcalib/ecalMIBI/NTUPLES_EOverP/PromptReco/".$period."/".$dataset.
                " | grep :".
                " | awk \'{print \$9}\'".
                " > list_".$period."_".$dataset."_jobs.txt";
@@ -95,4 +124,18 @@ while(<LISTPERIODS>)
 # launch the plot production
 
 $command = "sh lancia.sh";
+system($command);
+
+
+
+#---------------
+# clean up a bit
+
+$command = "mv ./lastStabilityPlot.dat /afs/cern.ch/user/e/ecalmon/www/EoPStability2012/lastStabilityPlot.dat";
+system($command);
+
+$command = "rm list_*.txt";
+system($command);
+
+$command = "rm -rf /tmp/ecalmon/*";
 system($command);
