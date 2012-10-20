4d4ef3d0110716bbfcbd723f95fb92a9d1bf1a35
deguio <>
2012-10-20 13:48:34 +0000

    to run over rereco from sandbox

--- Calibration/EcalCalibNtuple/test/lxbatch/simpleNtuple_data_template_cfg.py
+++ Calibration/EcalCalibNtuple/test/lxbatch/simpleNtuple_data_template_cfg.py
@@ -1,26 +1,29 @@
 import FWCore.ParameterSet.Config as cms
 
-from Calibration.EcalCalibNtuple.simpleNtuple_cff import *
+from Calibration.EcalCalibNtuple.simpleNtupleEoverP_cff import *
 from Calibration.EcalCalibNtuple.recoTags_cff import *
 
-process = cms.Process("SimpleNtuple")
-
+process = cms.Process("SimpleNtupleEoverP")
 
 
 # initialize MessageLogger and output report
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1)
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
 process.MessageLogger.cerr.threshold = cms.untracked.string("DEBUG")
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True))
 
-
-
 # simpleNtuple
-ReReco=False
-makeSimpleNtuple(process,GlobalTag="GR_R_42_V14::All",ReReco=ReReco)
-makeRecoTags(process)
+makeSimpleNtuple(process,GlobalTag="GR_P_V42B::All",runOverSandbox=True,runOverData=True)
 
 
+# path
+process.simpleNtuple_step = cms.Path(
+    #process.hltfilter
+    #process.ecalDigis *
+    process.simpleNtupleEoverP
+    )
+
+process.simpleNtupleEoverP.saveRecHitMatrix = cms.untracked.bool(False)
 
 # source
 process.source.fileNames = cms.untracked.vstring(
@@ -31,48 +34,12 @@ process.maxEvents = cms.untracked.PSet(
    input = cms.untracked.int32(-1)
 )
 
-# out
-if not ReReco:
-    process.TFileService = cms.Service(
-        "TFileService",
-        fileName = cms.string("OUTPUTFILENAME_runRUN.root")
-        )
-
-    #process.out = cms.OutputModule(
-    #    "PoolOutputModule",
-    #    fileName = cms.untracked.string('file:SimplePATple.root'),
-    #    outputCommands = cms.untracked.vstring()
-    #    )
-
-if ReReco:
-    process.TFileService = cms.Service(
-        "TFileService",
-        fileName = cms.string("OUTPUTFILENAME_runRUN.root")
-        )
-
-    #process.out = cms.OutputModule(
-    #    "PoolOutputModule",
-    #    fileName = cms.untracked.string('simplePATpleReReco.root'),
-    #    outputCommands = cms.untracked.vstring('keep *_*_*_*')
-    #    )
-    #process.e = cms.EndPath(process.out)
+process.TFileService = cms.Service(
+    "TFileService",
+    fileName = cms.string("root://eoscms//eos/cms/OUTPUTFILENAME.root")
+    )
 
 
-
-# schedule
-
-if not ReReco:
-    process.schedule = cms.Schedule(
-        process.simpleNtuple_step
-        #process.e
-        )
-
-if ReReco:
-    process.schedule = cms.Schedule(
-        process.raw2digi_step,
-        process.L1Reco_step,
-        process.reconstruction_step,
-        process.endjob_step,
-        process.simpleNtuple_step,
-        #process.e
-        )
+process.schedule = cms.Schedule(
+    process.simpleNtuple_step
+    )
