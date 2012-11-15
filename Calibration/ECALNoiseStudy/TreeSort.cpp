f0fdce69e57fa7cbf8eefc4cd3e2a44ad6065eb9
rgerosa <>
2012-11-15 10:32:16 +0000

    Code for Tree ordering over a chosen Variable

--- Calibration/ECALNoiseStudy/TreeSort.cpp
+++ Calibration/ECALNoiseStudy/TreeSort.cpp
@@ -1,6 +1,6 @@
 
-// compile: g++ -Wall -o TreeSort `root-config --glibs --cflags` TreeSort.cpp
-// To run : ./TreeSort <inputFileList> <OutputFile> <Variable to sort>
+// compile: g++ -Wall -o TreeSort `root-config --glibs --cflags` Sorter.h TreeSort.cpp
+// To run : ./TreeSort <inputFile> <ouptutPath> <OutputFile> <Variable to sort> <type of variable>
 
 #include "TROOT.h"
 #include "TStyle.h"
@@ -14,6 +14,7 @@
 #include "TTree.h"
 #include "TVirtualFitter.h"
 #include "TChain.h"
+#include "Sorter.h"
 
 #include <iostream>
 #include <iomanip>
@@ -26,113 +27,171 @@
 #include <vector>
 #include <fstream>
 
-struct Sorter{
-  int sortVar;
-  int entry;
-
-  bool operator() (const Sorter& s1, const Sorter& s2)
-  {
-    return s1.sortVar < s2.sortVar;
-  }
-};
-
-bool FillChain(TChain& chain, const std::string& inputFileList);
 
 
 
 int main ( int argc, char** argv){
   
-  if(argc!=5) { std::cerr<<" Error: input on command line not correct "<<std::endl; return 1; }
+  if(argc!=6) { std::cerr<<" Error: input on command line not correct "<<std::endl; return 1; }
 
-  std::string InputList        = argv[1];
+  std::string InputFile        = argv[1];
   std::string OutputPath       = argv[2]; 
   std::string OutputFile       = argv[3];
   std::string SortVarName      = argv[4];
+  std::string SortVarType      = argv[5];
 
-  TChain* chain = new TChain("simpleNtupleEoverP/SimpleNtupleEoverP"); // Input list of File
-  FillChain(*chain,InputList);
+  TFile *Input = new TFile(InputFile.c_str(),"READ");
+  TTree* tree= (TTree*) Input->Get("simpleNtupleEoverP/SimpleNtupleEoverP");
 
   std::string Name = OutputPath + OutputFile;
   TFile* out = new TFile(Name.c_str(),"RECREATE"); // Output File definition
 
-  // Set branch addresses                                                                                                                                    
-  int SortVariable;
-  chain -> SetBranchAddress(SortVarName.c_str(), &SortVariable);
+  int nEntries = tree -> GetEntries();
 
-  int nEntries = chain -> GetEntries();
+  TTree *cloneTree = (TTree*) tree->CloneTree(0); // Clone the chain
 
-  TChain *cloneChain = (TChain*)chain->CloneTree(0); // Clone the chain
+  std::cout << "DATA: " <<  nEntries  << " entries in " << InputFile << std::endl;
+ 
+  std::cout<<" **************************** "<<std::endl;
+  std::cout<<" ********* SORT ************* "<<std::endl;
 
-  TTree* clone = cloneChain -> GetTree(); // Get a Tree
-
-  std::cout << "DATA: " <<  nEntries  << " entries in " << InputList << std::endl;
-  
-    
-  // Loop and sort events
-  std::cout << std::endl;
-  std::cout << "***** Sort events *****" << std::endl;
-  
-  std::vector<Sorter> sortedEntries; // in order to sort the entries
-  
   int nSavePts = 0;
 
-  for(int iEntry = 0; iEntry < nEntries; ++iEntry){
+  if(SortVarType=="int"){
+ 
+   int SortVariable; tree->SetBranchAddress(SortVarName.c_str(),&SortVariable);
+  
+   std::vector< Sorter<int> >  sortedEntries; // in order to sort the entries
 
+  
+   for(int iEntry = 0; iEntry < nEntries; ++iEntry){
     if( (iEntry%100000 == 0) ) std::cout << "reading saved entry " << iEntry << std::endl;
-    
-    chain -> GetEntry(iEntry);
-    //    std::cout<<SortVariable<<std::endl;  
-    Sorter dummy;
-    dummy.sortVar = SortVariable;
-    dummy.entry   = iEntry;
-    sortedEntries.push_back(dummy);
-    
+    tree -> GetEntry(iEntry);
+    Sorter<int> dummy; 
+    dummy.sortVar = SortVariable; dummy.entry   = iEntry; sortedEntries.push_back(dummy);
     nSavePts++; 
   }
 
-  std::sort(sortedEntries.begin(),sortedEntries.end(),Sorter());
-  std::cout << "DATA: " << std::setw(8) << nSavePts << " entries in " << InputList << std::endl;
+  std::sort(sortedEntries.begin(),sortedEntries.end(),Sorter<int>());
+  std::cout << "DATA: " << std::setw(8) << nSavePts << " entries in " << InputFile << std::endl;
   
-    
-  // Clone tree
+  // Clone tree                                                                                                                                                   
   std::cout << std::endl;
-  std::cout << "***** Clone tree *****" << std::endl;
-  
+  std::cout<<" **************************** "<<std::endl;
+  std::cout << "******* Clone tree ********" << std::endl;
+
   for(int iSaved = 0; iSaved < nSavePts; ++iSaved){
 
-   if( (iSaved%10000 == 0) ) std::cout << "reading saved entry " << iSaved << std::endl;
-    
-      chain -> GetEntry(sortedEntries[iSaved].entry);
-    
-      clone -> Fill();
-  }
-  
-   out -> Close();
+    if( (iSaved%10000 == 0) ) std::cout << "reading saved entry " << iSaved << std::endl;
 
+    tree -> GetEntry(sortedEntries[iSaved].entry);
+    cloneTree -> Fill();
+  }
+ }
+
+ if(SortVarType=="float"){
+
+    float SortVariable; tree->SetBranchAddress(SortVarName.c_str(),&SortVariable);
+
+    std::vector< Sorter<float> >  sortedEntries; // in order to sort the entries                                                                                
+                                                                                                                                                                  
+      for(int iEntry = 0; iEntry < nEntries; ++iEntry){
+	if( (iEntry%100000 == 0) ) std::cout << "reading saved entry " << iEntry << std::endl;
+	tree -> GetEntry(iEntry);
+	Sorter<float> dummy ;    dummy.sortVar = SortVariable; dummy.entry   = iEntry; sortedEntries.push_back(dummy);
+	nSavePts++;
+      }
+
+      std::sort(sortedEntries.begin(),sortedEntries.end(),Sorter<float>());
+      std::cout << "DATA: " << std::setw(8) << nSavePts << " entries in " << InputFile << std::endl;
+
+      // Clone tree 
+                                                                                                                                                               
+      std::cout << std::endl;
+      std::cout<<" **************************** "<<std::endl;
+      std::cout << "******* Clone tree ********" << std::endl;
+
+      for(int iSaved = 0; iSaved < nSavePts; ++iSaved){
+
+	  if( (iSaved%10000 == 0) ) std::cout << "reading saved entry " << iSaved << std::endl;
+
+	  tree -> GetEntry(sortedEntries[iSaved].entry);
+	  cloneTree -> Fill();
+      }
+
+
+ }
+
+
+
+ if(SortVarType=="double"){
+
+    double SortVariable; tree->SetBranchAddress(SortVarName.c_str(),&SortVariable);
+
+    std::vector< Sorter<double> >  sortedEntries; // in order to sort the entries                                                                                       
+    for(int iEntry = 0; iEntry < nEntries; ++iEntry){
+      if( (iEntry%100000 == 0) ) std::cout << "reading saved entry " << iEntry << std::endl;
+      tree -> GetEntry(iEntry);
+      Sorter<double> dummy ;    dummy.sortVar = SortVariable; dummy.entry   = iEntry; sortedEntries.push_back(dummy);
+      nSavePts++;
+    }
+
+    std::sort(sortedEntries.begin(),sortedEntries.end(),Sorter<double>());
+    std::cout << "DATA: " << std::setw(8) << nSavePts << " entries in " << InputFile << std::endl;
+
+
+    // Clone tree                                                                                                                                                   
+    std::cout << std::endl;
+    std::cout<<" **************************** "<<std::endl;
+    std::cout << "******* Clone tree ********" << std::endl;
+
+    for(int iSaved = 0; iSaved < nSavePts; ++iSaved){
+
+      if( (iSaved%10000 == 0) ) std::cout << "reading saved entry " << iSaved << std::endl;
+
+      tree -> GetEntry(sortedEntries[iSaved].entry);
+      cloneTree -> Fill();
+    }
+
+
+  }
+
+  if(SortVarType=="string"){
+
+    std::string SortVariable; tree->SetBranchAddress(SortVarName.c_str(),&SortVariable);
+
+    std::vector< Sorter<std::string> >  sortedEntries; // in order to sort the entries                                                                                       
+
+    for(int iEntry = 0; iEntry < nEntries; ++iEntry){
+      if( (iEntry%100000 == 0) ) std::cout << "reading saved entry " << iEntry << std::endl;
+      tree -> GetEntry(iEntry);
+      Sorter<std::string> dummy ;    dummy.sortVar = SortVariable; dummy.entry   = iEntry; sortedEntries.push_back(dummy);
+      nSavePts++;
+    }
+
+    std::sort(sortedEntries.begin(),sortedEntries.end(),Sorter<std::string>());
+    std::cout << "DATA: " << std::setw(8) << nSavePts << " entries in " << InputFile << std::endl;
+
+
+    // Clone tree                                                                                                                                                   
+    std::cout << std::endl;
+    std::cout<<" **************************** "<<std::endl;
+    std::cout << "******* Clone tree ********" << std::endl;
+
+    for(int iSaved = 0; iSaved < nSavePts; ++iSaved){
+
+      if( (iSaved%10000 == 0) ) std::cout << "reading saved entry " << iSaved << std::endl;
+
+      tree -> GetEntry(sortedEntries[iSaved].entry);
+      cloneTree -> Fill();
+    }
+
+  }
+
+  out -> Close();
+  
   return 0; 
   
 }
 
 
-bool FillChain(TChain& chain, const std::string& inputFileList)
-{
-  std::ifstream inFile(inputFileList.c_str());
-  std::string buffer;
-
-  if(!inFile.is_open())
-    {
-      std::cerr << "** ERROR: Can't open '" << inputFileList << "' for input" << std::endl;
-      return false;
-    }
-
-  while(1)
-    {
-      inFile >> buffer;
-      if(!inFile.good()) break;
-      if( buffer.at(0) == '#' ) continue;
-      chain.Add(buffer.c_str());
-      std::cout << ">>> ntupleUtils::FillChain - treeName = " << chain.GetName() << " from file " << buffer << std::endl;
-    }
-
-  return true;
-}
