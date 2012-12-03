0974dba9521e253865cca3d091f21ce2bd50c025
abenagli <>
2012-12-03 18:35:42 +0000

    updates

--- Calibration/EcalCalibNtuple/python/recoTags_cff.py
+++ Calibration/EcalCalibNtuple/python/recoTags_cff.py
@@ -5,26 +5,26 @@ import FWCore.ParameterSet.Config as cms
 def makeRecoTags(process):
     
     process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")    
-
+    
     process.GlobalTag.toGet = cms.VPSet(
-        cms.PSet(record = cms.string("EcalADCToGeVConstantRcd"),
-                 tag = cms.string('EcalADCToGeVConstant_Bon_V20111129'),
-                 connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_ECAL")
+        
+        cms.PSet(record = cms.string("EcalIntercalibConstantsRcd"),
+                 tag = cms.string("EcalIntercalibConstants_V20121130_PizC_EleABC_2012HR9EtaScaleABC")
+                 connect = cms.untracked.string("frontier://FrontierInt/CMS_COND_ECAL")
                  ),
         cms.PSet(record = cms.string("EcalLaserAPDPNRatiosRcd"),
-                 tag = cms.string('EcalLaserAPDPNRatios_20121020_447_p1_v2'),
+                 tag = cms.string("EcalLaserAPDPNRatios_20121020_447_p1_v2"),
                  connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_42X_ECAL_LAS")
                  ),
         cms.PSet(record = cms.string('EcalLaserAlphasRcd'),
                  tag = cms.string('EcalLaserAlphas_EB_sic1_btcp152_EE_sic1_btcp116'),
                  connect = cms.untracked.string('frontier://FrontierInt/CMS_COND_ECAL')
-                 ),
-        cms.PSet(record = cms.string("EcalIntercalibConstantsRcd"),
-                 tag = cms.string('EcalIntercalibConstants_V20120620_piZPhiSEtaScale2012_IOV2_AlphaStudies'),
-                 connect = cms.untracked.string("frontier://FrontierInt/CMS_COND_ECAL")
                  )
-
-)
+        cms.PSet(record = cms.string("EcalADCToGeVConstantRcd"),
+                 tag = cms.string("EcalADCToGeVConstant_Bon_V20111129"),
+                 connect =cms.untracked.string("frontier://FrontierProd/CMS_COND_31X_ECAL")
+                 )
+        )
 
 
 
@@ -38,7 +38,6 @@ def makeSqliteTags(process):
         toGet = cms.VPSet(cms.PSet(
             record = cms.string('EcalLaserAPDPNRatiosRcd'),
             tag = cms.string('test')
-            )
+            ))
         )
-    )
     process.es_prefer_conddb = cms.ESPrefer("PoolDBESSource","conddb")
