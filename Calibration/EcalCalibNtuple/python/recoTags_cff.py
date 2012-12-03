52977f9feb97bf2f306234c1435bb6a9a83e4e22
abenagli <>
2012-12-03 19:42:27 +0000

    updates

--- Calibration/EcalCalibNtuple/python/recoTags_cff.py
+++ Calibration/EcalCalibNtuple/python/recoTags_cff.py
@@ -9,16 +9,16 @@ def makeRecoTags(process):
     process.GlobalTag.toGet = cms.VPSet(
         
         cms.PSet(record = cms.string("EcalIntercalibConstantsRcd"),
-                 tag = cms.string("EcalIntercalibConstants_V20121130_PizC_EleABC_2012HR9EtaScaleABC")
+                 tag = cms.string("EcalIntercalibConstants_V20121130_PizC_EleABC_2012HR9EtaScaleABC"),
                  connect = cms.untracked.string("frontier://FrontierInt/CMS_COND_ECAL")
                  ),
         cms.PSet(record = cms.string("EcalLaserAPDPNRatiosRcd"),
                  tag = cms.string("EcalLaserAPDPNRatios_20121020_447_p1_v2"),
                  connect = cms.untracked.string("frontier://FrontierProd/CMS_COND_42X_ECAL_LAS")
                  ),
-        cms.PSet(record = cms.string('EcalLaserAlphasRcd'),
-                 tag = cms.string('EcalLaserAlphas_EB_sic1_btcp152_EE_sic1_btcp116'),
-                 connect = cms.untracked.string('frontier://FrontierInt/CMS_COND_ECAL')
+        cms.PSet(record = cms.string("EcalLaserAlphasRcd"),
+                 tag = cms.string("EcalLaserAlphas_EB_sic1_btcp152_EE_sic1_btcp116"),
+                 connect = cms.untracked.string("frontier://FrontierInt/CMS_COND_ECAL")
                  ),
         cms.PSet(record = cms.string("EcalADCToGeVConstantRcd"),
                  tag = cms.string("EcalADCToGeVConstant_Bon_V20111129"),
