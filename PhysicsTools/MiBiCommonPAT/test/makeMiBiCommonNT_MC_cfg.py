35a89ba88412fb05f31996bd269d44b1c6dd42d3
Martina <martina.malberti@cern.ch>
2013-08-07 19:08:48 +0200

    update

--- PhysicsTools/MiBiCommonPAT/test/makeMiBiCommonNT_MC_cfg.py
+++ PhysicsTools/MiBiCommonPAT/test/makeMiBiCommonNT_MC_cfg.py
@@ -6,12 +6,13 @@ process = cms.Process("MiBiCommonNT")
 
 # the MiBiNT: Choose the MC type!!!
 # Possible choices: TTBar, Higgs, HiggsWW, HiggsGammaGamma, ZW, Other
-makeMiBiCommonNT(process, GlobalTag="START52_V9::All", HLT='', MC=True, MCType='ZW')
+#makeMiBiCommonNT(process, GlobalTag="START53_V27::All", HLT='', MC=True, MCType='ZW') # MC 2012
+makeMiBiCommonNT(process, GlobalTag="START53_V7N::All", HLT='', MC=True, MCType='ZW') # run dep MC 2012
+#makeMiBiCommonNT(process, GlobalTag="FT_R_53_LV3::All", HLT='', MC=True, MCType='ZW') # MC 2011
   
 process.source.fileNames = cms.untracked.vstring(
     #'file:/data2/amassiro/CMSSWRoot/Summer12/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_AODSIM/AODSIM.root'
-     'file:/tmp/malberti/AODSIM.root'
-        )
+    )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False))
